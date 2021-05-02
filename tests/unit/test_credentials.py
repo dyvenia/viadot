f5d4a240b89f6ad8ce72fc9bc22ec98ad8cfb6f8
trymzet <michalmzawadzki@gmail.com>
2021-05-03 00:22:45 +0200

    🚚 More path changes

--- tests/unit/test_credentials.py
+++ tests/unit/test_credentials.py
@@ -1,35 +1,36 @@
 import json
 import os
 import pytest
-from os.path import expanduser, join
+from os.path import expanduser, join, dirname, realpath
 from viadot.config import Config
 
 USER_HOME = expanduser("~")
-UNIT_TESTS_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
-LOCAL_CREDENTIALS_PATH = os.path.join(UNIT_TESTS_DIR_PATH, "credentials.json")
+UNIT_TESTS_DIR_PATH = dirname(realpath(__file__))
+UNIT_TESTS_CONFIG_PATH = join(UNIT_TESTS_DIR_PATH, "config.json")
+DEFAULT_CONFIG_PATH = join(USER_HOME, ".config", "config.json")
 
 
 @pytest.fixture(scope="session")
-def home_credentials():
-    HOME_CREDENTIALS_PATH = join(USER_HOME, ".config", "credentials.json")
-    yield HOME_CREDENTIALS_PATH
-    os.rename(HOME_CREDENTIALS_PATH + "_renamed", HOME_CREDENTIALS_PATH)
+def config():
+    CONFIG_PATH = os.environ.get("VIADOT_CONFIG_PATH") or DEFAULT_CONFIG_PATH
+    yield CONFIG_PATH
+    os.rename(CONFIG_PATH + "_renamed", CONFIG_PATH)
 
 
 def get_credentials(key: str):
-    with open(LOCAL_CREDENTIALS_PATH, "r") as f:
+    with open(UNIT_TESTS_CONFIG_PATH, "r") as f:
         credentials = json.load(f)
     return credentials[key]
 
 
 def test_credentials():
-    test_credentials = Config.from_json(LOCAL_CREDENTIALS_PATH, key="TEST_STANZA")
+    test_credentials = Config.from_json(UNIT_TESTS_CONFIG_PATH, key="TEST_STANZA")
     credentials = get_credentials("TEST_STANZA")
     assert test_credentials == credentials
 
 
-def test_no_credentials_file(home_credentials):
-    os.rename(home_credentials, home_credentials + "_renamed")
+def test_no_credentials_file(config):
+    os.rename(config, config + "_renamed")
     from viadot.sources import UKCarbonIntensity
 
     UKCarbonIntensity()
