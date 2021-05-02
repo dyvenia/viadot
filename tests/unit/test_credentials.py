00dd205b85964fdf8c69fd96626c47790299d0bb
trymzet <michalmzawadzki@gmail.com>
2021-05-02 22:16:41 +0200

    🎨 Moved fixture to top

--- tests/unit/test_credentials.py
+++ tests/unit/test_credentials.py
@@ -7,15 +7,6 @@ from viadot.config import Config
 LOCAL_CREDENTIALS_PATH = "/home/viadot/tests/unit/credentials.json"
 USER_HOME = expanduser("~")
 
-def get_credentials(key: str):
-    with open(LOCAL_CREDENTIALS_PATH, "r") as f:
-        credentials = json.load(f)
-    return credentials[key]
-
-def test_credentials():
-    test_credentials = Config.from_json(LOCAL_CREDENTIALS_PATH, key="TEST_STANZA")
-    credentials = get_credentials("TEST_STANZA")
-    assert test_credentials == credentials
 
 @pytest.fixture(scope="session")
 def home_credentials():
@@ -23,8 +14,21 @@ def home_credentials():
     yield HOME_CREDENTIALS_PATH
     os.rename(HOME_CREDENTIALS_PATH + "_renamed", HOME_CREDENTIALS_PATH)
 
+
+def get_credentials(key: str):
+    with open(LOCAL_CREDENTIALS_PATH, "r") as f:
+        credentials = json.load(f)
+    return credentials[key]
+
+
+def test_credentials():
+    test_credentials = Config.from_json(LOCAL_CREDENTIALS_PATH, key="TEST_STANZA")
+    credentials = get_credentials("TEST_STANZA")
+    assert test_credentials == credentials
+
+
 def test_no_credentials_file(home_credentials):
     os.rename(home_credentials, home_credentials + "_renamed")
     from viadot.sources import UKCarbonIntensity
-    ukci = UKCarbonIntensity()
 
+    UKCarbonIntensity()
