f59ce0242a588e1a28c6c873590c310b498ee629
trymzet <mzawadzki@dyvenia.com>
2021-05-13 14:35:42 +0200

    ✨ Added retries

--- viadot/tasks/supermetrics.py
+++ viadot/tasks/supermetrics.py
@@ -2,13 +2,15 @@ from typing import Any, Dict
 
 import prefect
 from prefect import Task
+from datetime import timedelta
+
 
 from ..sources import Supermetrics
 
 
 class SupermetricsToCSV(Task):
     def __init__(self, *args, **kwargs):
-        super().__init__(name="supermetrics_to_csv", *args, **kwargs)
+        super().__init__(name="supermetrics_to_csv", max_retries=5, retry_delay=timedelta(seconds=10), *args, **kwargs)
 
     def __call__(self):
         """Download Supermetrics data to a CSV"""
