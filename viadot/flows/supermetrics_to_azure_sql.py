aa60240bc3e9e98f9101bdbc6a07b8903fa9a397
trymzet <mzawadzki@dyvenia.com>
2021-05-18 11:38:12 +0200

    ♻️ Rewrote so each account is a separate task

--- viadot/flows/supermetrics_to_azure_sql.py
+++ viadot/flows/supermetrics_to_azure_sql.py
@@ -1,8 +1,11 @@
 from typing import Any, Dict, List
 
-from prefect import Flow
+from prefect import Flow, Task, apply_map
+from prefect.utilities import logging
 
 from ..tasks import BlobFromCSV, CreateTableFromBlob, SupermetricsToCSV
+
+logger = logging.get_logger(__name__)
 
 supermetrics_to_csv_task = SupermetricsToCSV()
 csv_to_blob_storage_task = BlobFromCSV()
@@ -13,18 +16,30 @@ class SupermetricsToAzureSQL(Flow):
     def __init__(
         self,
         name: str,
-        query: Dict[str, Any] = None,
+        ds_id: str,
+        ds_accounts: List[str],
+        ds_user: str,
+        fields: List[str],
+        date_range_type: str = None,
+        max_rows: int = 1000000,
         dtypes: Dict[str, Any] = None,
         blob_path: str = None,
         overwrite_blob: bool = True,
         table: str = None,
         schema: str = None,
         local_file_path: str = None,
-        if_exists: str = "replace",
+        if_exists: str = "replace",  # this applies to the full CSV file, not per chunk
+        if_empty: str = "warn",
+        max_download_retries: int = 5,
         *args: List[any],
         **kwargs: Dict[str, Any]
     ):
-        self.query = query
+        self.ds_id = ds_id
+        self.ds_accounts = ds_accounts
+        self.ds_user = ds_user
+        self.fields = fields
+        self.date_range_type = date_range_type
+        self.max_rows = max_rows
         self.local_file_path = local_file_path or "test.csv"
         self.blob_path = blob_path
         self.overwrite_blob = overwrite_blob
@@ -32,6 +47,8 @@ class SupermetricsToAzureSQL(Flow):
         self.schema = schema
         self.dtypes = dtypes
         self.if_exists = if_exists
+        self.if_empty = if_empty
+        self.max_download_retries = max_download_retries
         self.tasks = [
             supermetrics_to_csv_task,
             csv_to_blob_storage_task,
@@ -40,10 +57,23 @@ class SupermetricsToAzureSQL(Flow):
         super().__init__(*args, name=name, **kwargs)
         self.gen_flow()
 
-    def gen_flow(self):
-        supermetrics_to_csv_task.bind(
-            query=self.query, path=self.local_file_path, flow=self
+    def gen_supermetrics_task(self, ds_account: str, flow: Flow = None) -> Task:
+        t = supermetrics_to_csv_task.bind(
+            ds_id = self.ds_id,
+            ds_account = ds_account,
+            ds_user = self.ds_user,
+            fields = self.fields,
+            date_range_type = self.date_range_type,
+            path=self.local_file_path,
+            if_exists="append",
+            if_empty=self.if_empty,
+            max_retries=self.max_download_retries,
+            flow=flow
         )
+        return t
+
+    def gen_flow(self) -> Flow:
+        supermetrics_downloads = apply_map(self.gen_supermetrics_task, self.ds_accounts, flow=self)
         csv_to_blob_storage_task.bind(
             from_path=self.local_file_path,
             to_path=self.blob_path,
@@ -59,5 +89,5 @@ class SupermetricsToAzureSQL(Flow):
             flow=self,
         )
 
-        csv_to_blob_storage_task.set_upstream(supermetrics_to_csv_task, flow=self)
+        csv_to_blob_storage_task.set_upstream(supermetrics_downloads, flow=self)
         blob_to_azure_sql_task.set_upstream(csv_to_blob_storage_task, flow=self)
