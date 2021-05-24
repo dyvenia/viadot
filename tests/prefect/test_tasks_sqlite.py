426e6a11a2854f8ba3749b607463f212949d96c6
AdamSulek <adam-s91@o2.pl>
2021-05-24 17:17:01 +0200

    docs howtos changes for each task

--- tests/prefect/test_tasks_sqlite.py
+++ tests/prefect/test_tasks_sqlite.py
@@ -16,14 +16,14 @@ def load_table():
 def test_create_table_from_df(load_table):
     dtypes = {"country": "VARCHAR(100)", "sales": "FLOAT(24)"}
     df_data = pd.DataFrame({"country": ['italy'], "sales": [100.]})
-    load = load_table.run(table_name=TABLE,
+    result = load_table.run(table_name=TABLE,
                    schema=None,
                    dtypes=dtypes,
                    db_path=DB_PATH,
                    df=df_data,
                    if_exists="replace"
                    )
-    assert load == True
+    assert result == True
 
 def test_run_sql():
     run_sql = SQLtoDF(db_path=DB_PATH, sql_path=SQL_PATH)
