b7239f781f418f5439acdb34705a9d7cbbdf8a9d
trymzet <mzawadzki@dyvenia.com>
2021-05-27 18:56:24 +0200

    ✨ Added a new ReadAzureKeyVaultSecret task

--- viadot/tasks/__init__.py
+++ viadot/tasks/__init__.py
@@ -1,3 +1,5 @@
 from .azure_blob_storage import BlobFromCSV
 from .azure_sql import CreateTableFromBlob
 from .supermetrics import SupermetricsToCSV
+from .github import CloneRepo
+from .azure_key_vault import ReadAzureKeyVaultSecret
