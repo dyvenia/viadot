65310bcf2a1a366d2eba3a41f74e2fbc030ab077
m-paz <mpazdzior@dyvenia.com>
2021-07-07 12:05:13 +0200

    🐛 Fixed flow_of_flows project_name issue

--- viadot/flows/flow_of_flows.py
+++ viadot/flows/flow_of_flows.py
@@ -11,6 +11,7 @@ class Pipeline(Flow):
     def __init__(
         self,
         name: str,
+        project_name: str,
         extract_flows_names: List[str],
         transform_flow_name: str,
         *args: List[any],
@@ -18,11 +19,14 @@ class Pipeline(Flow):
     ):
         self.extract_flows_names = extract_flows_names
         self.transform_flow_name = transform_flow_name
+        self.project_name = project_name
         super().__init__(*args, name=name, **kwargs)
         self.gen_flow()
 
     def gen_start_flow_run_task(self, flow_name: str, flow: Flow = None) -> Task:
-        t = start_flow_run_task.bind(flow_name=flow_name, flow=flow)
+        t = start_flow_run_task.bind(
+            flow_name=flow_name, project_name=self.project_name, flow=flow
+        )
         return t
 
     def gen_flow(self) -> Flow:
@@ -30,6 +34,8 @@ class Pipeline(Flow):
             self.gen_start_flow_run_task, self.extract_flows_names, flow=self
         )
         transform_flow_run = start_flow_run_task_2.bind(
-            flow_name=self.transform_flow_name, flow=self
+            flow_name=self.transform_flow_name,
+            project_name=self.project_name,
+            flow=self,
         )
         transform_flow_run.set_upstream(extract_flow_runs, flow=self)
