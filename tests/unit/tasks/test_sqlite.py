50dd9fed2e45b9883ee6d82f016f00ccc2a6c167
trymzet <mzawadzki@dyvenia.com>
2021-07-08 17:06:00 +0200

    🐛 Fixing SQLite tests failing on GitHub

--- tests/unit/tasks/test_sqlite.py
+++ tests/unit/tasks/test_sqlite.py
@@ -1,14 +1,23 @@
+import errno
 import os
 
 import pandas as pd
 import pytest
-
 from viadot.tasks.sqlite import Insert, SQLtoDF
 
 TABLE = "test"
 DB_PATH = "/home/viadot/tests/testfile_db.sqlite"
 SQL_PATH_SELECT = "/home/viadot/tests/testfile_select.sql"
 SQL_PATH_NOTSELECT = "/home/viadot/tests/testfile_notselect.sql"
+
+
+# make dir for files if it doesn't exist
+if not os.path.exists(os.path.dirname(SQL_PATH_SELECT)):
+    try:
+        os.makedirs(os.path.dirname(SQL_PATH_SELECT))
+    except OSError as exc:  # Guard against race condition
+        if exc.errno != errno.EEXIST:
+            raise
 
 
 @pytest.fixture(scope="session")
