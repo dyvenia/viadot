52a3474f6cfca348941e378a6a76eb52134f856f
trymzet <mzawadzki@dyvenia.com>
2021-07-12 13:41:48 +0200

    ✨ The flow now accepts and passes `credentials_secret` to `RunAzureSQLQuery` task.

--- viadot/flows/azure_sql_transform.py
+++ viadot/flows/azure_sql_transform.py
@@ -1,11 +1,8 @@
 from typing import Any, Dict, List
 
 from prefect import Flow
-from prefect.utilities import logging
 
 from ..tasks.azure_sql import RunAzureSQLDBQuery
-
-logger = logging.get_logger(__name__)
 
 query_task = RunAzureSQLDBQuery()
 
@@ -15,15 +12,25 @@ class AzureSQLTransform(Flow):
         self,
         name: str,
         query: str,
+        sqldb_credentials_secret: str = None,
+        vault_name: str = None,
         tags: List[str] = ["transform"],
         *args: List[any],
         **kwargs: Dict[str, Any]
     ):
         self.query = query
-        self.tasks = [query_task]
         self.tags = tags
+        self.sqldb_credentials_secret = sqldb_credentials_secret
+        self.vault_name = vault_name
+        self.tasks = [query_task]
+
         super().__init__(*args, name=name, **kwargs)
         self.gen_flow()
 
     def gen_flow(self) -> Flow:
-        query_task.bind(query=self.query, flow=self)
+        query_task.bind(
+            query=self.query,
+            credentials_secret=self.sqldb_credentials_secret,
+            vault_name=self.vault_name,
+            flow=self,
+        )
