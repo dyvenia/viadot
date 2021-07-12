cbd5f961e53f9fa552faddc50b2c572d8aa3ac8d
trymzet <mzawadzki@dyvenia.com>
2021-07-12 14:35:42 +0200

    ✅ Added a test for `AzureSQLTransform` flow

---
+++ tests/integration/flows/test_azure_sql_transform.py
@@ -0,0 +1,27 @@
+import pytest
+from viadot.flows import AzureSQLTransform
+from viadot.tasks import RunAzureSQLDBQuery
+
+SCHEMA = "sandbox"
+TABLE = "test"
+FQN = f"{SCHEMA}.{TABLE}"
+
+
+@pytest.fixture()
+def TEST_TABLE():
+    run_sql_task = RunAzureSQLDBQuery()
+    run_sql_task.run(f"CREATE TABLE {FQN} (id INT, name VARCHAR(25))")
+    run_sql_task.run(f"INSERT INTO {FQN} VALUES (1, 'Mike')")
+    yield
+    run_sql_task.run(f"DROP TABLE {FQN}")
+
+
+def test_azure_sql_transform(TEST_TABLE):
+    flow = AzureSQLTransform(
+        name="test",
+        query=f"SELECT * FROM {FQN}",
+    )
+    result = flow.run()
+    task = list(result.result.keys())[0]
+    actual_result = result.result[task].result[0]
+    assert list(actual_result) == [1, "Mike"]
