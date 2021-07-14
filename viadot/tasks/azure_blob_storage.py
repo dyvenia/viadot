5cbdad810fe5f62d4bb3a5dde606c173919ed88d
trymzet <mzawadzki@dyvenia.com>
2021-07-14 13:43:55 +0200

    📝 Added docs

--- viadot/tasks/azure_blob_storage.py
+++ viadot/tasks/azure_blob_storage.py
@@ -2,6 +2,8 @@ from prefect import Task
 
 from ..sources import AzureBlobStorage
 
+# from ..tasks import ReadAzureKeyVaultSecret
+
 
 class BlobFromCSV(Task):
     def __init__(self, *args, **kwargs):
@@ -10,12 +12,55 @@ class BlobFromCSV(Task):
     def __call__(self):
         """Generate a blob from a local CSV file"""
 
-    def run(self, from_path: str, to_path: str, overwrite: bool = False):
+    def run(
+        self,
+        from_path: str,
+        to_path: str,
+        overwrite: bool = False,
+        sp_credentials_secret: str = None,
+        vault_name: str = None,
+    ):
+        """[summary]
+
+        Args:
+            from_path (str): [description]
+            to_path (str): [description]
+            overwrite (bool, optional): [description]. Defaults to False.
+            sp_credentials_secret (str, optional): The name of the Azure KeyVault secret containing a dictionary with
+            Service Principal credentials (TENANT_ID, CLIENT_ID, CLIENT_SECRET). Defaults to None.
+            vault_name (str, optional): The name of the vault from which to obtain the secret. Defaults to None.
+
+        Example:
+            ```python
+            from prefect import Flow
+            from prefect.tasks.secrets import PrefectSecret
+            from viadot.tasks import ReadAzureKeyVaultSecret
+
+            azure_secret_task = ReadAzureKeyVaultSecret()
+
+            with Flow(name="example") as f:
+                azure_credentials = PrefectSecret("AZURE_CREDENTIALS")
+                secret = azure_secret_task(secret="test", vault_name="my_vault_name", credentials=azure_credentials)
+            out = f.run()
+            ```
+        """
+
+        # if sp_credentials_secret:
+        #     azure_secret_task = ReadAzureKeyVaultSecret()
+        #     credentials = azure_secret_task.run(
+        #         secret=sp_credentials_secret, vault_name=vault_name
+        #     )
+        # else:
+        #     credentials = None
+        # blob_storage = AzureBlobStorage(credentials=credentials)
 
         blob_storage = AzureBlobStorage()
 
         self.logger.info(f"Copying from {from_path} to {to_path}...")
-        blob_storage.to_storage(
+
+        result = blob_storage.to_storage(
             from_path=from_path, to_path=to_path, overwrite=overwrite
         )
         self.logger.info(f"Successfully uploaded data to {to_path}.")
+
+        return result
