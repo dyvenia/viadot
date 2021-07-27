878e90ae923585b0b0e512cee8f15a83dde0f426
m-paz <mpazdzior@dyvenia.com>
2021-07-27 17:32:56 +0200

    🐛 Changed task_utils_v2 to taks_utils

--- viadot/flows/supermetrics_to_adls.py
+++ viadot/flows/supermetrics_to_adls.py
@@ -14,7 +14,7 @@ from prefect.tasks.control_flow import case
 from prefect.utilities import logging
 from prefect.backend import set_key_value, get_key_value
 
-from ..task_utils_v2 import METADATA_COLUMNS, add_ingestion_metadata_task
+from ..task_utils import METADATA_COLUMNS, add_ingestion_metadata_task
 from ..tasks import (
     AzureDataLakeUpload,
     DownloadGitHubFile,
