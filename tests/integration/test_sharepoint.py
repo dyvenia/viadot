647b1f6b557ff12cca0b8e04f944b7a6e47a3e07
Rafalz13 <rrav08@gmail.com>
2021-11-08 09:58:00 +0100

    🐛 Deleted sensitive data and added file deletion

--- tests/integration/test_sharepoint.py
+++ tests/integration/test_sharepoint.py
@@ -3,56 +3,63 @@ import os
 import pathlib
 import pandas as pd
 
-from .sources import Sharepoint
-from .tasks import SharepointToDF
-from .flows import sharepoint_to_adls as s_flow
-
+from viadot.sources import Sharepoint
+from viadot.flows import SharepointToADLS as s_flow
+from viadot.config import local_config
+from viadot.task_utils import df_get_data_types_task
 
 s = Sharepoint()
-excel_to_df_task = SharepointToDF()
 
-file_name = 'EUL Data.xlsm'
-df_v_n = pd.read_excel(file_name, sheet_name=2)
+FILE_NAME = "EUL Data.xlsm"
+s.download_file(filename=FILE_NAME)
+DF = pd.read_excel(FILE_NAME, sheet_name=2)
 
 
 def test_connection():
-    s_connect = s.check_connection()
-    status = s_connect.get('https://velux.sharepoint.com/sites/EULNORDIC/Shared%20Documents/Dashboard')
-    assert str(status) == '<Response [200]>'
+    credentials = local_config.get("SHAREPOINT")
+    site = f'https://{credentials["site"]}'
+    s_connect = s.get_connection()
+    status = s_connect.get(site)
+    assert str(status) == "<Response [200]>"
 
 
 def test_file_extension():
-    file_ext = ['.xlsm','.xlsx']
+    file_ext = [".xlsm", ".xlsx"]
     assert pathlib.Path(s.url_to_file).suffix in file_ext
 
 
 def test_file_name():
-    assert os.path.basename(s.url_to_file) == file_name
+    assert os.path.basename(s.url_to_file) == FILE_NAME
 
 
 def test_file_download():
-    s.download_file(filename = file_name)
+    s.download_file(filename=FILE_NAME)
     files = []
     for file in os.listdir():
         if os.path.isfile(os.path.join(file)):
             files.append(file)
-    assert file_name in files
+    assert FILE_NAME in files
+    os.remove(FILE_NAME)
 
 
 def test_file_to_df():
-    df_test = pd.DataFrame(data={'col1': [1, 2]})
-    assert type(df_v_n) == type(df_test)
+    df_test = pd.DataFrame(data={"col1": [1, 2]})
+    assert type(DF) == type(df_test)
 
 
 def test_get_data_types():
-    dtypes_map = s_flow.df_get_data_types_task.run(df_v_n)
-    dtypes = [v for k,v in dtypes_map.items()]
-    assert 'String' in dtypes
+    dtypes_map = df_get_data_types_task.run(DF)
+    dtypes = [v for k, v in dtypes_map.items()]
+    assert "String" in dtypes
 
 
-def test_map_dtypes_for_parquet(): 
-    dtyps_dict = s_flow.df_get_data_types_task.run(df_v_n)
-    df_map = s_flow.df_mapp_mixed_dtypes_for_parquet_task.run(df_v_n, dtyps_dict)
-    sum_df_cols = df_v_n.select_dtypes(include = ['object', 'string']).columns.value_counts().sum()
-    sum_df_map_cols = df_map.select_dtypes(include = 'string').columns.value_counts().sum()
+def test_map_dtypes_for_parquet():
+    dtyps_dict = df_get_data_types_task.run(DF)
+    df_map = s_flow.df_mapp_mixed_dtypes_for_parquet_task(DF, dtyps_dict)
+    sum_df_cols = (
+        DF.select_dtypes(include=["object", "string"]).columns.value_counts().sum()
+    )
+    sum_df_map_cols = (
+        df_map.select_dtypes(include="string").columns.value_counts().sum()
+    )
     assert sum_df_cols == sum_df_map_cols
