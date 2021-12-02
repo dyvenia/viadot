d53348e33bb69371aa186d21b325a7cdb7cf6455
AdamSulek <adam-s91@o2.pl>
2021-12-02 14:18:51 +0100

    ✅ Adding tests for c4c tasks

---
+++ tests/integration/tasks/test_cloud_for_customers.py
@@ -0,0 +1,20 @@
+from viadot.tasks import c4c_to_df, c4c_report_to_df
+from viadot.config import local_config
+
+
+def test_c4c_to_df():
+    credentials = local_config.get("CLOUD_FOR_CUSTOMERS")
+    credentials_prod = credentials["Prod"]
+    report_url = credentials_prod["server"]
+    df = c4c_to_df.run(report_url=report_url, env="Prod")
+
+    assert df.shape[1] == 28
+
+
+def test_c4c_report_to_df():
+    credentials = local_config.get("CLOUD_FOR_CUSTOMERS")
+    credentials_prod = credentials["Prod"]
+    report_url = credentials_prod["server"]
+    df = c4c_report_to_df.run(report_url=report_url, env="Prod")
+
+    assert df.shape[0] == 28879
