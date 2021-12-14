b1c382eddf63027b9fd54a01247e4a401225632c
lzuchowska <lzuchowska@dyvenia.com>
2021-12-14 13:27:56 +0100

    Revert ":sparkles: Added list handling for flow_of_flows to extract and transform; added tests"
    This reverts commit 27726043f131c18dd0480944769e656ad18d6b6d.

--- viadot/flows/flow_of_flows.py
+++ viadot/flows/flow_of_flows.py
@@ -23,16 +23,25 @@ class Pipeline(Flow):
 
     def gen_start_flow_run_task(self, flow_name: str, flow: Flow = None) -> Task:
         flow_extract_run = StartFlowRun(wait=True)
-        t = flow_extract_run.bind(
+        task = flow_extract_run.bind(
             flow_name=flow_name, project_name=self.project_name, flow=flow
         )
-        return t
+        return task
 
     def gen_flow(self) -> Flow:
-        extract_flow_runs = apply_map(
-            self.gen_start_flow_run_task, self.extract_flows_names, flow=self
-        )
-        transform_flow_runs = apply_map(
-            self.gen_start_flow_run_task, self.transform_flow_names, flow=self
-        )
-        transform_flow_runs.set_upstream(extract_flow_runs, flow=self)
+        try:
+            for extract in self.extract_flows_names:
+                extract_single_flow_run = apply_map(
+                    self.gen_start_flow_run_task, extract, flow=self
+                )
+        except PrefectException as e:
+            msg = "Error while extracting flow"
+            raise PrefectException(msg)
+        else:
+            for transform in self.transform_flow_names:
+                flow_run = StartFlowRun(wait=True)
+                transform_single_flow_run = flow_run.bind(
+                    flow_name=transform,
+                    project_name=self.project_name,
+                    flow=self,
+                )
