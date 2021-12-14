d68152f5022f60ff0b92a34530eabf22add17f34
lzuchowska <lzuchowska@dyvenia.com>
2021-12-14 13:28:10 +0100

    Revert "✨ Added flow list handling for flow_of_flows"
    This reverts commit efd0f88c4aa102dc2ef739196123cfd96411b95a.

--- viadot/flows/flow_of_flows.py
+++ viadot/flows/flow_of_flows.py
@@ -2,7 +2,9 @@ from typing import Any, Dict, List
 
 from prefect import Flow, Task, apply_map
 from prefect.tasks.prefect import StartFlowRun
-from prefect.exceptions import PrefectException
+
+start_flow_run_task = StartFlowRun(wait=True)
+start_flow_run_task_2 = StartFlowRun(wait=True)
 
 
 class Pipeline(Flow):
@@ -11,37 +13,29 @@ class Pipeline(Flow):
         name: str,
         project_name: str,
         extract_flows_names: List[str],
-        transform_flow_names: List[str],
+        transform_flow_name: str,
         *args: List[any],
         **kwargs: Dict[str, Any]
     ):
         self.extract_flows_names = extract_flows_names
-        self.transform_flow_names = transform_flow_names
+        self.transform_flow_name = transform_flow_name
         self.project_name = project_name
         super().__init__(*args, name=name, **kwargs)
         self.gen_flow()
 
     def gen_start_flow_run_task(self, flow_name: str, flow: Flow = None) -> Task:
-        flow_extract_run = StartFlowRun(wait=True)
-        task = flow_extract_run.bind(
+        t = start_flow_run_task.bind(
             flow_name=flow_name, project_name=self.project_name, flow=flow
         )
-        return task
+        return t
 
     def gen_flow(self) -> Flow:
-        try:
-            for extract in self.extract_flows_names:
-                extract_single_flow_run = apply_map(
-                    self.gen_start_flow_run_task, extract, flow=self
-                )
-        except PrefectException as e:
-            msg = "Error while extracting flow"
-            raise PrefectException(msg)
-        else:
-            for transform in self.transform_flow_names:
-                flow_run = StartFlowRun(wait=True)
-                transform_single_flow_run = flow_run.bind(
-                    flow_name=transform,
-                    project_name=self.project_name,
-                    flow=self,
-                )
+        extract_flow_runs = apply_map(
+            self.gen_start_flow_run_task, self.extract_flows_names, flow=self
+        )
+        transform_flow_run = start_flow_run_task_2.bind(
+            flow_name=self.transform_flow_name,
+            project_name=self.project_name,
+            flow=self,
+        )
+        transform_flow_run.set_upstream(extract_flow_runs, flow=self)
