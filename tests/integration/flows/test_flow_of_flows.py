35173d994cf5142862c0ec6c209dc45e88a044b3
Michał Zawadzki <mzawadzki@dyvenia.com>
2022-01-04 13:56:03 +0100

    ✏️ Fixed a typo

--- tests/integration/flows/test_flow_of_flows.py
+++ tests/integration/flows/test_flow_of_flows.py
@@ -9,11 +9,11 @@ def test_flow_of_flows():
         "1-raw Linkedin ads Global extract",
         "1-raw Facebook ads Global extract test append",
     ]
-    transform_flow_names = ["2-conformed Linkedin ads", "2-conformed Hubspot"]
+    transform_flows_names = ["2-conformed Linkedin ads", "2-conformed Hubspot"]
     flow = Pipeline(
         name="Trigger test lists",
         extract_flows_names=extract_flows_names,
-        transform_flow_names=transform_flow_names,
+        transform_flows_names=transform_flows_names,
         project_name="Marketing KPI",
     )
 
