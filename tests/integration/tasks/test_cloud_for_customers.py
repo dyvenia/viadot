39091c9301cdd0dd335164a04a79eb0a535b1ae7
AdamSulek <adam-s91@o2.pl>
2022-01-11 17:22:37 +0100

    ✅ Repairing tests case scenario

--- tests/integration/tasks/test_cloud_for_customers.py
+++ tests/integration/tasks/test_cloud_for_customers.py
@@ -3,14 +3,13 @@ from viadot.config import local_config
 
 
 def test_c4c_to_df():
-    credentials = local_config.get("CLOUD_FOR_CUSTOMERS")
-    credentials_prod = credentials["Prod"]
-    report_url = credentials_prod["server"]
+    url = "http://services.odata.org/V2/Northwind/Northwind.svc/"
+    endpoint = "Employees"
     c4c_to_df = C4CToDF()
-    df = c4c_to_df.run(report_url=report_url, env="Prod")
+    df = c4c_to_df.run(url=url, endpoint=endpoint)
     answer = df.head()
 
-    assert answer.shape[1] == 28
+    assert answer.shape[1] == 23
 
 
 def test_c4c_report_to_df():
