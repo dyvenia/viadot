fceb1cef34be6999f1cdbee543fbb2b7bd8ddd2a
Rafalz13 <rrav08@gmail.com>
2022-01-18 15:34:42 +0100

    ✅ Added test to ChangeColumnOrder task with append option

--- tests/integration/tasks/test_azure_sql.py
+++ tests/integration/tasks/test_azure_sql.py
@@ -1,6 +1,9 @@
 import logging
+import pandas as pd
+import pytest
+from viadot.exceptions import ValidationError
 
-from viadot.tasks import AzureSQLCreateTable, AzureSQLDBQuery
+from viadot.tasks import AzureSQLCreateTable, AzureSQLDBQuery, ChangeColumnOrder
 
 logger = logging.getLogger(__name__)
 
@@ -63,3 +66,25 @@ def test_azure_sql_run_drop_query():
     """
     exists = bool(sql_query_task.run(list_table_info_query))
     assert not exists
+
+
+def test_change_column_order_check_append(caplog):
+    create_table_task = AzureSQLCreateTable()
+    with caplog.at_level(logging.INFO):
+        create_table_task.run(
+            schema=SCHEMA,
+            table=TABLE,
+            dtypes={"id": "INT", "name": "VARCHAR(25)", "street": "VARCHAR(25)"},
+            if_exists="replace",
+        )
+    assert "Successfully created table sandbox" in caplog.text
+
+    data = {"id": [1], "street": ["Green"], "name": ["Tom"]}
+    df = pd.DataFrame(data)
+
+    check_column_order = ChangeColumnOrder()
+    with pytest.raises(
+        ValidationError,
+        match=r"The columns differ in the SQL table and the file being loaded.",
+    ):
+        check_column_order.run(table=TABLE, if_exists="append", df=df)
