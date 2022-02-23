5f6a938e7961ca57ac87727d94d7c9fb35f955f6
Rafalz13 <rrav08@gmail.com>
2022-02-23 10:18:08 +0100

    ✅ Added tests for GetFlowLastSuccessfulRun Task

--- tests/integration/tasks/test_prefect.py
+++ tests/integration/tasks/test_prefect.py
@@ -8,6 +8,34 @@ from viadot.tasks import GetFlowLastSuccessfulRun
 PREFECT_TASK = GetFlowLastSuccessfulRun()
 DATE_FROM_PREFECT = "2022-01-01T01:30:00+00:00"
 DATE_FROM_PREFECT2 = "2022-01-04T02:20:00+00:00"
+PREFECT_JSON = {
+    "data": {
+        "flow": [
+            {
+                "flow_runs": [
+                    {
+                        "id": "88a505ec-f7c9-4242-baa4-81c9db1aaafe",
+                        "start_time": "2022-02-21T01:04:44.146191+00:00",
+                        "state": "Failed",
+                        "scheduled_start_time": "2022-02-21T01:00:00+00:00",
+                    },
+                    {
+                        "id": "9d310a47-2fb0-4435-971c-4fcaf3efca06",
+                        "start_time": "2022-02-20T01:05:36.142547+00:00",
+                        "state": "Success",
+                        "scheduled_start_time": "2022-02-20T01:00:00+00:00",
+                    },
+                    {
+                        "id": "90c32b13-4a0a-467b-a07f-c568a34f1bc2",
+                        "start_time": "2022-02-19T01:05:30.572431+00:00",
+                        "state": "Failed",
+                        "scheduled_start_time": "2022-02-19T01:00:00+00:00",
+                    },
+                ]
+            }
+        ]
+    }
+}
 
 
 def test_get_formatted_dated():
@@ -39,7 +67,7 @@ def test_calculate_difference_time():
         base_date=DATE_FROM_PREFECT,
         diff_type="time",
     )
-    assert diff_hours == 1
+    assert diff_hours == 0
 
     diff_hours = PREFECT_TASK.calculate_difference(
         date_to_compare="2022-01-04T02:50:00+00:00",
@@ -47,3 +75,23 @@ def test_calculate_difference_time():
         diff_type="time",
     )
     assert diff_hours == 1.20
+
+
+def test_get_time_from_last_successful_run():
+    flow_runs = PREFECT_JSON["data"]["flow"][0]["flow_runs"]
+    start_time_success = PREFECT_TASK.get_time_from_last_successful_run(
+        flow_runs_details=flow_runs
+    )
+    assert start_time_success == "2022-02-20T01:05:36.142547+00:00"
+
+
+def test_check_if_scheduled_run():
+    is_scheduled = PREFECT_TASK.check_if_scheduled_run(
+        time_run="2022-02-21T01:40:00+00:00", time_schedule="2022-02-15T01:00:00+00:00"
+    )
+    assert is_scheduled is True
+
+    is_scheduled = PREFECT_TASK.check_if_scheduled_run(
+        time_run="2022-02-21T02:20:00+00:00", time_schedule="2022-02-15T01:00:00+00:00"
+    )
+    assert is_scheduled is False
