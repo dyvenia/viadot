43ace93b8aca25f05413deae015437ec4fba8a75
winiar93 <mwiniarski@dyvenia.com>
2022-02-23 16:25:44 +0100

    ✅ added super crazy test

--- tests/unit/test_task_utils.py
+++ tests/unit/test_task_utils.py
@@ -1,8 +1,12 @@
 import pytest
 import pandas as pd
 from typing import List
-
-from viadot.task_utils import df_get_data_types_task, df_map_mixed_dtypes_for_parquet
+import numpy as np
+from viadot.task_utils import (
+    df_get_data_types_task,
+    df_map_mixed_dtypes_for_parquet,
+    df_converts_bytes_to_int,
+)
 
 
 def count_dtypes(dtypes_dict: dict = None, dtypes_to_count: List[str] = None) -> int:
@@ -32,3 +36,30 @@ def test_map_dtypes_for_parquet():
     sum_of_mapped_dtypes = count_dtypes(dtyps_dict_mapped, ["String"])
 
     assert sum_of_dtypes == sum_of_mapped_dtypes
+
+
+def test_df_converts_bytes_to_int():
+    dane = {
+        "ID": {0: 1, 1: 2, 2: 100, 3: 101, 4: 102},
+        "SpracheText": {
+            0: "TE_CATALOG_BASE_LANG",
+            1: "TE_Docu",
+            2: "TE_German",
+            3: "TE_English",
+            4: "TE_French",
+        },
+        "RKZ": {
+            0: b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\r\x01\x00\x00\x00\x00\x00\x00\x04\x00\x00q\x9f#NV\x8dG\x00",
+            1: b"\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\r\x01\x00\x00\x00\x00\x00\x00\x04\x00\x00<\xa0#NV\x8dG\x00",
+            2: b"\r\xa3\x86\x01\x00\x01\x00\x00\x00\x00\x04\r\x01\x00\x00\x00\x00\x00\x00\x04\x00\x003\x9f#NV\x8dG\x00",
+            3: b"\r\xa3\x86\x01\x00\x01\x00\x00\x00\x00\x04\r\x01\x00\x00\x00\x00\x00\x00\x04\x00\x00R\x9f#NV\x8dG\x00",
+            4: b"\r\xa3\x86\x01\x00\x01\x00\x00\x00\x00\x04\r\x01\x00\x00\x00\x00\x00\x00\x04\x00\x00\xee\x9f#NV\x8dG\x00",
+        },
+    }
+
+    df = pd.DataFrame.from_dict(dane)
+    test_df = df_converts_bytes_to_int.run(df)
+    test_df = df_converts_bytes_to_int.run(df)
+    lst = test_df["RKZ"][0]
+    is_it_or_not = all(isinstance(x, (int, int)) for x in lst)
+    assert is_it_or_not == True
