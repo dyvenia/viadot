c3042f7d6ea489bce54e57b20ce95c9ed392101a
Angelika Tarnawa <angelikat26@gmail.com>
2022-04-12 14:03:53 +0200

    ✅ added tests

--- tests/integration/flows/test_multiple_flows.py
+++ tests/integration/flows/test_multiple_flows.py
@@ -23,3 +23,22 @@ def test_multiple_flows_not_working(caplog):
     flow = MultipleFlows(name="test", flows_list=list)
     flow.run()
     assert "One of the flows has failed!" in caplog.text
+
+
+def test_multiple_flows_working_one_flow(caplog):
+    list = [["Flow of flows 1 test", "dev"]]
+    flow = MultipleFlows(name="test", flows_list=list)
+    with caplog.at_level(logging.INFO):
+        flow.run()
+    assert "All of the tasks succeeded." in caplog.text
+
+
+def test_multiple_flows_last_not_working(caplog):
+    list = [
+        ["Flow of flows 1 test", "dev"],
+        ["Flow of flows 2 - working", "dev"],
+        ["Flow of flows 2 test - not working", "dev"],
+    ]
+    flow = MultipleFlows(name="test", flows_list=list)
+    flow.run()
+    assert "One of the flows has failed!" in caplog.text
