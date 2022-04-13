d2a1b87ff7166010b6852ff2ecf8806a6f9d61e7
Angelika Tarnawa <angelikat26@gmail.com>
2022-04-13 16:21:07 +0200

    🎨 Changed variable name

--- tests/integration/flows/test_multiple_flows.py
+++ tests/integration/flows/test_multiple_flows.py
@@ -3,42 +3,42 @@ import logging
 
 
 def test_multiple_flows_working(caplog):
-    list = [
+    flow_list = [
         ["Flow of flows 1 test", "dev"],
         ["Flow of flows 2 - working", "dev"],
         ["Flow of flows 3", "dev"],
     ]
-    flow = MultipleFlows(name="test", flows_list=list)
+    flow = MultipleFlows(name="test", flows_list=flow_list)
     with caplog.at_level(logging.INFO):
         flow.run()
     assert "All of the tasks succeeded." in caplog.text
 
 
 def test_multiple_flows_not_working(caplog):
-    list = [
+    flow_list = [
         ["Flow of flows 1 test", "dev"],
         ["Flow of flows 2 test - not working", "dev"],
         ["Flow of flows 3", "dev"],
     ]
-    flow = MultipleFlows(name="test", flows_list=list)
+    flow = MultipleFlows(name="test", flows_list=flow_list)
     flow.run()
     assert "One of the flows has failed!" in caplog.text
 
 
 def test_multiple_flows_working_one_flow(caplog):
-    list = [["Flow of flows 1 test", "dev"]]
-    flow = MultipleFlows(name="test", flows_list=list)
+    flow_list = [["Flow of flows 1 test", "dev"]]
+    flow = MultipleFlows(name="test", flows_list=flow_list)
     with caplog.at_level(logging.INFO):
         flow.run()
     assert "All of the tasks succeeded." in caplog.text
 
 
 def test_multiple_flows_last_not_working(caplog):
-    list = [
+    flow_list = [
         ["Flow of flows 1 test", "dev"],
         ["Flow of flows 2 - working", "dev"],
         ["Flow of flows 2 test - not working", "dev"],
     ]
-    flow = MultipleFlows(name="test", flows_list=list)
+    flow = MultipleFlows(name="test", flows_list=flow_list)
     flow.run()
     assert "One of the flows has failed!" in caplog.text
