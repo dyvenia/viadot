47fcf4fd926b1b8edd165916c675cd6c8801850e
mwieloch <mwieloch@dyvenia.com>
2022-04-21 15:38:36 +0200

    added init

--- viadot/flows/__init__.py
+++ viadot/flows/__init__.py
@@ -19,3 +19,4 @@ except ImportError:
 from .duckdb_transform import DuckDBTransform
 from .duckdb_to_sql_server import DuckDBToSQLServer
 from .multiple_flows import MultipleFlows
+from .prefect_logs import PrefectLogs
