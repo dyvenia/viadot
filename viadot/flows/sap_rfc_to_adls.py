99eadacaa72c558ce72f2a13b9c292b93d1fd98a
Angelika Tarnawa <angelikat26@gmail.com>
2022-04-26 14:07:00 +0200

    📝 Added docstrings

--- viadot/flows/sap_rfc_to_adls.py
+++ viadot/flows/sap_rfc_to_adls.py
@@ -13,17 +13,25 @@ file_to_adls_task = AzureDataLakeUpload()
 
 @task
 def concat_dfs(dfs: List[pd.DataFrame]):
-    output_df = pd.DataFrame()
+    """
+    Task to combine list of data frames into one
+
+    Args:
+        dfs (List[pd.DataFrame]): List of dataframes to concat.
+    Returns:
+        full_df (pd.DataFrame()): Pandas dataframe containing all columns from dataframes from list.
+    """
+    full_df = pd.DataFrame()
     for i in range(len(dfs) - 1):
-        output_df = pd.concat([output_df, dfs[i]], axis=1)
-    return output_df
+        full_df = pd.concat([full_df, dfs[i]], axis=1)
+    return full_df
 
 
 class SAPRFCToADLS(Flow):
     def __init__(
         self,
         name: str,
-        query_list: List[str] = None,
+        query_list: List[str],
         sep: str = None,
         func: str = "BBP_RFC_READ_TABLE",
         sap_credentials: dict = None,
@@ -34,11 +42,38 @@ class SAPRFCToADLS(Flow):
         overwrite: bool = False,
         sp_credentials_secret: str = None,
         vault_name: str = None,
-        gen: int = 2,
         *args: List[any],
         **kwargs: Dict[str, Any],
     ):
-        """ """
+        """
+        Flow for downloading data from SAP DataBase using the RFC protocol.
+
+        Note that only a very limited subset of SQL is supported:
+            - aliases
+            - where clauses combined using the AND operator
+            - limit & offset
+
+        Unsupported:
+            - aggregations
+            - joins
+            - subqueries
+            - etc.
+
+        Args:
+            name (str): The name of the flow.
+            query_list(List[str]) The list of queries to be executed with pyRFC.
+            sep(str, optional): Which separator to use when querying SAP. If not provided, multiple options are automatically tried.
+            func (str, optional): SAP RFC function to use. Defaults to "BBP_RFC_READ_TABLE".
+            credentials (dict, optional): The credentials to use to authenticate with SAP. By default, they're taken from the local viadot config.
+            local_file_path (str, optional): Local destination path. Defaults to None.
+            file_sep(str, optional): The separator to use in the CSV. Defaults to "\t".
+            if_exists (Literal["append", "replace", "skip"], optional): What to do if the table exists. Defaults to "replace".
+            adls_path(str, optional): Azure Data Lake destination file path. Defaults to None.
+            overwrite(bool, optional) Whether to overwrite the file in ADLS. Defaults to False.
+            sp_credentials_secret(str, optional): The name of the Azure Key Vault secret containing a dictionary with ACCOUNT_NAME and Service Principal
+            credentials (TENANT_ID, CLIENT_ID, CLIENT_SECRET) for the Azure Data Lake.Defaults to None.
+            vault_name(str, optional): The name of the vault from which to obtain the secrets. Defaults to None.
+        """
         self.query_list = query_list
         self.sep = sep
         self.func = func
@@ -50,7 +85,6 @@ class SAPRFCToADLS(Flow):
         self.overwrite = overwrite
         self.sp_credentials_secret = sp_credentials_secret
         self.vault_name = vault_name
-        self.gen = gen
 
         super().__init__(*args, name=name, **kwargs)
 
@@ -78,7 +112,6 @@ class SAPRFCToADLS(Flow):
             to_path=self.adls_path,
             overwrite=self.overwrite,
             sp_credentials_secret=self.sp_credentials_secret,
-            gen=self.gen,
             flow=self,
         )
         df_full.set_upstream(df, flow=self)
