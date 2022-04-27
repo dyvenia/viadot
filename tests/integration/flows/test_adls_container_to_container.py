2226c2106caac63ec637c84c299695ac1df3059e
Angelika Tarnawa <angelikat26@gmail.com>
2022-04-27 10:37:09 +0200

    🎨 Removed space

--- tests/integration/flows/test_adls_container_to_container.py
+++ tests/integration/flows/test_adls_container_to_container.py
@@ -7,7 +7,7 @@ TEST_FILE_BLOB_PATH2 = "operations/supermetrics/mp/test.csv"
 
 def test_adls_container_to_container():
     flow = ADLSContainerToContainer(
-        name="test  to container",
+        name="test to container",
         from_path=TEST_FILE_BLOB_PATH,
         to_path=TEST_FILE_BLOB_PATH2,
     )
