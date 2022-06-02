8b8f0ed966bf9ee5644b234a089b9a70deb2825b
AdamSulek <48438721+AdamSulek@users.noreply.github.com>
2022-06-02 09:48:35 +0200

    ✅ Adding env var in test

--- tests/integration/test_outlook.py
+++ tests/integration/test_outlook.py
@@ -1,9 +1,11 @@
 from viadot.sources import Outlook
+from viadot.config import local_config
 
 
 def test_outlook_to_df():
+    outlook_env_vars = local_config.get("OUTLOOK")
     outlook = Outlook(
-        mailbox_name="bulgaria@velux.com",
+        mailbox_name=outlook_env_vars["mail_example"],
         start_date="2022-04-28",
         end_date="2022-04-29",
     )
