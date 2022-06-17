17ce7984fb3082dc8f4f31300111345e2a87b6fd
Angelika Tarnawa <angelikat26@gmail.com>
2022-06-17 09:26:32 +0200

    ✅ Added test for creating a table

--- tests/unit/tasks/test_duckdb.py
+++ tests/unit/tasks/test_duckdb.py
@@ -27,3 +27,11 @@ def test_create_table_empty_file(duckdb):
 
     assert duckdb._check_if_table_exists(TABLE, schema=SCHEMA) == False
     os.remove(path)
+
+
+def test_create_table(duckdb, TEST_PARQUET_FILE_PATH):
+    duckdb_creds = {f"database": DATABASE_PATH}
+    task = DuckDBCreateTableFromParquet(credentials=duckdb_creds)
+    task.run(schema=SCHEMA, table=TABLE, path=TEST_PARQUET_FILE_PATH, if_empty="skip")
+
+    assert duckdb._check_if_table_exists(TABLE, schema=SCHEMA)
