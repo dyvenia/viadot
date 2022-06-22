b82fc3ab0ad07b865a6a28704a58ca9111f38395
Angelika Tarnawa <angelikat26@gmail.com>
2022-06-22 13:39:22 +0200

    ✅ Updated check_if_empty tests

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -6,6 +6,9 @@ import os
 from viadot.utils import gen_bulk_insert_query_from_df, check_if_empty_file
 from viadot.signals import SKIP
 
+EMPTY_CSV_PATH = "empty.csv"
+EMPTY_PARQUET_PATH = "empty.parquet"
+
 
 def test_single_quotes_inside():
     TEST_VALUE = "a'b"
@@ -73,31 +76,79 @@ VALUES ({TEST_VALUE_ESCAPED}, 'c')"""
     ), test_insert_query
 
 
-def test_check_if_empty_file(caplog):
-    with open("empty.csv", "w"):
+def test_check_if_empty_file_csv(caplog):
+    with open(EMPTY_CSV_PATH, "w"):
         pass
 
     with caplog.at_level(logging.WARNING):
-        check_if_empty_file(path="empty.csv", if_empty="warn", file_extension=".csv")
-        assert "The input file is empty." in caplog.text
+        check_if_empty_file(path=EMPTY_CSV_PATH, if_empty="warn", file_extension=".csv")
+        assert f"Input file - '{EMPTY_CSV_PATH}' is empty." in caplog.text
     with pytest.raises(ValueError):
-        check_if_empty_file(path="empty.csv", if_empty="fail", file_extension=".csv")
+        check_if_empty_file(path=EMPTY_CSV_PATH, if_empty="fail", file_extension=".csv")
     with pytest.raises(SKIP):
-        check_if_empty_file(path="empty.csv", if_empty="skip", file_extension=".csv")
+        check_if_empty_file(path=EMPTY_CSV_PATH, if_empty="skip", file_extension=".csv")
 
-    os.remove("empty.csv")
+    os.remove(EMPTY_CSV_PATH)
 
 
-def test_check_if_empty_file_one_column(caplog):
+def test_check_if_empty_file_one_column_csv(caplog):
     df = pd.DataFrame(columns=["_viadot_downloaded_at_utc"], index=[0])
-    df.to_csv("empty.csv", index=False)
+    df.to_csv(EMPTY_CSV_PATH, index=False)
 
     with caplog.at_level(logging.WARNING):
-        check_if_empty_file(path="empty.csv", if_empty="warn", file_extension=".csv")
-        assert "The input file is empty." in caplog.text
+        check_if_empty_file(path=EMPTY_CSV_PATH, if_empty="warn", file_extension=".csv")
+        assert (
+            f"Input file - '{EMPTY_CSV_PATH}' has only one column '_viadot_downloaded_at_utc'."
+            in caplog.text
+        )
     with pytest.raises(ValueError):
-        check_if_empty_file(path="empty.csv", if_empty="fail", file_extension=".csv")
+        check_if_empty_file(path=EMPTY_CSV_PATH, if_empty="fail", file_extension=".csv")
     with pytest.raises(SKIP):
-        check_if_empty_file(path="empty.csv", if_empty="skip", file_extension=".csv")
+        check_if_empty_file(path=EMPTY_CSV_PATH, if_empty="skip", file_extension=".csv")
 
-    os.remove("empty.csv")
+    os.remove(EMPTY_CSV_PATH)
+
+
+def test_check_if_empty_file_parquet(caplog):
+    with open(EMPTY_PARQUET_PATH, "w"):
+        pass
+
+    with caplog.at_level(logging.WARNING):
+        check_if_empty_file(
+            path=EMPTY_PARQUET_PATH, if_empty="warn", file_extension=".parquet"
+        )
+        assert f"Input file - '{EMPTY_PARQUET_PATH}' is empty." in caplog.text
+    with pytest.raises(ValueError):
+        check_if_empty_file(
+            path=EMPTY_PARQUET_PATH, if_empty="fail", file_extension=".parquet"
+        )
+    with pytest.raises(SKIP):
+        check_if_empty_file(
+            path=EMPTY_PARQUET_PATH, if_empty="skip", file_extension=".parquet"
+        )
+
+    os.remove(EMPTY_PARQUET_PATH)
+
+
+def test_check_if_empty_file_one_column_parquet(caplog):
+    df = pd.DataFrame(columns=["_viadot_downloaded_at_utc"], index=[0])
+    df.to_parquet(EMPTY_PARQUET_PATH, index=False)
+
+    with caplog.at_level(logging.WARNING):
+        check_if_empty_file(
+            path=EMPTY_PARQUET_PATH, if_empty="warn", file_extension=".parquet"
+        )
+        assert (
+            f"Input file - '{EMPTY_PARQUET_PATH}' has only one column '_viadot_downloaded_at_utc'."
+            in caplog.text
+        )
+    with pytest.raises(ValueError):
+        check_if_empty_file(
+            path=EMPTY_PARQUET_PATH, if_empty="fail", file_extension=".parquet"
+        )
+    with pytest.raises(SKIP):
+        check_if_empty_file(
+            path=EMPTY_PARQUET_PATH, if_empty="skip", file_extension=".parquet"
+        )
+
+    os.remove(EMPTY_PARQUET_PATH)
