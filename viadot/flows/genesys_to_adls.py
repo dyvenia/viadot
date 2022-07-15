43d9d7fea941e3bc26ebbbc91062750ecad59e6e
Mike <mwiniarski@dyvenia.com>
2022-07-15 10:24:28 +0200

    ⚡️ Adding flow class method

--- viadot/flows/genesys_to_adls.py
+++ viadot/flows/genesys_to_adls.py
@@ -1,6 +1,6 @@
 from typing import Any, Dict, List, Literal
 
-from prefect import Flow
+from prefect import Flow, Task
 
 from viadot.task_utils import df_to_csv
 from viadot.tasks import AzureDataLakeUpload
@@ -14,6 +14,7 @@ from ..task_utils import (
 )
 
 genesys_report = GenesysToDF()
+file_to_adls_task = AzureDataLakeUpload()
 
 
 class GenesysToADLS(Flow):
@@ -22,13 +23,13 @@ class GenesysToADLS(Flow):
         name: str,
         columns: List[str] = None,
         vault_name: str = None,
-        file_path: str = None,
+        local_file_path: str = None,
         output_file_extension: str = ".csv",
         sep: str = "\t",
         to_path: str = None,
         if_exists: Literal["replace", "append", "delete"] = "replace",
         overwrite_adls: bool = True,
-        sp_credentials_secret: str = None,
+        adls_sp_credentials_secret: str = None,
         credentials_secret: str = None,
         *args: List[any],
         **kwargs: Dict[str, Any]
@@ -37,17 +38,50 @@ class GenesysToADLS(Flow):
         self.name = name
         self.columns = columns
         self.vault_name = vault_name
-        self.file_path = file_path
+        self.local_file_path = local_file_path
         self.output_file_extension = output_file_extension
         self.sep = sep
         self.to_path = to_path
         self.if_exists = if_exists
         self.overwrite_adls = overwrite_adls
-        self.sp_credentials_secret = sp_credentials_secret
+        self.adls_sp_credentials_secret = adls_sp_credentials_secret
         self.credentials_secret = credentials_secret
+        self.if_exsists = if_exists
 
         super().__init__(*args, name=name, **kwargs)
 
+    def gen_genesys_df(self) -> Task:
+
+        return
+
     def gen_flow(self) -> Flow:
 
-        df = genesys_report.to_df()
+        df = genesys_report.run()
+        df_with_metadata = add_ingestion_metadata_task.bind(df, flow=self)
+        # print(df_with_metadata)
+        if self.output_file_extension == ".parquet":
+            df_to_file = df_to_parquet.bind(
+                df=df_with_metadata,
+                path=self.local_file_path,
+                if_exists=self.if_exsists,
+                flow=self,
+            )
+        else:
+            df_to_file = df_to_csv.bind(
+                df=df_with_metadata,
+                path=self.local_file_path,
+                if_exists=self.if_exsists,
+                flow=self,
+            )
+
+        # file_to_adls_task.bind(
+        #     from_path=self.local_file_path,
+        #     to_path=self.adls_file_path,
+        #     overwrite=self.overwrite_adls,
+        #     sp_credentials_secret=self.adls_sp_credentials_secret,
+        #     flow=self,
+        # )
+
+        df_with_metadata.set_upstream(df, flow=self)
+        df_to_file.set_upstream(df_with_metadata, flow=self)
+        # file_to_adls_task.set_upstream(df_to_file, flow=self)
