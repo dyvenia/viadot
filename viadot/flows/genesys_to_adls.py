9f30aca221040026c657482a2e8cdc4781d28dcf
Mike <mwiniarski@dyvenia.com>
2022-07-15 11:43:39 +0200

    ⚡️  Added forking flow

--- viadot/flows/genesys_to_adls.py
+++ viadot/flows/genesys_to_adls.py
@@ -57,7 +57,6 @@ class GenesysToADLS(Flow):
     def gen_flow(self) -> Flow:
 
         df = genesys_report.bind(flow=self)
-        print(df)
         df_with_metadata = add_ingestion_metadata_task.bind(df, flow=self)
 
         if self.output_file_extension == ".parquet":
