ba8d4ffee26d67b41d013484547c4f0f4a056873
AdamSulek <48438721+AdamSulek@users.noreply.github.com>
2022-07-15 12:01:25 +0200

    📝 Adding docs to genesys flow

--- viadot/flows/genesys_to_adls.py
+++ viadot/flows/genesys_to_adls.py
@@ -35,6 +35,24 @@ class GenesysToADLS(Flow):
         *args: List[any],
         **kwargs: Dict[str, Any]
     ):
+        """Flow for downloading data from genesys API to Azure Data Lake in csv format by default.
+
+        Args:
+            name (str): The name of the flow.
+            columns (List[str], optional): The report fields. Defaults to None.
+            vault_name (str, optional): The name of the vault from which to obtain the secrets. Defaults to None.
+            local_file_path (str, optional): Local destination path. Defaults to None.
+            output_file_extension (str, optional): Output file extension - to allow selection of.csv for data
+            which is not easy to handle with parquet. Defaults to ".csv".
+            sep (str, optional): The separator used in csv. Defaults to "\t".
+            adls_file_path (str, optional): Azure Data Lake destination file path. Defaults to None.
+            if_exists (str, optional): What to do if the file exists. Defaults to "replace".
+            overwrite_adls (bool, optional): Whether to overwrite files in the lake. Defaults to True.
+            adls_sp_credentials_secret (str, optional): The name of the Azure Key Vault secret containing a dictionary with
+            ACCOUNT_NAME and Service Principal credentials (TENANT_ID, CLIENT_ID, CLIENT_SECRET) for the Azure Data Lake.
+            Defaults to None.
+            credentials_secret (str, optional): The name of the Azure Key Vault secret for Bigquery project. Defaults to None.
+        """
 
         self.name = name
         self.columns = columns
@@ -42,7 +60,6 @@ class GenesysToADLS(Flow):
         self.local_file_path = local_file_path
         self.output_file_extension = output_file_extension
         self.sep = sep
-        self.to_path = to_path
         self.if_exists = if_exists
         self.adls_file_path = adls_file_path
         self.overwrite_adls = overwrite_adls
@@ -57,7 +74,6 @@ class GenesysToADLS(Flow):
     def gen_flow(self) -> Flow:
 
         df = genesys_report.bind(flow=self)
-        print(df)
         df_with_metadata = add_ingestion_metadata_task.bind(df, flow=self)
 
         if self.output_file_extension == ".parquet":
