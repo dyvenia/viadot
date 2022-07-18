35336329a44b30ac93d4ae451dd2bbc629c25922
mwieloch <mwieloch@dyvenia.com>
2022-07-18 17:28:54 +0200

    applied changes

--- viadot/flows/prefect_logs.py
+++ viadot/flows/prefect_logs.py
@@ -1,3 +1,4 @@
+from sqlite3 import Timestamp
 from typing import Literal
 import prefect
 import pandas as pd
@@ -36,7 +37,7 @@ class PrefectLogs(Flow):
             filter_type (Literal, optional): A comparison operator passed to the Prefect API query (_gte >=, _lte <=) that refers to the left or right boundary of date ranges
                 for which flow extracts data. Defaults to _gte.
             local_file_path (str, optional): Local destination path. Defaults to None.
-            adls_path (str, optional): Azure Data Lake destination folder/catalog path. Defaults to None.
+            adls_path (str, optional): Azure Data Lake destination catalog or file path. Defaults to None.
             adls_sp_credentials_secret (str, optional): The name of the Azure Key Vault secret containing a dictionary with
                 ACCOUNT_NAME and Service Principal credentials (TENANT_ID, CLIENT_ID, CLIENT_SECRET) for the Azure Data Lake.
                 Defaults to None.
@@ -61,22 +62,34 @@ class PrefectLogs(Flow):
             self.scheduled_start_time = scheduled_start_time
 
         super().__init__(
-            name="prefect_extract_flows_logs",
+            name="prefect_extract_flow_logs",
             *args,
             **kwargs,
         )
 
         self.gen_flow()
 
-    def get_formatted_date_from_timestamp(self, value) -> str:
+    def get_formatted_value_from_timestamp(self, value_type, value) -> str:
         """
-        Function returns cleaned date from timestamp in string format (ex. "2022-01-01")
+        Function returns cleaned date/time from timestamp in string format (ex. "2022-01-01")
         Args:
+            type (str):  Desired part of the Timestamp value, "date" or "time".
             value (timestamp): Timestamp value from Prefect.
+        Return: (str) Date or time from the Timestamp
         """
-
-        if value is not None:
+        if value is not None and value_type == "date":
             return value.split("T")[0]
+        elif value is not None and value_type == "time":
+            return (value.split("T")[1])[:5]
+
+    def check_if_run_authomatically(self, value) -> bool:
+        """
+        Function checks if flow has been run automatically by scheduler or triggered manually.
+        """
+        if value == "09720c91-a99c-4f72-b7b5-3c061c83408b":  # ID of a Prefect scheduler
+            return True
+        else:
+            return False
 
     def gen_flow(self) -> Flow:
 
@@ -128,16 +141,38 @@ class PrefectLogs(Flow):
             meta=["name", "project_id", "version"],
         )
 
-        df.insert(
-            3,
-            "start_date",
-            value=[
-                self.get_formatted_date_from_timestamp(df["start_time"][x])
-                for x in range(df.shape[0])
-            ],
-        )
+        df["start_time_date"] = [
+            self.get_formatted_value_from_timestamp("date", df["start_time"][x])
+            for x in range(df.shape[0])
+        ]
+        df["start_time_time"] = [
+            self.get_formatted_value_from_timestamp("time", df["start_time"][x])
+            for x in range(df.shape[0])
+        ]
+        df["is_run_automatically"] = [
+            self.check_if_run_authomatically(df["created_by_user_id"][x])
+            for x in range(df.shape[0])
+        ]
 
         df["project_name"] = df["project_id"].map(projects_details)
+        df = df.loc[
+            :,
+            [
+                "id",
+                "name",
+                "version",
+                "scheduled_start_time",
+                "start_time",
+                "start_time_date",
+                "start_time_time",
+                "end_time",
+                "state",
+                "created_by_user_id",
+                "is_run_automatically",
+                "project_id",
+                "project_name",
+            ],
+        ]
 
         df = add_ingestion_metadata_task.run(df)
 
