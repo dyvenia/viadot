a9cf197dd5cc3427499d4e9dd44e9d2fa610e3c4
Mike <mwiniarski@dyvenia.com>
2022-07-22 15:26:11 +0200

    ✅ Added Diego tests

--- tests/integration/test_genesys.py
+++ tests/integration/test_genesys.py
@@ -1,9 +1,39 @@
+import os
 import pytest
 import pandas as pd
 from typing import List
+from unittest import mock
+
 from viadot.sources import Genesys
 from viadot.tasks import GenesysToDF
-from unittest import mock
+
+
+@pytest.fixture
+def var_dictionary():
+    variables = {
+        "id": "9fb3a99e-aa5b-438b-9047-f4d7fe6d4ff3",
+        "data_to_post": {
+            "name": "Schedule report job for test",
+            "quartzCronExpression": "0 15 * * * ?",
+            "description": "Export Test",
+            "timeZone": "Europe/Warsaw",
+            "timePeriod": "YESTERDAY",
+            "interval": "2022-07-10T22:00:00.000Z/2022-07-11T22:00:00.000Z",
+            "reportFormat": "XLS",
+            "locale": "en_US",
+            "enabled": True,
+            "reportId": "03bc1eef-082e-46c1-b9a8-fe45fbc3b205",
+            "parameters": {
+                "queueIds": [
+                    "780807e6-83b9-44be-aff0-a41c37fab004",
+                ]
+            },
+        },
+        "url": "https://apps.mypurecloud.de/platform/api/v2/downloads/7dca3529280847af",
+        "file_name": "Genesys_Queue_Metrics_Interval_Export.xls",
+    }
+
+    return variables
 
 
 @pytest.mark.init
@@ -30,55 +60,73 @@ def test_schedule_id_param():
     assert g.schedule_id != None and type(g.schedule_id) == str
 
 
-@pytest.mark.report
-def test_report_url_param():
-    g = Genesys(schedule_id="9fb3a99e-aa5b-438b-9047-f4d7fe6d4ff3")
-    test_url = g.get_analitics_url_report
-    assert type(test_url) == str and test_url.startswith("http")
-
-
-@pytest.mark.init
-def test_report_clomuns_param():
-    g = Genesys()
-    assert g.report_columns != None and type(g.report_columns) == List
-
-
-@pytest.mark.parametrize("input_name", ["", "test_name", "12345", ".##@@"])
+@pytest.mark.parametrize("input_name", ["test_name", "12345", ".##@@"])
 @pytest.mark.init
 def test_other_inicial_params(input_name):
     g = Genesys(report_name=input_name)
     assert len(g.report_name) > 0 and type(g.report_name) == str
 
 
-@pytest.mark.get_all
+@pytest.mark.proper
+def test_connection_with_genesys_api():
+    g = Genesys()
+    test_genesys_connection = g.authorization_token
+    assert (
+        type(test_genesys_connection) == dict
+        and len(test_genesys_connection.items()) > 0
+    )
+
+
+@pytest.mark.proper
+def test_get_analitics_url_report(var_dictionary):
+    g = Genesys(schedule_id=var_dictionary["id"])
+    test_url = g.get_analitics_url_report
+    assert type(test_url) == str and test_url.startswith("http")
+
+
+@mock.patch.object(Genesys, "schedule_report")
+@pytest.mark.connection
+def test_scheduling_report(mock_api_response, var_dictionary):
+    mock_api_response.return_value = 200
+
+    g = Genesys()
+    result = g.schedule_report(data_to_post=var_dictionary["data_to_post"])
+    assert result == 200
+    mock_api_response.assert_called()
+
+
+@pytest.mark.dependency(name="url")
+@pytest.mark.url
 def test_get_all_schedules_job():
     g = Genesys()
-    assert type(g.get_all_schedules_job()) == dict
+    reports = g.get_all_schedules_job()
+    assert (
+        type(reports) == dict
+        and len(reports.keys()) > 0
+        and len(reports.get("entities")[0]["id"]) > 0
+    )
 
 
-@pytest.mark.schedule_job
-def test_schedule_rob():
-    with mock.patch("viadot.sources.genesys.Genesys.schedule_report") as mock_method:
-        mock_method.return_value = 200
-        g = Genesys()
+@pytest.mark.dependency(depends=["url"])
+@pytest.mark.url
+def test_report_to_df(var_dictionary):
+    g = Genesys(schedule_id=var_dictionary["id"])
+    df = g.to_df()
+    assert type(df) == pd.DataFrame and df.shape != (0, 0)
 
-        data_to_post = {
-            "name": "Schedule report job for test",
-            "quartzCronExpression": "0 15 * * * ?",
-            "description": "Export Test",
-            "timeZone": "Europe/Warsaw",
-            "timePeriod": "YESTERDAY",
-            "interval": "2022-07-10T22:00:00.000Z/2022-07-11T22:00:00.000Z",
-            "reportFormat": "XLS",
-            "locale": "en_US",
-            "enabled": True,
-            "reportId": "03bc1eef-082e-46c1-b9a8-fe45fbc3b205",
-            "parameters": {
-                "queueIds": [
-                    "780807e6-83b9-44be-aff0-a41c37fab004",
-                ]
-            },
-        }
 
-        status_code = g.schedule_report(data_to_post=data_to_post)
-        assert status_code == 200
+@pytest.mark.download
+def test_download_report_file(var_dictionary):
+    g = Genesys()
+    g.download_report(report_url=var_dictionary["url"])
+    assert os.path.exists(var_dictionary["file_name"]) == True
+
+    if os.path.exists(var_dictionary["file_name"]):
+        os.remove(var_dictionary["file_name"])
+
+
+@pytest.mark.task
+def test_genesys_to_df_task(var_dictionary):
+    task = GenesysToDF(schedule_id=var_dictionary["id"])
+    result = task.run()
+    assert isinstance(result, pd.DataFrame)
