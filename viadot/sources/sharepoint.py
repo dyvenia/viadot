41d7b65ed4d767fe633f0b2459a06b426a40a5a9
trymzet <michalmzawadzki@gmail.com>
2022-10-07 02:55:38 +0200

    ♻️ Simplified and improved docs

--- viadot/sources/sharepoint.py
+++ viadot/sources/sharepoint.py
@@ -1,45 +1,39 @@
-from typing import Any, Dict
+from typing import Optional
 
 import sharepy
+from pydantic import BaseModel
 
-from ..config import local_config
-from ..exceptions import CredentialError
+from ..config import DEFAULT_CONFIG
 from .base import Source
+
+
+class SharepointCredentials(BaseModel):
+    site: str  # Path to sharepoint website (e.g : {tenant_name}.sharepoint.com)
+    username: str  # Sharepoint username (e.g username@{tenant_name}.com)
+    password: str  # Sharepoint password
 
 
 class Sharepoint(Source):
     """
-    A Sharepoint class to connect and download specific Excel file from Sharepoint.
+    Download Excel files from Sharepoint.
 
     Args:
-        credentials (dict): In credentials should be included:
-            "site" - Path to sharepoint website (e.g : {tenant_name}.sharepoint.com)
-            "username" - Sharepoint username (e.g username@{tenant_name}.com)
-            "password"
-        download_from_path (str, optional): Full url to file
-                        (e.g : https://{tenant_name}.sharepoint.com/sites/{directory}/Shared%20Documents/Dashboard/file). Defaults to None.
+        credentials (SharepointCredentials): Sharepoint credentials.
+        config_key (str, optional): The key in the viadot config holding relevant credentials.
     """
 
     def __init__(
         self,
-        credentials: Dict[str, Any] = None,
-        download_from_path: str = None,
+        credentials: SharepointCredentials = None,
+        config_key: Optional[str] = None,
         *args,
         **kwargs,
     ):
-
-        DEFAULT_CREDENTIALS = local_config.get("SHAREPOINT")
-        credentials = credentials or DEFAULT_CREDENTIALS
-        if credentials is None:
-            raise CredentialError("Credentials not found.")
-        self.url = download_from_path
-        self.required_credentials = ["site", "username", "password"]
+        credentials = credentials or DEFAULT_CONFIG.get(config_key)
+        SharepointCredentials(**credentials)  # validate the credentials schema
         super().__init__(*args, credentials=credentials, **kwargs)
 
     def get_connection(self) -> sharepy.session.SharePointSession:
-        if any([rq not in self.credentials for rq in self.required_credentials]):
-            raise CredentialError("Missing credentials.")
-
         return sharepy.connect(
             site=self.credentials["site"],
             username=self.credentials["username"],
@@ -48,20 +42,25 @@ class Sharepoint(Source):
 
     def download_file(
         self,
-        download_from_path: str = None,
-        download_to_path: str = "Sharepoint_file.xlsm",
+        from_path: str,
+        to_path: str,
     ) -> None:
-        """Function to download files from Sharepoint.
-        Args:
-            download_from_path (str): Path from which to download file. Defaults to None.
-            download_to_path (str, optional): Path to destination file. Defaults to "Sharepoint_file.xlsm".
         """
-        download_from_path = download_from_path or self.url
-        if not download_from_path:
-            raise ValueError("Missing required parameter 'download_from_path'.")
+        Download a file from Sharepoint.
 
+        Args:
+            from_path (str): The URL of the file.
+            to_path (str): Where to download the file.
+
+        Example:
+            download_file(
+                from_path="https://{tenant_name}.sharepoint.com/sites/{directory}/Shared%20Documents/Dashboard/file",
+                to_path="file.xlsx"
+            )
+        """
         conn = self.get_connection()
         conn.getfile(
-            url=download_from_path,
-            filename=download_to_path,
+            url=from_path,
+            filename=to_path,
         )
+        conn.close()
