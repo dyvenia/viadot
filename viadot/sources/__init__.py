07caf16f11f7700bc1b02040dbf396a67407ee0d
trymzet <michalmzawadzki@gmail.com>
2022-10-13 14:43:00 +0200

    ♻️ Readd Databricks to __init__

--- viadot/sources/__init__.py
+++ viadot/sources/__init__.py
@@ -1,7 +1,7 @@
 from .sharepoint import Sharepoint
 
 # from .azure_data_lake import AzureDataLake
-# from .databricks import Databricks
+from .databricks import Databricks
 
 # APIS
 from .exchange_rates import ExchangeRates
