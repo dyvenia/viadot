0a839e97eab5f672028c05a367915052fa054ab0
trymzet <michalmzawadzki@gmail.com>
2022-10-19 23:42:28 +0200

    ⬆️ Update version

--- viadot/__init__.py
+++ viadot/__init__.py
@@ -1,6 +1,6 @@
 import logging
 
-__version__ = "2.0a9"
+__version__ = "2.0a10"
 
 # Remove trash Azure INFO logs which contain low-level debugging information
 # but keep WARNING and higher ones in case something actually important happens.
