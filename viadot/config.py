32063f3a3f145aa9066697eeab41afa411bf0f70
trymzet <michalmzawadzki@gmail.com>
2022-10-25 17:55:34 +0200

    🥅 Add handling of empty/incorrect viadot config file

--- viadot/config.py
+++ viadot/config.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
 
 import json
+import logging
 from os.path import expanduser, join
 from typing import Optional
 
 from yaml import safe_load
+
+logger = logging.getLogger(__name__)
 
 USER_HOME = expanduser("~")
 
@@ -27,13 +30,19 @@ class Config(dict):
             return cls(**config)
 
 
+config_dir = join(USER_HOME, ".config", "viadot")
+
 try:
-    CONFIG = Config.from_yaml(join(USER_HOME, ".config", "viadot", "config.yaml"))
+    CONFIG = Config.from_yaml(join(config_dir, "config.yaml"))
 except FileNotFoundError:
     try:
-        CONFIG = Config.from_json(join(USER_HOME, ".config", "viadot", "config.json"))
+        CONFIG = Config.from_json(join(config_dir, "config.json"))
     except FileNotFoundError:
         CONFIG = Config()
+except ValueError:
+    # Incorrect config structure.
+    logger.warning(f"Could not read default viadot config file from '{config_dir}'.")
+    CONFIG = Config()
 
 
 def get_source_config(key, config=CONFIG):
