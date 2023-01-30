4fe77382ef09f2a9d4da0fb18db1456a1f5bba75
Rafalz13 <rziemianek@dyvenia.com>
2023-01-30 14:55:38 +0100

    🐛 Fixed tests before release

--- tests/integration/flows/test_sharepoint_to_adls.py
+++ tests/integration/flows/test_sharepoint_to_adls.py
@@ -1,6 +1,6 @@
 import os
 from unittest import mock
-
+import pytest
 import pandas as pd
 import pendulum
 from prefect.tasks.secrets import PrefectSecret
@@ -11,75 +11,64 @@ from viadot.tasks import AzureDataLakeRemove
 ADLS_FILE_NAME = str(pendulum.now("utc")) + ".csv"
 ADLS_DIR_PATH = "raw/tests/"
 CREDENTIALS_SECRET = PrefectSecret("AZURE_DEFAULT_ADLS_SERVICE_PRINCIPAL_SECRET").run()
+DATA = {"country": [1, 2], "sales": [3, 4]}
 
 
-def test_sharepoint_to_adls_run_flow():
-
-    d = {"country": [1, 2], "sales": [3, 4]}
-    df = pd.DataFrame(data=d)
-
-    with mock.patch(
-        "viadot.flows.sharepoint_to_adls.excel_to_df_task.bind"
-    ) as excel_to_df_task_mock:
-        excel_to_df_task_mock.return_value = df
-
-        flow = SharepointToADLS(
-            "test_sharepoint_to_adls_run_flow",
-            output_file_extension=".csv",
-            adls_sp_credentials_secret=CREDENTIALS_SECRET,
-            adls_dir_path=ADLS_DIR_PATH,
-            adls_file_name=ADLS_FILE_NAME,
-        )
-        result = flow.run()
-        assert result.is_successful()
-        os.remove("test_sharepoint_to_adls_run_flow.csv")
-        os.remove("test_sharepoint_to_adls_run_flow.json")
+@mock.patch(
+    "viadot.tasks.SharepointToDF.run",
+    return_value=pd.DataFrame(data=DATA),
+)
+@pytest.mark.run
+def test_sharepoint_to_adls_run_flow(mocked_class):
+    flow = SharepointToADLS(
+        "test_sharepoint_to_adls_run_flow",
+        output_file_extension=".csv",
+        adls_sp_credentials_secret=CREDENTIALS_SECRET,
+        adls_dir_path=ADLS_DIR_PATH,
+        adls_file_name=ADLS_FILE_NAME,
+    )
+    result = flow.run()
+    assert result.is_successful()
+    os.remove("test_sharepoint_to_adls_run_flow.csv")
+    os.remove("test_sharepoint_to_adls_run_flow.json")
 
 
-def test_sharepoint_to_adls_run_flow_overwrite_true():
-
-    d = {"country": [1, 2], "sales": [3, 4]}
-    df = pd.DataFrame(data=d)
-
-    with mock.patch(
-        "viadot.flows.sharepoint_to_adls.excel_to_df_task.bind"
-    ) as excel_to_df_task_mock:
-        excel_to_df_task_mock.return_value = df
-
-        flow = SharepointToADLS(
-            "test_sharepoint_to_adls_run_flow_overwrite_true",
-            output_file_extension=".csv",
-            adls_sp_credentials_secret=CREDENTIALS_SECRET,
-            adls_dir_path=ADLS_DIR_PATH,
-            adls_file_name=ADLS_FILE_NAME,
-            overwrite_adls=True,
-        )
-        result = flow.run()
-        assert result.is_successful()
-        os.remove("test_sharepoint_to_adls_run_flow_overwrite_true.csv")
-        os.remove("test_sharepoint_to_adls_run_flow_overwrite_true.json")
+@mock.patch(
+    "viadot.tasks.SharepointToDF.run",
+    return_value=pd.DataFrame(data=DATA),
+)
+@pytest.mark.run
+def test_sharepoint_to_adls_run_flow_overwrite_true(mocked_class):
+    flow = SharepointToADLS(
+        "test_sharepoint_to_adls_run_flow_overwrite_true",
+        output_file_extension=".csv",
+        adls_sp_credentials_secret=CREDENTIALS_SECRET,
+        adls_dir_path=ADLS_DIR_PATH,
+        adls_file_name=ADLS_FILE_NAME,
+        overwrite_adls=True,
+    )
+    result = flow.run()
+    assert result.is_successful()
+    os.remove("test_sharepoint_to_adls_run_flow_overwrite_true.csv")
+    os.remove("test_sharepoint_to_adls_run_flow_overwrite_true.json")
 
 
-def test_sharepoint_to_adls_run_flow_overwrite_false():
+@mock.patch(
+    "viadot.tasks.SharepointToDF.run",
+    return_value=pd.DataFrame(data=DATA),
+)
+@pytest.mark.run
+def test_sharepoint_to_adls_run_flow_overwrite_false(mocked_class):
+    flow = SharepointToADLS(
+        "test_sharepoint_to_adls_run_flow_overwrite_false",
+        output_file_extension=".csv",
+        adls_sp_credentials_secret=CREDENTIALS_SECRET,
+        adls_dir_path=ADLS_DIR_PATH,
+        adls_file_name=ADLS_FILE_NAME,
+        overwrite_adls=False,
+    )
+    result = flow.run()
 
-    d = {"country": [1, 2], "sales": [3, 4]}
-    df = pd.DataFrame(data=d)
-
-    with mock.patch(
-        "viadot.flows.sharepoint_to_adls.excel_to_df_task.bind"
-    ) as excel_to_df_task_mock:
-        excel_to_df_task_mock.return_value = df
-
-        flow = SharepointToADLS(
-            "test_sharepoint_to_adls_run_flow_overwrite_false",
-            output_file_extension=".csv",
-            adls_sp_credentials_secret=CREDENTIALS_SECRET,
-            adls_dir_path=ADLS_DIR_PATH,
-            adls_file_name=ADLS_FILE_NAME,
-            overwrite_adls=False,
-        )
-        result = flow.run()
-
-        assert result.is_failed()
-        os.remove("test_sharepoint_to_adls_run_flow_overwrite_false.csv")
-        os.remove("test_sharepoint_to_adls_run_flow_overwrite_false.json")
+    assert result.is_failed()
+    os.remove("test_sharepoint_to_adls_run_flow_overwrite_false.csv")
+    os.remove("test_sharepoint_to_adls_run_flow_overwrite_false.json")
