1f4c6060c4e190e1317acfda8789e80316d5f084
AnnaGerlich <gerlichanna@gmail.com>
2023-02-14 15:55:52 +0100

    ✨ Add `test_download_file()` test for `Sharepoint` source

--- tests/unit/test_sharepoint.py
+++ tests/unit/test_sharepoint.py
@@ -1,3 +1,5 @@
+import os
+
 from viadot.sources import Sharepoint
 
 
@@ -5,3 +7,14 @@ def test_to_df(sharepoint_url):
     s = Sharepoint(config_key="sharepoint_prod")
     df = s.to_df(sharepoint_url)
     assert not df.empty
+
+
+def test_download_file(sharepoint_url):
+    s = Sharepoint(config_key="sharepoint_prod")
+
+    file = "sharepoint_test" + sharepoint_url.split(".")[-1]
+    s.download_file(url=sharepoint_url, to_path=file)
+
+    assert file in os.listdir()
+
+    os.remove(file)
