8b99f6a083e9d9f412046ce94051843de7d78033
djagoda881 <djagoda@dyvenia.com>
2023-02-21 15:47:54 +0100

    ✅ Updated test structure

--- tests/unit/test_salesforce.py
+++ tests/unit/test_salesforce.py
@@ -3,8 +3,10 @@ import pytest
 from viadot.sources import Salesforce
 
 OBJECT_NAME = "TestObject__c"
-ROW_NAME = "Test_row_1"
-UPSERTED_ROW = "Test_upser"
+ROW_NAME_1 = "Test_row_1"
+ROW_NAME_2 = "Test_row_2"
+UPSERTED_ROW_1 = "Test_upser_1"
+UPSERTED_ROW_2 = "Test_upser_2"
 
 
 @pytest.fixture(scope="session", autouse=True)
@@ -25,7 +27,8 @@ def setting_up_test_environment():
     )
 
     mdapi.CustomObject.create(custom_object)
-    sf_api.TestObject__c.create({"Name": ROW_NAME})
+    sf_api.TestObject__c.create({"Name": ROW_NAME_1})
+    sf_api.TestObject__c.create({"Name": ROW_NAME_2})
 
     yield
 
@@ -36,27 +39,74 @@ def test_to_df_selected_table(setting_up_test_environment):
 
     sf = Salesforce(config_key="sales-force")
     df = sf.to_df(table=OBJECT_NAME)
-    assert df["Name"][0] == ROW_NAME
+    print(df)
+    assert df["Name"][0] == ROW_NAME_1
+    assert df["Name"][1] == ROW_NAME_2
     assert len(df.axes[1]) == 10
 
 
 def test_to_df_selected_table_query(setting_up_test_environment):
     sf = Salesforce(config_key="sales-force")
-    df = sf.to_df(query="SELECT FIELDS(STANDARD) FROM TestObject__c")
-    assert df["Name"][0] == ROW_NAME
+    df = sf.to_df(query=f"SELECT FIELDS(STANDARD) FROM {OBJECT_NAME}")
+    assert df["Name"][0] == ROW_NAME_1
+    assert df["Name"][1] == ROW_NAME_2
     assert len(df.axes[1]) == 10
 
 
 def test_upsert_existing_row(setting_up_test_environment):
 
     sf = Salesforce(config_key="sales-force")
-    df = sf.to_df(query="SELECT Id FROM TestObject__c")
+    df = sf.to_df(query=f"SELECT Id FROM {OBJECT_NAME}")
 
-    df1 = pd.DataFrame([{"Id": df["Id"][0], "Name": UPSERTED_ROW}])
+    df1 = pd.DataFrame([{"Id": df["Id"][0], "Name": UPSERTED_ROW_1}])
     sf = Salesforce(config_key="sales-force")
     sf.upsert(df=df1, table=OBJECT_NAME)
 
-    df = sf.to_df(query="SELECT FIELDS(STANDARD) FROM TestObject__c")
+    df = sf.to_df(query=f"SELECT FIELDS(STANDARD) FROM {OBJECT_NAME}")
 
-    assert df["Name"][0] == UPSERTED_ROW
+    assert df["Name"][0] == UPSERTED_ROW_1
     assert len(df.axes[1]) == 10
+
+
+def test_bulk_upsert_existing_rows():
+
+    sf = Salesforce(config_key="sales-force")
+    df = sf.to_df(query=f"SELECT Id FROM {OBJECT_NAME}")
+
+    df1 = pd.DataFrame(
+        [
+            {"Id": df["Id"][0], "Name": UPSERTED_ROW_1},
+            {"Id": df["Id"][1], "Name": UPSERTED_ROW_2},
+        ]
+    )
+    sf = Salesforce(config_key="sales-force")
+    sf.upsert(df=df1, table=OBJECT_NAME)
+
+    df = sf.to_df(query=f"SELECT FIELDS(STANDARD) FROM {OBJECT_NAME}")
+    print(df)
+    assert df["Name"][1] == UPSERTED_ROW_2
+    assert len(df.axes[1]) == 10
+
+
+"""
+sf = Salesforce(config_key="sales-force")
+sf_api = sf.salesforce
+
+mdapi = sf_api.mdapi
+
+custom_object = mdapi.CustomObject(
+    fullName=OBJECT_NAME,
+    label="Test Object",
+    pluralLabel="Custom Objects",
+    nameField=mdapi.CustomField(label="Name", type=mdapi.FieldType("Text")),
+    deploymentStatus=mdapi.DeploymentStatus("Deployed"),
+    sharingModel=mdapi.SharingModel("ReadWrite"),
+)
+mdapi.CustomObject.delete(OBJECT_NAME)
+# mdapi.CustomObject.create(custom_object)
+# sf_api.TestObject__c.create({"Name": ROW_NAME_1})
+# sf_api.TestObject__c.create({"Name": ROW_NAME_2})
+
+# df = sf.to_df(table=OBJECT_NAME)
+# print(df)
+"""
