c3ae4823cb1168da4b841426b191219fcecba8f9
Anna Gerlich <74189459+AnnaGerlich@users.noreply.github.com>
2023-02-27 12:58:55 +0100

    ✨ Add `test_download_file()` test for `Sharepoint` source (#608)
    * 🎨 fix import format
    
    * ✨ Add `SharepointCredentials` to sources `__init__`
    
    * ✨ Add `test_download_file()` test for `Sharepoint` source
    
    * Revert "✨ Add `SharepointCredentials` to sources `__init__`"
    
    This reverts commit fb76dd7db294c1094a81c561db86573c76eeb137.
    
    * ✨ Added support for sharepoint_config_key

--- tests/unit/test_sharepoint.py
+++ tests/unit/test_sharepoint.py
@@ -1,7 +1,20 @@
+import os
+
 from viadot.sources import Sharepoint
 
 
-def test_to_df(sharepoint_url):
-    s = Sharepoint(config_key="sharepoint_prod")
+def test_to_df(sharepoint_url, sharepoint_config_key):
+    s = Sharepoint(config_key=sharepoint_config_key)
     df = s.to_df(sharepoint_url)
     assert not df.empty
+
+
+def test_download_file(sharepoint_url, sharepoint_config_key):
+    s = Sharepoint(config_key=sharepoint_config_key)
+
+    file = "sharepoint_test" + sharepoint_url.split(".")[-1]
+    s.download_file(url=sharepoint_url, to_path=file)
+
+    assert file in os.listdir()
+
+    os.remove(file)
--- viadot/sources/__init__.py
+++ viadot/sources/__init__.py
@@ -1,13 +1,12 @@
 from .azure_data_lake import AzureDataLake
 from .cloud_for_customers import CloudForCustomers
+from .databricks import Databricks
+from .exchange_rates import ExchangeRates
+from .redshift_spectrum import RedshiftSpectrum
+from .s3 import S3
+from .sharepoint import Sharepoint
 
 try:
     from .sap_rfc import SAPRFC
 except ImportError:
     pass
-
-from .databricks import Databricks
-from .exchange_rates import ExchangeRates
-from .s3 import S3
-from .sharepoint import Sharepoint
-from .redshift_spectrum import RedshiftSpectrum
