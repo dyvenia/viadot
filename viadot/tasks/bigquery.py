7ddc627e82311b0595c7da6a6bf019f998cbd687
Rafalz13 <rziemianek@dyvenia.com>
2023-03-06 15:10:09 +0100

    ✨ Added new parameter and modified run method

--- viadot/tasks/bigquery.py
+++ viadot/tasks/bigquery.py
@@ -20,6 +20,7 @@ class BigQueryToDF(Task):
 
     def __init__(
         self,
+        query: str = None,
         dataset_name: str = None,
         table_name: str = None,
         start_date: str = None,
@@ -33,20 +34,22 @@ class BigQueryToDF(Task):
         **kwargs: Dict[str, Any],
     ):
         """
-        Initialize BigQueryToDF object. For querying on database - dataset and table name is required.
+        Initialize BigQueryToDF object. For querying on database user have to provide `query` parameter or `dataset` and `table_name`.
         The name of the project is taken from the config/credential json file so there is no need to enter its name.
 
-        There are 3 cases:
-            If start_date and end_date are not None - all data from the start date to the end date will be retrieved.
-            If start_date and end_date are left as default (None) - the data is pulled till "yesterday" (current date -1)
+        If user does not provide `query` there are 3 cases:
+            If `start_date` and `end_date` are not None - all data from the start date to the end date will be retrieved.
+            If `start_date` and `end_date` are left as default (None) - the data is pulled till "yesterday" (current date -1)
             If the column that looks like a date does not exist in the table, get all the data from the table.
 
         Args:
+            query(str, optional): SQL query to querying data in BigQuery. Format of basic query: (SELECT * FROM `{project}.{dataset_name}.{table_name}`)
+                Defaults to None.
             dataset_name (str, optional): Dataset name. Defaults to None.
             table_name (str, optional): Table name. Defaults to None.
             date_column_name (str, optional): The query is based on a date, the user can provide the name
-            of the date columnn if it is different than "date". If the user-specified column does not exist,
-            all data will be retrieved from the table. Defaults to "date".
+                of the date columnn if it is different than "date". If the user-specified column does not exist,
+                all data will be retrieved from the table. Defaults to "date".
             start_date (str, optional): A query parameter to pass start date e.g. "2022-01-01". Defaults to None.
             end_date (str, optional): A query parameter to pass end date e.g. "2022-01-01". Defaults to None.
             credentials_key (str, optional): Credential key to dictionary where details are stored (local config).
@@ -56,6 +59,7 @@ class BigQueryToDF(Task):
             timeout(int, optional): The amount of time (in seconds) to wait while running this task before
                 a timeout occurs. Defaults to 3600.
         """
+        self.query = query
         self.dataset_name = dataset_name
         self.table_name = table_name
         self.start_date = start_date
@@ -77,6 +81,7 @@ class BigQueryToDF(Task):
         super().__call__(self)
 
     @defaults_from_attrs(
+        "query",
         "dataset_name",
         "table_name",
         "date_column_name",
@@ -88,6 +93,7 @@ class BigQueryToDF(Task):
     )
     def run(
         self,
+        query: str = None,
         dataset_name: str = None,
         table_name: str = None,
         date_column_name: str = "date",
@@ -107,34 +113,44 @@ class BigQueryToDF(Task):
 
         bigquery = BigQuery(credentials_key=credentials_key, credentials=credentials)
         project = bigquery.get_project_id()
-        try:
-            table_columns = bigquery.list_columns(
-                dataset_name=dataset_name, table_name=table_name
-            )
-            if date_column_name not in table_columns:
-                logger.warning(
-                    f"'{date_column_name}' column is not recognized. Downloading all the data from '{table_name}'."
+        if query is None:
+            try:
+                table_columns = bigquery.list_columns(
+                    dataset_name=dataset_name, table_name=table_name
                 )
-                query = f"SELECT * FROM `{project}.{dataset_name}.{table_name}`"
-                df = bigquery.query_to_df(query)
-            else:
-                if start_date is not None and end_date is not None:
-                    query = f"""SELECT * FROM `{project}.{dataset_name}.{table_name}` 
-                    where {date_column_name} between PARSE_DATE("%Y-%m-%d", "{start_date}") and PARSE_DATE("%Y-%m-%d", "{end_date}") 
-                    order by {date_column_name} desc"""
+                if date_column_name not in table_columns:
+                    logger.warning(
+                        f"'{date_column_name}' column is not recognized. Downloading all the data from '{table_name}'."
+                    )
+                    query = f"SELECT * FROM `{project}.{dataset_name}.{table_name}`"
+                    df = bigquery.query_to_df(query)
                 else:
-                    query = f"""SELECT * FROM `{project}.{dataset_name}.{table_name}` 
-                    where {date_column_name} < CURRENT_DATE() 
-                    order by {date_column_name} desc"""
+                    if start_date is not None and end_date is not None:
+                        query = f"""SELECT * FROM `{project}.{dataset_name}.{table_name}` 
+                        where {date_column_name} between PARSE_DATE("%Y-%m-%d", "{start_date}") and PARSE_DATE("%Y-%m-%d", "{end_date}") 
+                        order by {date_column_name} desc"""
+                    else:
+                        query = f"""SELECT * FROM `{project}.{dataset_name}.{table_name}` 
+                        where {date_column_name} < CURRENT_DATE() 
+                        order by {date_column_name} desc"""
 
-                df = bigquery.query_to_df(query)
-                logger.info(
-                    f"Downloaded the data from the '{table_name}' table into the data frame."
+                    df = bigquery.query_to_df(query)
+                    logger.info(
+                        f"Downloaded the data from the '{table_name}' table into the data frame."
+                    )
+
+            except DBDataAccessError:
+                logger.warning(
+                    f"Table name '{table_name}' or dataset '{dataset_name}' not recognized. Returning empty data frame."
                 )
-
-        except DBDataAccessError:
-            logger.warning(
-                f"Table name '{table_name}' or dataset '{dataset_name}' not recognized. Returning empty data frame."
-            )
-            df = pd.DataFrame()
-        return df
+                df = pd.DataFrame()
+            return df
+        else:
+            try:
+                df = bigquery.query_to_df(query)
+            except Exception as e:
+                logger.warning(
+                    f"The query is invalid. Please enter a valid query. Returning an empty dataframe."
+                )
+                df = pd.DataFrame()
+            return df
