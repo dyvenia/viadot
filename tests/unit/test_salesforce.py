4215dea7f100786defa7dddc4222ca483cc92a48
djagoda881 <djagoda881@gmail.com>
2023-03-09 09:14:29 +0000

    ✅ Chenged testing structure in upsert tests

--- tests/unit/test_salesforce.py
+++ tests/unit/test_salesforce.py
@@ -5,7 +5,6 @@ from viadot.sources import Salesforce
 TABLE_TO_DOWNLOAD = "Account"
 TABLE_TO_UPSERT = "Contact"
 TEST_LAST_NAME = "viadot-test"
-ID_TO_UPSERT = "0035E00001YGWK3QAP"
 
 
 @pytest.fixture(scope="session")
@@ -15,53 +14,76 @@ def salesforce():
 
 
 @pytest.fixture(scope="session")
-def test_df_data(salesforce):
+def test_row_creation(salesforce):
+
+    # Creating a test row
+    test_row = {"LastName": "salesforce-test", "SAPContactId__c": "88111"}
+    sf = salesforce.salesforce
+    sf.Contact.create(test_row)
+
+    yield
+
+    # Finding the Id of a created row in a table and removing it
+    result = sf.query(f"SELECT Id FROM {TABLE_TO_UPSERT} WHERE SAPContactId__c='88111'")
+    sf.Contact.delete(result["records"][0]["Id"])
+
+
+def test_upsert_external_id_correct(salesforce, test_row_creation):
+
     data = {
-        "Id": [ID_TO_UPSERT],
         "LastName": [TEST_LAST_NAME],
+        "SAPContactId__c": ["88111"],
     }
     df = pd.DataFrame(data=data)
 
-    yield df
-
-    sf = salesforce.salesforce
-    sf.Contact.update(ID_TO_UPSERT, {"LastName": "LastName"})
-
-
-@pytest.fixture(scope="session")
-def test_df_external(salesforce):
-    data = {
-        "LastName": [TEST_LAST_NAME],
-        "SAPContactId__c": ["111"],
-    }
-    df = pd.DataFrame(data=data)
-    yield df
-
-    sf = salesforce.salesforce
-    sf.Contact.update(ID_TO_UPSERT, {"LastName": "LastName"})
-
-
-def test_upsert_external_id_correct(salesforce, test_df_external):
     try:
-        salesforce.upsert(
-            df=test_df_external, table=TABLE_TO_UPSERT, external_id="SAPContactId__c"
-        )
+        salesforce.upsert(df=df, table=TABLE_TO_UPSERT, external_id="SAPContactId__c")
     except Exception as exception:
         raise exception
 
     sf = salesforce.salesforce
     result = sf.query(
-        f"SELECT ID, LastName FROM {TABLE_TO_UPSERT} WHERE ID='{ID_TO_UPSERT}'"
+        f"SELECT Id, LastName FROM {TABLE_TO_UPSERT} WHERE SAPContactId__c='88111'"
     )
 
     assert result["records"][0]["LastName"] == TEST_LAST_NAME
 
 
-def test_upsert_external_id_wrong(salesforce, test_df_external):
+def test_upsert_row_id(salesforce, test_row_creation):
+
+    sf = salesforce.salesforce
+    created_row = sf.query(
+        f"SELECT Id FROM {TABLE_TO_UPSERT} WHERE SAPContactId__c='88111'"
+    )
+
+    created_row_id = created_row["records"][0]["Id"]
+
+    data = {
+        "Id": [created_row_id],
+        "LastName": [TEST_LAST_NAME],
+    }
+    df = pd.DataFrame(data=data)
+
+    try:
+        salesforce.upsert(df=df, table=TABLE_TO_UPSERT)
+    except Exception as exception:
+        raise exception
+
+    result = sf.query(
+        f"SELECT Id, LastName FROM {TABLE_TO_UPSERT} WHERE Id ='{created_row_id}'"
+    )
+
+    assert result["records"][0]["LastName"] == TEST_LAST_NAME
+
+
+def test_upsert_external_id_wrong(salesforce, test_row_creation):
+    data = {
+        "LastName": [TEST_LAST_NAME],
+        "SAPContactId__c": ["88111"],
+    }
+    df = pd.DataFrame(data=data)
     with pytest.raises(ValueError):
-        salesforce.upsert(
-            df=test_df_external, table=TABLE_TO_UPSERT, external_id="SAPId"
-        )
+        salesforce.upsert(df=df, table=TABLE_TO_UPSERT, external_id="SAPId")
 
 
 def test_download_no_query(salesforce):
@@ -81,17 +103,3 @@ def test_to_df(salesforce):
     assert df.empty == False
     assert len(df.columns) == 98
     assert len(df.values) >= 1000
-
-
-def test_upsert_row_id(salesforce, test_df_data):
-    try:
-        salesforce.upsert(df=test_df_data, table=TABLE_TO_UPSERT)
-    except Exception as exception:
-        raise exception
-
-    sf = salesforce.salesforce
-    result = sf.query(
-        f"SELECT ID, LastName FROM {TABLE_TO_UPSERT} WHERE ID='{ID_TO_UPSERT}'"
-    )
-
-    assert result["records"][0]["LastName"] == TEST_LAST_NAME
