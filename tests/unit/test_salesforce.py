990775a64baf15b9f9fb5eb1b7d19dbc24a53dba
djagoda881 <djagoda881@gmail.com>
2023-03-10 14:14:08 +0000

    ✅ Added bulk upsert test and refeactored tests

--- tests/unit/test_salesforce.py
+++ tests/unit/test_salesforce.py
@@ -7,65 +7,63 @@ TABLE_TO_UPSERT = "Contact"
 TEST_LAST_NAME = "viadot-test"
 
 EXPECTED_VALUE = pd.DataFrame(
-    {"LastName": ["salesforce-test"], "SAPContactId__c": ["88111"]}
+    {"LastName": ["salesforce-test"], "SAPContactId__c": ["8811111"]}
 )
+
+TEST_ROW = {"LastName": "salesforce-test", "SAPContactId__c": "8811111"}
+
+TWO_TEST_ROWS_INSERT = {
+    "LastName": ["viadot-insert-1", "viadot-insert-2"],
+    "SAPContactId__c": ["8812000", "8812100"],
+}
+
+TWO_TEST_ROWS_UPDATE = {
+    "LastName": ["viadot-update-1", "viadot-update-2"],
+    "SAPContactId__c": ["8812000", "8812100"],
+}
+
+
+def get_tested_records(salesforce, multiple_rows=False):
+    sf = salesforce.salesforce
+    if multiple_rows:
+        result = sf.query(
+            f"SELECT Id, LastName FROM {TABLE_TO_UPSERT} WHERE SAPContactId__c in ('8812000','8812100')"
+        )
+    else:
+        result = sf.query(
+            f"SELECT Id, LastName FROM {TABLE_TO_UPSERT} WHERE SAPContactId__c='8811111'"
+        )
+
+    return result["records"]
 
 
 @pytest.fixture(scope="session")
 def salesforce():
+
     s = Salesforce(config_key="salesforce_dev")
+
     yield s
 
-
-@pytest.fixture(scope="session")
-def test_row_creation(salesforce):
-
-    # Creating a test row
-    test_row = {"LastName": "salesforce-test", "SAPContactId__c": "88111"}
-    sf = salesforce.salesforce
-    sf.Contact.create(test_row)
-
-    yield
-
-    # Finding the Id of a created row in a table and removing it
-    result = sf.query(f"SELECT Id FROM {TABLE_TO_UPSERT} WHERE SAPContactId__c='88111'")
-    sf.Contact.delete(result["records"][0]["Id"])
-
-
-def test_upsert_external_id_column(salesforce, test_row_creation):
-
-    data = {
-        "LastName": [TEST_LAST_NAME],
-        "SAPContactId__c": ["88111"],
-    }
-    df = pd.DataFrame(data=data)
-
-    try:
-        salesforce.upsert(
-            df=df, table=TABLE_TO_UPSERT, external_id_column="SAPContactId__c"
-        )
-    except Exception as exception:
-        raise exception
-
-    sf = salesforce.salesforce
+    sf = s.salesforce
     result = sf.query(
-        f"SELECT Id, LastName FROM {TABLE_TO_UPSERT} WHERE SAPContactId__c='88111'"
+        f"SELECT Id FROM {TABLE_TO_UPSERT} WHERE SAPContactId__c in ('8812000','8812100','8811111') "
     )
 
-    assert result["records"][0]["LastName"] == TEST_LAST_NAME
+    # Deletes test rows from Salesforce if any remain
+    nr_rows = len(result["records"])
+    for nr in range(nr_rows):
+        sf.Contact.delete(result["records"][nr]["Id"])
 
 
-def test_upsert_row_id(salesforce, test_row_creation):
+def test_upsert_row_id(salesforce):
+
+    assert not get_tested_records(salesforce)
 
     sf = salesforce.salesforce
-    created_row = sf.query(
-        f"SELECT Id FROM {TABLE_TO_UPSERT} WHERE SAPContactId__c='88111'"
-    )
-
-    created_row_id = created_row["records"][0]["Id"]
+    sf.Contact.create(TEST_ROW)
 
     data = {
-        "Id": [created_row_id],
+        "Id": [get_tested_records(salesforce)[0]["Id"]],
         "LastName": [TEST_LAST_NAME],
     }
     df = pd.DataFrame(data=data)
@@ -75,42 +73,84 @@ def test_upsert_row_id(salesforce, test_row_creation):
     except Exception as exception:
         raise exception
 
-    result = sf.query(
-        f"SELECT Id, LastName FROM {TABLE_TO_UPSERT} WHERE Id ='{created_row_id}'"
-    )
+    updated_row = get_tested_records(salesforce)
 
-    assert result["records"][0]["LastName"] == TEST_LAST_NAME
+    assert updated_row[0]["LastName"] == TEST_LAST_NAME
+
+    sf.Contact.delete(updated_row[0]["Id"])
 
 
-def test_upsert_non_existent_row(salesforce):
+def test_upsert(salesforce):
 
-    data = {
-        "LastName": ["viadot-insert-1", "viadot-insert-2"],
-        "SAPContactId__c": ["88120", "88121"],
-    }
-    df = pd.DataFrame(data=data)
+    assert not get_tested_records(salesforce, multiple_rows=True)
+
+    df = pd.DataFrame(data=TWO_TEST_ROWS_INSERT)
 
     try:
         salesforce.upsert(df=df, table="Contact", external_id_column="SAPContactId__c")
     except Exception as exception:
         raise exception
 
+    inserted_rows = get_tested_records(salesforce, multiple_rows=True)
+    assert inserted_rows[0]["LastName"] == "viadot-insert-1"
+    assert inserted_rows[1]["LastName"] == "viadot-insert-2"
+
+    df = pd.DataFrame(data=TWO_TEST_ROWS_UPDATE)
+
+    try:
+        salesforce.upsert(df=df, table="Contact", external_id_column="SAPContactId__c")
+    except Exception as exception:
+        raise exception
+
+    updated_rows = get_tested_records(salesforce, multiple_rows=True)
+    assert updated_rows[0]["LastName"] == "viadot-update-1"
+    assert updated_rows[1]["LastName"] == "viadot-update-2"
+
     sf = salesforce.salesforce
-    created_rows = sf.query(
-        f"SELECT Id, LastName FROM {TABLE_TO_UPSERT} WHERE SAPContactId__c in ('88120','88121')"
-    )
-
-    assert created_rows["records"][0]["LastName"] == "viadot-insert-1"
-    assert created_rows["records"][1]["LastName"] == "viadot-insert-2"
-
-    sf.Contact.delete(created_rows["records"][0]["Id"])
-    sf.Contact.delete(created_rows["records"][1]["Id"])
+    sf.Contact.delete(inserted_rows[0]["Id"])
+    sf.Contact.delete(inserted_rows[1]["Id"])
 
 
-def test_upsert_external_id_column_wrong(salesforce, test_row_creation):
+def test_bulk_upsert(salesforce):
+
+    assert not get_tested_records(salesforce, multiple_rows=True)
+
+    df_insert = pd.DataFrame(data=TWO_TEST_ROWS_INSERT)
+
+    try:
+        salesforce.bulk_upsert(
+            df=df_insert, table="Contact", external_id_column="SAPContactId__c"
+        )
+    except Exception as exception:
+        raise exception
+
+    inserted_rows = get_tested_records(salesforce, multiple_rows=True)
+
+    assert inserted_rows[0]["LastName"] == "viadot-insert-1"
+    assert inserted_rows[1]["LastName"] == "viadot-insert-2"
+
+    df_update = pd.DataFrame(data=TWO_TEST_ROWS_UPDATE)
+
+    try:
+        salesforce.bulk_upsert(
+            df=df_update, table="Contact", external_id_column="SAPContactId__c"
+        )
+    except Exception as exception:
+        raise exception
+
+    updated_rows = get_tested_records(salesforce, multiple_rows=True)
+    assert updated_rows[0]["LastName"] == "viadot-update-1"
+    assert updated_rows[1]["LastName"] == "viadot-update-2"
+
+    sf = salesforce.salesforce
+    sf.Contact.delete(updated_rows[0]["Id"])
+    sf.Contact.delete(updated_rows[1]["Id"])
+
+
+def test_upsert_external_id_column_wrong(salesforce):
     data = {
         "LastName": [TEST_LAST_NAME],
-        "SAPContactId__c": ["88111"],
+        "SAPContactId__c": ["8811111"],
     }
     df = pd.DataFrame(data=data)
     with pytest.raises(ValueError):
@@ -128,8 +168,17 @@ def test_download_with_query(salesforce):
     assert len(records) > 0
 
 
-def test_to_df(salesforce, test_row_creation):
+def test_to_df(salesforce):
+
+    assert not get_tested_records(salesforce)
+
+    sf = salesforce.salesforce
+    sf.Contact.create(TEST_ROW)
+
     df = salesforce.to_df(
-        query=f"SELECT LastName, SAPContactId__c FROM {TABLE_TO_UPSERT} WHERE SAPContactId__c='88111'"
+        query=f"SELECT LastName, SAPContactId__c FROM {TABLE_TO_UPSERT} WHERE SAPContactId__c='8811111'"
     )
+
     assert df.equals(EXPECTED_VALUE)
+
+    sf.Contact.delete(get_tested_records(salesforce)[0]["Id"])
