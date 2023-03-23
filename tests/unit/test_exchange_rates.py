03f831b8d17f46eade84dbe3412c1dc9bbcc109e
djagoda881 <114922270+djagoda881@users.noreply.github.com>
2023-03-23 13:37:43 +0100

    Fixed bug in exchange rates unit tests (#638)
    * ✅ 🐛 Fixed bug in `test_to_df_values`
    
    * ✅ Updated exchange_rates test structure
    
    * ✅  Deleted not used import
    
    * ✅ Updated test_to_df_values

--- tests/unit/test_exchange_rates.py
+++ tests/unit/test_exchange_rates.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import pytest
 from viadot.sources import ExchangeRates
 
 TEST_DATA = {
@@ -40,44 +40,38 @@ TEST_DATA = {
 TEST_DF = pd.json_normalize(TEST_DATA["currencies"])
 
 
-def test_credentials():
-    source = ExchangeRates(config_key="exchange_rates_dev")
-    assert source.credentials is not None
-
-
-def test_to_json_values():
-    source = ExchangeRates(
+@pytest.fixture(scope="session")
+def exchange_rates():
+    e = ExchangeRates(
         currency="PLN",
         start_date="2022-10-09",
         end_date="2022-10-11",
         symbols=["USD", "EUR", "GBP", "CHF", "PLN", "DKK"],
         config_key="exchange_rates_dev",
     )
+    yield e
+
+
+def test_to_json_values(exchange_rates):
+
     expected_value = TEST_DATA.items()
-    retrieved_value = source.to_json().items()
+    retrieved_value = exchange_rates.to_json().items()
 
     assert retrieved_value == expected_value
 
 
-# def test_to_df_values():
-#     source = ExchangeRates(
-#         currency="PLN",
-#         start_date="2022-10-09",
-#         end_date="2022-10-11",
-#         symbols=["USD", "EUR", "GBP", "CHF", "PLN", "DKK"],
-#         config_key="exchange_rates_dev",
-#     )
-#     expected_value = TEST_DF
-#     retrieved_value = source.to_df()
-#     retrieved_value.drop(["_viadot_downloaded_at_utc"], axis=1, inplace=True)
-#     assert retrieved_value.iloc[0].equals(expected_value.iloc[0])
+def test_to_df_values(exchange_rates):
+    expected_value = TEST_DF
+
+    # Running the to_df function without the wrapper adding metadata columns
+    origin_to_df = exchange_rates.to_df.__wrapped__
+    retrieved_value = origin_to_df(exchange_rates)
+
+    assert retrieved_value.equals(expected_value)
 
 
-# def test_get_columns():
-#     source = ExchangeRates(
-#         symbols=["USD", "EUR", "GBP", "CZK", "SEK", "NOK", "ISK"],
-#         config_key="exchange_rates_dev",
-#     )
-#     expected_columns = ["Date", "Base", "USD", "EUR", "GBP", "CZK", "SEK", "NOK", "ISK"]
-#     retrieved_columns = source.get_columns()
-#     assert retrieved_columns == expected_columns
+def test_get_columns(exchange_rates):
+    expected_columns = ["Date", "Base", "USD", "EUR", "GBP", "CHF", "PLN", "DKK"]
+    retrieved_columns = exchange_rates.get_columns()
+
+    assert retrieved_columns == expected_columns
