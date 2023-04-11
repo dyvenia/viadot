82c22bc8a7950abdf1a7a954e0adf83980c1bcdd
trymzet <michalmzawadzki@gmail.com>
2023-04-11 12:05:15 +0200

    ✨ Migrated `Genesys` source

--- viadot/sources/__init__.py
+++ viadot/sources/__init__.py
@@ -2,6 +2,7 @@ from .azure_data_lake import AzureDataLake
 from .cloud_for_customers import CloudForCustomers
 from .databricks import Databricks
 from .exchange_rates import ExchangeRates
+from .genesys import Genesys
 from .redshift_spectrum import RedshiftSpectrum
 from .s3 import S3
 from .sharepoint import Sharepoint
