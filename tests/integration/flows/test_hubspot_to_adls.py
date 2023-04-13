73559ab07f88286639d08dd38c7b9b3056f7f19b
gwieloch <mwieloch@dyvenia.com>
2023-04-13 11:38:28 +0200

    updated test_hubspot_to_adls.py

--- tests/integration/flows/test_hubspot_to_adls.py
+++ tests/integration/flows/test_hubspot_to_adls.py
@@ -6,7 +6,18 @@ import json
 
 from viadot.flows import HubspotToADLS
 
-DATA = '{"id":{"0":"514733951"},"createdAt":{"0":"2023-01-01T11:04:39.774Z"},"updatedAt":{"0":"2023-03-31T07:07:46.191Z"},"archived":{"0":false},"properties.createdate":{"0":"2023-01-01T11:04:39.774Z"},"properties.email":{"0":"svob.marcela@seznam.cz"},"properties.firstname":{"0":null},"properties.hs_object_id":{"0":"514733951"},"properties.lastmodifieddate":{"0":"2023-03-31T07:07:46.191Z"},"properties.lastname":{"0":null}}'
+DATA = {
+    "id": {"0": "820306930"},
+    "createdAt": {"0": "2020-08-31T11:58:55.762Z"},
+    "updatedAt": {"0": "2021-07-17T05:25:41.480Z"},
+    "archived": {"0": "False"},
+    "properties.amount": {"0": "3744.000"},
+    "properties.createdate": {"0": "2020-08-31T11:58:55.762Z"},
+    "properties.hs_lastmodifieddate": {"0": "2021-07-17T05:25:41.480Z"},
+    "properties.hs_object_id": {"0": "820306930"},
+    "properties.hs_product_id": {"0": "2344"},
+    "properties.quantity": {"0": "2"},
+}
 
 ADLS_FILE_NAME = "test_hubspot.parquet"
 ADLS_DIR_PATH = "raw/tests/"
@@ -14,37 +25,29 @@ ADLS_DIR_PATH = "raw/tests/"
 
 @mock.patch(
     "viadot.tasks.HubspotToDF.run",
-    return_value=pd.DataFrame(data=json.loads(DATA)),
+    return_value=pd.DataFrame(data=DATA),
 )
 @pytest.mark.run
 def test_hubspot_to_adls_flow_run(mocked_class):
     flow = HubspotToADLS(
         "test_hubspot_to_adls_flow_run",
         hubspot_credentials_key="HUBSPOT",
-        endpoint="contacts",
+        endpoint="line_items",
         filters=[
             {
                 "filters": [
                     {
                         "propertyName": "createdate",
                         "operator": "BETWEEN",
-                        "highValue": "2023-01-02",
-                        "value": "2023-01-01",
+                        "highValue": "2021-01-01",
+                        "value": "2021-01-01",
                     },
-                    {
-                        "propertyName": "email",
-                        "operator": "CONTAINS_TOKEN",
-                        "value": "*marcela@seznam.cz",
-                    },
+                    {"propertyName": "quantity", "operator": "EQ", "value": "2"},
                 ]
             },
             {
                 "filters": [
-                    {
-                        "propertyName": "lastmodifieddate",
-                        "operator": "EQ",
-                        "value": "2023-01-03",
-                    }
+                    {"propertyName": "amount", "operator": "EQ", "value": "3744.000"}
                 ]
             },
         ],
