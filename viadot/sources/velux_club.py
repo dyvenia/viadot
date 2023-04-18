d7727442367d4fcead67783b521d46a77d48126d
fdelgado <fdelgado@dyvenia.com>
2023-04-18 17:05:19 +0100

    [velux-club]: Created Class connector

--- viadot/sources/velux_club.py
+++ viadot/sources/velux_club.py
@@ -1,90 +1,109 @@
-import pandas as pd
+import json
+import urllib
+from copy import deepcopy
+from typing import Any, Dict, List
 import requests
 
-pd.set_option("display.max_columns", None)
-
-API_URL = "https://api.club.velux.com/api/v1/datalake/"
-TOKEN = "k7h8MvfoSxdpCMdZNval8SoGtyzoiM9SjSE03QgnYxOBEztw4YIQkJxRLKjXBnuB"
+import numpy as np
+import pandas as pd
 
 
-def velux_club_to_df(
-    source: list, from_date: str = "", to_date: str = "", region="null"
-) -> tuple:
-    """Function to download a file from sharepoint, given its URL
-    Args:
-        source (str):
-        from_date (str):
-        to_date (str,str):
-        region (str):
-    Returns:
-        str: filename
+from ..config import local_config
+from ..exceptions import CredentialError
+from ..utils import handle_api_response
+from .base import Source
+
+
+class VeluxClub(Source):
 
     """
-    df = pd.DataFrame()
+    A class implementing the Velux Club API.
 
-    if source in ["jobs", "product", "company"]:
-        # check if date filter was passed!
-        if from_date == "" or to_date == "":
-            return (df, "Introduce a 'FROM Date' and 'To Date'")
-        url = f"{API_URL}{source}?from={from_date}&to={to_date}&region&limit=100"
-    elif source in "survey":
-        url = f"{API_URL}{source}?language=en&type=question"
-    else:
-        return (df, "pick one these sources: jobs, product, company, survey")
+    Documentation for this API is located at: https://api.club.velux.com/api/v1/datalake/
+    There are 4 endpoints where to get the data!
 
-    headers = {
-        "Authorization": "Bearer " + TOKEN,
-        "Content-Type": "application/json",
-    }
+    Parameters
+    ----------
+    query_params : Dict[str, Any], optional
+        The parameters to pass to the GET query.
+        See https://supermetrics.com/docs/product-api-get-data/ for full specification,
+        by default None
+    """
 
-    r = requests.request("GET", url, headers=headers)
+    API_URL = "https://api.club.velux.com/api/v1/datalake/"
 
-    response = r.json()
+    def __init__(self, *args, credentials: Dict[str, Any] = None, **kwargs):
+        DEFAULT_CREDENTIALS = local_config.get("VELUX_CLUB")
+        credentials = kwargs.pop("credentials", DEFAULT_CREDENTIALS)
+        if credentials is None:
+            raise CredentialError("Missing credentials.")
+        super().__init__(*args, credentials=credentials, **kwargs)
 
-    if isinstance(response, dict):
-        keys_list = list(response.keys())
-    elif isinstance(response, list):
-        keys_list = list(response[0].keys())
-    else:
-        keys_list = []
+    # Split this function into Download info and df creation!
+    def velux_club_to_df(
+        self, source: list, from_date: str = "", to_date: str = "", region="null"
+    ) -> tuple:
+        """Function to download a file from sharepoint, given its URL
+        Args:
+            source (str):
+            from_date (str):
+            to_date (str,str):
+            region (str):
+        Returns:
+            str: filename
 
-    if "data" in keys_list:
-        # first page content
-        df = pd.DataFrame(response["data"])
-        # next pages
-        while response["next_page_url"] != None:
-            url = f"{response['next_page_url']}&from={from_date}&to={to_date}&region&limit=100"
-            r = requests.request("GET", url, headers=headers)
-            response = r.json()
-            df_page = pd.DataFrame(response["data"])
-            df_page_transpose = df_page.T
-            df = df.append(df_page_transpose, ignore_index=True)
+        """
+        df = pd.DataFrame()
 
-    else:
-        df = pd.DataFrame(response)
+        if self.source in ["jobs", "product", "company"]:
+            # check if date filter was passed!
+            if from_date == "" or to_date == "":
+                return (df, "Introduce a 'FROM Date' and 'To Date'")
+            url = (
+                f"{self.API_URL}{source}?from={from_date}&to={to_date}&region&limit=100"
+            )
+        elif source in "survey":
+            url = f"{self.API_URL}{source}?language=en&type=question"
+        else:
+            return (df, "pick one these sources: jobs, product, company, survey")
 
-    # file_name = f"{source}_from_{from_date}_to_{to_date}.parquet"
-    # df.astype(str).to_parquet(file_name)
-    return (df, f"Source {source}")
+        headers = {
+            "Authorization": "Bearer " + self.credentials["TOKEN"],
+            "Content-Type": "application/json",
+        }
 
+        r = requests.request("GET", url, headers=headers)
 
-def print_df(df: pd.DataFrame, service_name: str):
-    print(f"{service_name} Dataframe Columns")
-    print(list(df.columns))
-    print(f"{service_name} Number of Columns")
-    print(len(list(df.columns)))
-    print(f"{service_name} Dataframe Number Of Samples")
-    print(str(len(df.index)))
+        response = r.json()
 
+        if isinstance(response, dict):
+            keys_list = list(response.keys())
+        elif isinstance(response, list):
+            keys_list = list(response[0].keys())
+        else:
+            keys_list = []
 
-(df, msg) = velux_club_to_df("jobs", "2023-01-01", "2023-01-03")
-print_df(df, "Jobs")
+        if "data" in keys_list:
+            # first page content
+            df = pd.DataFrame(response["data"])
+            # next pages
+            while response["next_page_url"] != None:
+                url = f"{response['next_page_url']}&from={from_date}&to={to_date}&region&limit=100"
+                r = requests.request("GET", url, headers=headers)
+                response = r.json()
+                df_page = pd.DataFrame(response["data"])
+                df_page_transpose = df_page.T
+                df = df.append(df_page_transpose, ignore_index=True)
 
-(df, msg) = velux_club_to_df("product", "2023-01-01", "2023-01-03")
-print_df(df, "Product")
+        else:
+            df = pd.DataFrame(response)
 
-(df, msg) = velux_club_to_df("company", "2023-01-01", "2023-01-03")
-print_df(df, "Company")
+        return (df, f"Source {source}")
 
-(df, msg) = velux_club_to_df("survey")
-print_df(df, "Survey")
+    def print_df(df: pd.DataFrame, service_name: str):
+        print(f"{service_name} Dataframe Columns")
+        print(list(df.columns))
+        print(f"{service_name} Number of Columns")
+        print(len(list(df.columns)))
+        print(f"{service_name} Dataframe Number Of Samples")
+        print(str(len(df.index)))
