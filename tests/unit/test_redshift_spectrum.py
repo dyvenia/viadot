513b346ced92654fda62b717e735e434faf31202
Michał Zawadzki <michalmzawadzki@gmail.com>
2023-05-23 14:21:03 +0200

    Automatically create table folder in `RedshiftSpectrum.from_df()` (#693)
    * ♻️ Name AWS configs properly
    
    * 🔧 Add dotenv to compose
    
    * 🐛 Fix `S3.if_exists()` and `S3.rm()` not handling directories
    
    * ♻️  refactor RedshiftSpectrum source
        - `RedshiftSpectrum` can now be used as a context manager
        - added several methods for handling schemas
        - added some methods to work with underlying Glue databases and schemas
        - fixed logic so the source is actually working with Spectrum resources rather than Glue
        - removed references to `nesso`
        - added missing credentials
        - added credentials validation
        - changed the order of methods to improve readability
        - improved some tests, added some new ones
        - table folder is now automatically created if `to_path` param to `RedshiftSpectrum.from_df()` only points to a schema
    
    * 📝 Add info about `RedshiftSpectrum.from_df()` `to_path` param improvement
    
    * 📝 Add example `S3` and `RedshiftSpectrum` configs
    
    * 📝 Move comment to proper place
    
    * ♻️ Remove illogical assert
    
    * 📝 Remove duplicated docstrings
    
    * 🎨 Formatting
    
    * 📝 Remove incorrect docstring

--- tests/unit/test_redshift_spectrum.py
+++ tests/unit/test_redshift_spectrum.py
@@ -15,70 +15,251 @@ TEST_DF = pd.DataFrame(
 )
 TEST_DF = TEST_DF.astype({"col1": "Int32", "col2": "string"})
 
-S3_BUCKET = os.environ.get("S3_BUCKET")
-TEST_SCHEMA = "staging_dev"
-TEST_TABLE = "test_redshift_spectrum"
+S3_BUCKET = os.environ.get("VIADOT_S3_BUCKET")
+TEST_SCHEMA = "test_schema"
+TEST_TABLE = "test_table"
 
 
 @pytest.fixture(scope="session")
-def redshift(aws_config_key):
-    redshift = RedshiftSpectrum(config_key=aws_config_key)
+def redshift(redshift_config_key):
+    redshift = RedshiftSpectrum(config_key=redshift_config_key)
 
     yield redshift
 
+    try:
+        redshift.drop_schema(TEST_SCHEMA, drop_glue_database=True)
+    except Exception:
+        pass
+
+    try:
+        redshift._con.close()
+    except AttributeError:
+        pass
+
 
 @pytest.fixture(scope="session")
-def s3(aws_config_key):
-    s3 = S3(config_key=aws_config_key)
+def s3(s3_config_key):
+    s3 = S3(config_key=s3_config_key)
 
     yield s3
 
 
-def test_exists(redshift):
-    result = redshift.exists(
-        database=TEST_SCHEMA,
-        table="nesso_test_table",
-    )
+def test_create_schema(redshift):
+    # Assumptions.
+    schema_exists = redshift._check_if_schema_exists(schema=TEST_SCHEMA)
+    assert schema_exists is False
 
-    assert result is True
+    # Test.
+    redshift.create_schema(TEST_SCHEMA)
+    schema_exists = redshift._check_if_schema_exists(schema=TEST_SCHEMA)
+    assert schema_exists is True
+
+    # Cleanup.
+    redshift.drop_schema(TEST_SCHEMA, drop_glue_database=True)
 
 
-def test_from_df(redshift):
-    result_before = redshift.exists(
-        database=TEST_SCHEMA,
+def test__check_if_table_exists(redshift):
+    # Assumptions.
+    redshift.create_schema(TEST_SCHEMA)
+    schema_exists = redshift._check_if_schema_exists(schema=TEST_SCHEMA)
+    assert schema_exists is True
+
+    table_exists = redshift._check_if_table_exists(
+        schema=TEST_SCHEMA,
         table=TEST_TABLE,
     )
-    assert result_before is False
+    assert table_exists is False
 
+    # Test.
     redshift.from_df(
         df=TEST_DF,
-        to_path=f"s3://{S3_BUCKET}/nesso/{TEST_SCHEMA}/{TEST_TABLE}",
-        database=TEST_SCHEMA,
+        to_path=f"s3://{S3_BUCKET}/viadot/{TEST_SCHEMA}/{TEST_TABLE}",
+        schema=TEST_SCHEMA,
+        table=TEST_TABLE,
+    )
+    table_exists = redshift._check_if_table_exists(
+        schema=TEST_SCHEMA,
+        table=TEST_TABLE,
+    )
+    assert table_exists is True
+
+    # Cleanup.
+    redshift.drop_table(schema=TEST_SCHEMA, table=TEST_TABLE, remove_files=True)
+    redshift.drop_schema(TEST_SCHEMA)
+
+
+def test_from_df(redshift, s3):
+    # Assumptions.
+    table_exists = redshift._check_if_table_exists(
+        schema=TEST_SCHEMA,
+        table=TEST_TABLE,
+    )
+    assert table_exists is False
+
+    folder_exists = s3.exists(
+        path=f"s3://{S3_BUCKET}/viadot/{TEST_SCHEMA}/{TEST_TABLE}"
+    )
+    assert folder_exists is False
+
+    redshift.create_schema(TEST_SCHEMA)
+    schema_exists = redshift._check_if_schema_exists(schema=TEST_SCHEMA)
+    assert schema_exists is True
+
+    # Test.
+    redshift.from_df(
+        df=TEST_DF,
+        to_path=f"s3://{S3_BUCKET}/viadot/{TEST_SCHEMA}/{TEST_TABLE}",
+        schema=TEST_SCHEMA,
+        table=TEST_TABLE,
+    )
+    table_exists = redshift._check_if_table_exists(
+        schema=TEST_SCHEMA,
         table=TEST_TABLE,
     )
 
-    result_after = redshift.exists(
-        database=TEST_SCHEMA,
+    assert table_exists is True
+
+    folder_exists = s3.exists(
+        path=f"s3://{S3_BUCKET}/viadot/{TEST_SCHEMA}/{TEST_TABLE}"
+    )
+    assert folder_exists is True
+
+    # Cleanup.
+    redshift.drop_table(schema=TEST_SCHEMA, table=TEST_TABLE, remove_files=True)
+    redshift.drop_schema(TEST_SCHEMA, drop_glue_database=True)
+
+
+def test_from_df_no_table_folder_in_to_path(redshift, s3):
+    """Test that the table folder is created if it's not specified in `to_path`"""
+    # Assumptions.
+    table_exists = redshift._check_if_table_exists(
+        schema=TEST_SCHEMA,
         table=TEST_TABLE,
     )
-    redshift.drop_table(database=TEST_SCHEMA, table=TEST_TABLE, remove_files=True)
+    assert table_exists is False
 
-    assert result_after is True
+    folder_exists = s3.exists(
+        path=f"s3://{S3_BUCKET}/viadot/{TEST_SCHEMA}/{TEST_TABLE}"
+    )
+    assert folder_exists is False
+
+    redshift.create_schema(TEST_SCHEMA)
+    schema_exists = redshift._check_if_schema_exists(schema=TEST_SCHEMA)
+    assert schema_exists is True
+
+    # Test.
+    redshift.from_df(
+        df=TEST_DF,
+        to_path=f"s3://{S3_BUCKET}/viadot/{TEST_SCHEMA}",
+        schema=TEST_SCHEMA,
+        table=TEST_TABLE,
+    )
+
+    table_exists = redshift._check_if_table_exists(schema=TEST_SCHEMA, table=TEST_TABLE)
+    assert table_exists is True
+
+    folder_exists = s3.exists(
+        path=f"s3://{S3_BUCKET}/viadot/{TEST_SCHEMA}/{TEST_TABLE}"
+    )
+    assert folder_exists is True
+
+    # Cleanup.
+    redshift.drop_table(schema=TEST_SCHEMA, table=TEST_TABLE, remove_files=True)
+    redshift.drop_schema(TEST_SCHEMA, drop_glue_database=True)
 
 
 def test_to_df(redshift):
+    # Assumptions.
+    redshift.create_schema(TEST_SCHEMA)
+    schema_exists = redshift._check_if_schema_exists(schema=TEST_SCHEMA)
+    assert schema_exists is True
+
+    # Test.
     redshift.from_df(
         df=TEST_DF,
-        to_path=f"s3://{S3_BUCKET}/nesso/{TEST_SCHEMA}/{TEST_TABLE}",
-        database=TEST_SCHEMA,
+        to_path=f"s3://{S3_BUCKET}/viadot/{TEST_SCHEMA}/{TEST_TABLE}",
+        schema=TEST_SCHEMA,
         table=TEST_TABLE,
     )
 
-    result = redshift.to_df(
-        database=TEST_SCHEMA,
+    df = redshift.to_df(
+        schema=TEST_SCHEMA,
         table=TEST_TABLE,
     )
+    assert df.equals(TEST_DF)
 
-    assert result.equals(TEST_DF)
+    # Cleanup.
+    redshift.drop_table(schema=TEST_SCHEMA, table=TEST_TABLE, remove_files=True)
+    redshift.drop_schema(TEST_SCHEMA, drop_glue_database=True)
 
-    redshift.drop_table(database=TEST_SCHEMA, table=TEST_TABLE, remove_files=True)
+
+def test_get_tables(redshift):
+    # Assumptions.
+    redshift.create_schema(TEST_SCHEMA)
+    schema_exists = redshift._check_if_schema_exists(schema=TEST_SCHEMA)
+    assert schema_exists is True
+
+    tables = redshift.get_tables(schema=TEST_SCHEMA)
+    assert TEST_TABLE not in tables
+
+    # Test.
+    redshift.from_df(
+        df=TEST_DF,
+        to_path=f"s3://{S3_BUCKET}/viadot/{TEST_SCHEMA}/{TEST_TABLE}",
+        schema=TEST_SCHEMA,
+        table=TEST_TABLE,
+    )
+    tables = redshift.get_tables(schema=TEST_SCHEMA)
+    assert tables == [TEST_TABLE]
+
+    # Cleanup.
+    redshift.drop_table(schema=TEST_SCHEMA, table=TEST_TABLE, remove_files=True)
+    redshift.drop_schema(TEST_SCHEMA, drop_glue_database=True)
+
+
+def test_get_tables_incorrect_schema(redshift):
+    """Verify that `get_tables()` returns an empty list if the schema doesn't exist."""
+    # Assumptions.
+    schema_exists = redshift._check_if_schema_exists("test_schema2")
+    assert schema_exists is False
+
+    # Test.
+    tables = redshift.get_tables(schema="test_schema2")
+    assert tables == []
+
+
+def test_drop_table(redshift):
+    # Assumptions.
+    redshift.create_schema(TEST_SCHEMA)
+    schema_exists = redshift._check_if_schema_exists(schema=TEST_SCHEMA)
+    assert schema_exists is True
+
+    redshift.from_df(
+        df=TEST_DF,
+        to_path=f"s3://{S3_BUCKET}/viadot/{TEST_SCHEMA}/{TEST_TABLE}",
+        schema=TEST_SCHEMA,
+        table=TEST_TABLE,
+    )
+    table_exists = redshift._check_if_table_exists(schema=TEST_SCHEMA, table=TEST_TABLE)
+    assert table_exists is True
+
+    # Test.
+    redshift.drop_table(schema=TEST_SCHEMA, table=TEST_TABLE, remove_files=True)
+
+    table_exists = redshift._check_if_table_exists(schema=TEST_SCHEMA, table=TEST_TABLE)
+    assert table_exists is False
+
+    # Cleanup.
+    redshift.drop_schema(TEST_SCHEMA, drop_glue_database=True)
+
+
+def test__is_spectrum_schema(redshift):
+    # Prerequisite.
+    redshift.create_schema(TEST_SCHEMA)
+
+    # Test.
+    assert not redshift._is_spectrum_schema("public")
+    assert redshift._is_spectrum_schema(TEST_SCHEMA)
+
+    # Cleanup.
+    redshift.drop_schema(TEST_SCHEMA, drop_glue_database=True)
--- viadot/sources/redshift_spectrum.py
+++ viadot/sources/redshift_spectrum.py
@@ -1,9 +1,12 @@
-from typing import Any, Dict, List, Literal
+import os
+from pathlib import Path
+from typing import List, Literal, Optional, Tuple
 
 import awswrangler as wr
 import boto3
 import pandas as pd
-from pydantic import BaseModel
+import redshift_connector
+from pydantic import BaseModel, ValidationError, root_validator
 
 from viadot.config import get_source_credentials
 from viadot.sources.base import Source
@@ -12,19 +15,61 @@ from viadot.sources.base import Source
 class RedshiftSpectrumCredentials(BaseModel):
     profile_name: str  # The name of the IAM profile to use.
     region_name: str  # The name of the AWS region.
-    aws_access_key_id: str
-    aws_secret_access_key: str
+    aws_access_key_id: str  # The AWS access key ID.
+    aws_secret_access_key: str  # The AWS secret access key.
+
+    # Below credentials are required only by some methods.
+    #
+    # The name of a AWS Secret holding credentials to a Redshift instance.
+    # This AWS Secret has to include the following configuration:
+    # host: Optional[str]
+    # port: Optional[str] = "5439"
+    # username: Optional[str]
+    # password: Optional[str]
+    # engine: Optional[str] = "redshift"
+    # dbname: Optional[str]
+    credentials_secret: Optional[str]
+    iam_role: Optional[str]  # The IAM role to assume. Used by `create_schema()`.
+
+    @root_validator(pre=True)
+    def is_configured(cls, credentials):
+        profile_name = credentials.get("profile_name")
+        region_name = credentials.get("region_name")
+        aws_access_key_id = credentials.get("aws_access_key_id")
+        aws_secret_access_key = credentials.get("aws_secret_access_key")
+
+        profile_credential = profile_name and region_name
+        direct_credential = aws_access_key_id and aws_secret_access_key and region_name
+
+        if not (profile_credential or direct_credential):
+            raise ValueError(
+                "Either `profile_name` and `region_name`, or `aws_access_key_id`, "
+                "`aws_secret_access_key`, and `region_name` must be specified."
+            )
 
 
 class RedshiftSpectrum(Source):
     """
-    A class for pulling data from and uploading to a specified external database of Amazon Redshift Spectrum.
+    A class for pulling data from and uploading to a specified Amazon Redshift Spectrum
+    external schema.
+
+    Note that internally, AWS SDK refers to schemas as "databases", as external schemas
+    correspond to AWS Glue databases. However, to keep consistent naming with all other
+    viadot sources, we use the word "schema" instead.
 
     Args:
-        credentials (RedshiftSpectrumCredentials, optional): RedshiftSpectrumCredentials credentials.
-            Defaults to None.
+        credentials (RedshiftSpectrumCredentials, optional): RedshiftSpectrumCredentials
+            credentials. Defaults to None.
         config_key (str, optional): The key in the viadot config holding relevant
             credentials. Defaults to None.
+
+    Examples:
+        ```python
+        from viadot.sources import RedshiftSpectrum
+
+        with RedshiftSpectrum(config_key="redshift_spectrum") as redshift:
+            redshift.get_schemas()
+        ```
     """
 
     def __init__(
@@ -44,10 +89,23 @@ class RedshiftSpectrum(Source):
             )
 
         self._session = None
+        self._con = None
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        if self._con:
+            self._con.close()
+            self._con = None
 
     @property
     def session(self) -> boto3.session.Session:
-        """A singleton-like property for initiating a session to the AWS."""
+        """
+        A singleton-like property for initiating an AWS session with boto3.
+
+        Note that this is not an actual session, so it does not need to be closed.
+        """
         if not self._session:
             self._session = boto3.session.Session(
                 region_name=self.credentials.get("region_name"),
@@ -57,76 +115,26 @@ class RedshiftSpectrum(Source):
             )
         return self._session
 
-    def ls(
-        self,
-        database: str,
-        name_contains: str = None,
-    ) -> List[str]:
-        """
-        Returns a list of tables in a specified external database of Amazon Redshift Spectrum.
-
-        Args:
-            database (str): Catalog name in a specified external database of Amazon Redshift Spectrum.
-            name_contains (str, optional): Match by a specific substring of the table
-                name. Defaults to None.
-        """
-        df = wr.catalog.get_tables(
-            boto3_session=self.session,
-            database=database,
-            name_contains=name_contains,
-        )
-
-        return df
-
-    def exists(self, database: str, table: str) -> bool:
-        """
-        Check if a table exists in a specified external database of Amazon Redshift Spectrum.
-
-        Args:
-            database (str): Catalog name in a specified external database of Amazon Redshift Spectrum.
-            table (str): Table name in a specified external database of Amazon Redshift Spectrum.
-
-        Returns:
-            bool: Whether the paths exists.
-        """
-        return wr.catalog.does_table_exist(
-            boto3_session=self.session,
-            database=database,
-            table=table,
-        )
-
-    def drop_table(
-        self,
-        database: str,
-        table: str,
-        remove_files: bool = True,
-    ) -> None:
-        """
-        Deletes table from a specified external database of Amazon Redshift Spectrum database, including related file from Amazon S3, if specified.
-
-        Args:
-            database (str): Catalog name in a specified external database of Amazon Redshift Spectrum.
-            table (str): Table name in a specified external database of Amazon Redshift Spectrum.
-            remove_files (bool, optional): If True, Amazon S3 file related to the table
-                will be removed. Defaults to True.
-        """
-        if remove_files:
-            table_location = wr.catalog.get_table_location(
-                boto3_session=self.session,
-                database=database,
-                table=table,
-            )
-            wr.s3.delete_objects(boto3_session=self.session, path=table_location)
-
-        wr.catalog.delete_table_if_exists(
-            boto3_session=self.session, database=database, table=table
-        )
+    @property
+    def con(self) -> redshift_connector.Connection:
+        if not self._con:
+            if self.credentials.get("credentials_secret"):
+                self._con = wr.redshift.connect(
+                    boto3_session=self.session,
+                    timeout=10,
+                    secret_id=self.credentials.get("credentials_secret"),
+                )
+            else:
+                raise ValueError(
+                    "`credentials_secret` config is required to connect to Redshift."
+                )
+        return self._con
 
     def from_df(
         self,
         df: pd.DataFrame,
         to_path: str,
-        database: str,
+        schema: str,
         table: str,
         extension: Literal[".parquet", ".csv"] = ".parquet",
         if_exists: Literal["overwrite", "overwrite_partitions", "append"] = "overwrite",
@@ -136,26 +144,35 @@ class RedshiftSpectrum(Source):
         **kwargs,
     ) -> None:
         """
-        Upload a pandas `DataFrame` to a csv or parquet file in a specified external database of Amazon Redshift Spectrum.
-            For full list of available parameters please refer to the official documentation:
-            https://aws-sdk-pandas.readthedocs.io/en/3.0.0/stubs/awswrangler.s3.to_parquet.html
-            https://aws-sdk-pandas.readthedocs.io/en/3.0.0/stubs/awswrangler.s3.to_csv.html
+        Upload a pandas `DataFrame` into a CSV or Parquet file in a specified external
+        Amazon Redshift Spectrum schema.
+
+        For a full list of available parameters, please refer to the official documentation:
+        https://aws-sdk-pandas.readthedocs.io/en/3.0.0/stubs/awswrangler.s3.to_parquet.html
+        https://aws-sdk-pandas.readthedocs.io/en/3.0.0/stubs/awswrangler.s3.to_csv.html
 
         Args:
             df (pd.DataFrame): Pandas `DataFrame`.
-            to_path (str): Path to Amazon S3 folder where the table will be located.
-                Defaults to None.
-            database (str): Catalog name in a specified external database of Amazon Redshift Spectrum.
-            table (str): Table name in a specified external database of Amazon Redshift Spectrum.
-            extension (Literal[".parquet", ".csv"], optional): Required file type. Defaults to '.parquet'.
+            to_path (str): Path to Amazon S3 folder where the table will be located. If needed,
+                a bottom-level directory named f"{table}" is automatically created, so
+                that files are always located in a folder named the same as the table.
+            schema (str): The name of the schema.
+            table (str): The name of the table to load the data into.
+            extension (Literal[".parquet", ".csv"], optional): Required file type.
+                Defaults to '.parquet'.
             if_exists (Literal["overwrite", "overwrite_partitions", "append"], optional):
-                'overwrite' to recreate table, 'overwrite_partitions' to recreate only partitions of the table,
-                'append' to add data to the table. Defaults to 'overwrite'.
+                'overwrite' to recreate the table, 'overwrite_partitions' to only recreate
+                the partitions, 'append' to append the data. Defaults to 'overwrite'.
             partition_cols (List[str], optional): List of column names that will be used to
                 create partitions. Only takes effect if dataset=True. Defaults to None.
             sep (str, optional): Field delimiter for the output file. Defaults to ','.
-            description (str, optional): Amazon Redshift Spectrum table description. Defaults to None.
+            description (str, optional): Amazon Redshift Spectrum table description.
+                Defaults to None.
         """
+
+        # Ensure files are in a directory named {table}.
+        if not to_path.rstrip("/").endswith(table):
+            to_path = os.path.join(to_path, table)
 
         if extension == ".parquet":
             wr.s3.to_parquet(
@@ -164,7 +181,7 @@ class RedshiftSpectrum(Source):
                 path=to_path,
                 mode=if_exists,
                 dataset=True,
-                database=database,
+                database=schema,
                 table=table,
                 partition_cols=partition_cols,
                 description=description,
@@ -176,7 +193,7 @@ class RedshiftSpectrum(Source):
                 df=df,
                 path=to_path,
                 dataset=True,
-                database=database,
+                database=schema,
                 table=table,
                 sep=sep,
                 **kwargs,
@@ -186,25 +203,195 @@ class RedshiftSpectrum(Source):
 
     def to_df(
         self,
-        database: str,
+        schema: str,
         table: str,
         **kwargs,
     ) -> pd.DataFrame:
         """
-        Reads table from a specified external database of Amazon Redshift Spectrum to a pandas `DataFrame`.
-            For full list of available parameters please refer to the official documentation:
-            https://aws-sdk-pandas.readthedocs.io/en/3.0.0/stubs/awswrangler.s3.read_parquet_table.html
+        Reads a table from an Amazon Redshift Spectrum external schema into a pandas `DataFrame`.
+        For a full list of available parameters, please refer to the official documentation:
+        https://aws-sdk-pandas.readthedocs.io/en/3.0.0/stubs/awswrangler.s3.read_parquet_table.html
 
         Args:
-            database (str): Catalog name in a specified external database of Amazon Redshift Spectrum.
-            table (str): Table name in a specified external database of Amazon Redshift Spectrum.
+            schema (str): The name of the schema.
+            table (str): The name of the table to load.
         """
 
         df = wr.s3.read_parquet_table(
             boto3_session=self.session,
-            database=database,
+            database=schema,
             table=table,
             **kwargs,
         )
 
         return df
+
+    def drop_table(
+        self,
+        schema: str,
+        table: str,
+        remove_files: bool = True,
+    ) -> None:
+        """
+        Drops a table from a specified Amazon Redshift Spectrum external schema,
+        including related files from Amazon S3, if specified.
+
+        Args:
+            schema (str): The name of the schema.
+            table (str): The name of the table to drop.
+            remove_files (bool, optional): If True, Amazon S3 file related to the table
+                will be removed. Defaults to True.
+        """
+        if remove_files:
+            table_location = wr.catalog.get_table_location(
+                boto3_session=self.session,
+                database=schema,
+                table=table,
+            )
+            wr.s3.delete_objects(boto3_session=self.session, path=table_location)
+
+        wr.catalog.delete_table_if_exists(
+            boto3_session=self.session, database=schema, table=table
+        )
+
+    def get_tables(
+        self,
+        schema: str,
+    ) -> List[str]:
+        """
+        Returns a list of tables in a specified schema.
+
+        Args:
+            schema (str): The name of the schema.
+        """
+        get_tables_query = f"SELECT t.tablename FROM SVV_EXTERNAL_TABLES t WHERE t.schemaname = '{schema}'"
+        with self.con.cursor() as cursor:
+            tables_info = cursor.execute(get_tables_query).fetchall()
+        return [table_info[0] for table_info in tables_info]
+
+    def _check_if_table_exists(self, schema: str, table: str) -> bool:
+        """
+        Check if a table exists in a specified Amazon Redshift Spectrum external schema.
+
+        Args:
+            schema (str): The name of the schema.
+            table (str): The name of the table to verify.
+
+        Returns:
+            bool: Whether the table exists.
+        """
+        return table in self.get_tables(schema=schema)
+
+    def create_schema(
+        self,
+        schema: str,
+        description: Optional[str] = None,
+    ) -> None:
+        """
+        Create an external schema in Amazon Redshift Spectrum.
+
+        This involves two steps:
+        - creating a Glue database
+        - creating an external schema in Redshift, pointing to above Glue database
+
+        Args:
+            schema (str): The name of the schema.
+            description (str, optional): The description of the schema. Defaults to None.
+        """
+        self._create_glue_database(
+            database=schema, description=description, exist_ok=True
+        )
+        create_schema_query = f"""
+create external schema if not exists "{schema}" from data catalog 
+database '{schema}'
+iam_role '{self.credentials.get("iam_role")}'
+region '{self.credentials.get("region_name")}'
+CREATE EXTERNAL DATABASE IF NOT EXISTS;
+"""
+        with self.con.cursor() as cursor:
+            cursor.execute(create_schema_query)
+            self.con.commit()
+
+    def _create_glue_database(
+        self,
+        database: str,
+        description: Optional[str] = None,
+        exist_ok: bool = False,
+    ):
+        """Create an AWS Glue database.
+
+        Args:
+            database (str): The name of the database.
+            description (Optional[str], optional): The description of the database.
+                Defaults to None.
+            exist_ok (bool, optional): Whether to skip if the database already exists.
+                If set to False, will throw `AlreadyExistsException`. Defaults to False.
+        """
+        wr.catalog.create_database(
+            name=database,
+            description=description,
+            boto3_session=self.session,
+            exist_ok=exist_ok,
+        )
+
+    def get_schemas(self) -> list[str]:
+        """Returns a list of schemas in the current Redshift Spectrum database."""
+
+        # External Redshift schemas
+        get_schemas_query = "SELECT schemaname FROM SVV_EXTERNAL_SCHEMAS"
+        with self.con.cursor() as cursor:
+            schema_names: Tuple[list] = cursor.execute(get_schemas_query).fetchall()
+        external_schemas = [schema_name[0] for schema_name in schema_names]
+
+        # Glue databases.
+        schema_infos = wr.catalog.get_databases(boto3_session=self.session)
+        glue_schemas = [schema_info["Name"] for schema_info in schema_infos]
+
+        # An external Redshift schema is a Spectrum schema only if it's also a Glue database.
+        return [schema for schema in external_schemas if schema in glue_schemas]
+
+    def _check_if_schema_exists(self, schema: str) -> bool:
+        """
+        Check if a schema exists in Amazon Redshift Spectrum.
+
+        Args:
+            schema (str): The name of the schema.
+
+        Returns:
+            bool: Whether the schema exists.
+        """
+        return schema in self.get_schemas()
+
+    def _is_spectrum_schema(self, schema: str) -> bool:
+        """
+        Check if a Redshift schema is a Spectrum schema.
+
+        Args:
+            schema (str): The name of the schema.
+
+        Returns:
+            bool: Whether the schema is a Spectrum schema.
+        """
+        return self._check_if_schema_exists(schema)
+
+    def drop_schema(self, schema: str, drop_glue_database: bool = False) -> None:
+        """
+        Drop a Spectrum schema. If specified, also drop the underlying Glue database.
+
+        Args:
+            schema (str): The name of the schema.
+        """
+
+        if not self._is_spectrum_schema(schema):
+            raise ValueError(f"Schema {schema} is not a Spectrum schema.")
+
+        drop_external_schema_query = f"DROP SCHEMA IF EXISTS {schema}"
+        with self.con.cursor() as cursor:
+            cursor.execute(drop_external_schema_query)
+            self.con.commit()
+
+        if drop_glue_database:
+            wr.catalog.delete_database(
+                name=schema,
+                boto3_session=self.session,
+            )
