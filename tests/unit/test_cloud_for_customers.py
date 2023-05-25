e9580a6ef298b24b2fbbcb1d1e3c3736b278b015
trymzet <michalmzawadzki@gmail.com>
2023-05-25 11:51:37 +0200

    ✅ Add test for credentials validation

--- tests/unit/test_cloud_for_customers.py
+++ tests/unit/test_cloud_for_customers.py
@@ -1,6 +1,8 @@
 """Tests for CloudForCustomers source class"""
 import pandas as pd
+import pytest
 
+from viadot.exceptions import CredentialError
 from viadot.sources.cloud_for_customers import CloudForCustomers
 
 CONTACT_ENDPOINT = "ContactCollection"
@@ -10,7 +12,29 @@ cursor_filter = f"{cursor_field} ge datetimeoffset'{cursor_value}'"
 filter_params = {"$filter": cursor_filter}
 
 
-def test_cloud_for_customers_to_df(c4c_config_key):
+def test_is_configured():
+    c4c = CloudForCustomers(
+        credentials={"username": "test_user", "password": "test_password"},
+    )
+    assert c4c
+
+
+def test_is_configured_throws_credential_error():
+    with pytest.raises(CredentialError):
+        c4c = CloudForCustomers(
+            credentials={"username": "test_user", "password": None},
+        )
+    with pytest.raises(CredentialError):
+        c4c = CloudForCustomers(
+            credentials={"username": None, "password": "test_password"},
+        )
+    with pytest.raises(CredentialError):
+        c4c = CloudForCustomers(
+            credentials={"username": None, "password": None},
+        )
+
+
+def test_to_df(c4c_config_key):
     c4c = CloudForCustomers(
         config_key=c4c_config_key,
         endpoint=CONTACT_ENDPOINT,
