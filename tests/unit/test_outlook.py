ff5c14cf2cbb581ac71227951f8229dea9d4d00e
Diego-H-S <dhidalgosoto@dyvenia.com>
2023-05-26 13:31:18 +0200

    ✅ updated test file with the new structure.

--- tests/unit/test_outlook.py
+++ tests/unit/test_outlook.py
@@ -1,5 +1,6 @@
 import os
 import pytest
+from unittest import mock
 from datetime import datetime
 
 import pandas as pd
@@ -8,21 +9,23 @@ from O365.account import Account
 from viadot.sources import Outlook
 from viadot.exceptions import CredentialError
 
-o = Outlook(
-    mailbox_name="vevoszolgalat@velux.com",
-    start_date="2023-04-12",
-    end_date="2023-04-13",
-)
+os.system("clear")
 
-o.to_df()
+# o = Outlook(
+#     mailbox_name="vevoszolgalat@velux.com",
+#     start_date="2023-02-12",
+#     end_date="2023-02-13",
+# )
+
+# o.get_all_mails_to_df()
 
 
 @pytest.fixture
 def var_dictionary():
     variables = {
         "mailbox_name": "vevoszolgalat@velux.com",
-        "start_date": "2023-04-12",
-        "end_date": "2023-04-13",
+        "start_date": "2023-01-12",
+        "end_date": "2023-01-13",
     }
 
     return variables
@@ -53,26 +56,18 @@ def test_outlook_credential_exception(var_dictionary):
         )
 
 
-@pytest.mark.exceptions
-def test_outlook_mailbox_folders_exception(var_dictionary):
-    with pytest.raises(Exception):
-        o = Outlook(
-            mailbox_name=var_dictionary["mailbox_name"],
-            start_date=var_dictionary["start_date"],
-            end_date=var_dictionary["end_date"],
-            mailbox_folders=["trial"],
-        )
-
-
+@mock.patch(
+    "viadot.sources.outlook.Outlook._get_messages_from_mailbox", return_value=("a", 1)
+)
 @pytest.mark.to_df
-def test_outlook_to_df(var_dictionary):
+def test_outlook_to_df(mock_api_response, var_dictionary):
     o = Outlook(
         mailbox_name=var_dictionary["mailbox_name"],
         start_date=var_dictionary["start_date"],
         end_date=var_dictionary["end_date"],
     )
 
-    df = o.to_df()
+    df = o.get_all_mails_to_df()
     assert isinstance(df, pd.DataFrame)
 
 
@@ -84,8 +79,8 @@ def test_outlook_to_csv(var_dictionary):
         end_date=var_dictionary["end_date"],
     )
 
-    o.to_csv()
     file_name = (
         var_dictionary["mailbox_name"].split("@")[0].replace(".", "_").replace("-", "_")
     )
+    o.to_csv(pd.read_csv(f"{file_name}.csv", sep="\t"))
     assert os.path.exists(f"{file_name}.csv")
