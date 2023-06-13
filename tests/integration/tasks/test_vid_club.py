37e9d1edcb53ebdb11706e2646d3dcf47bf73fef
cgildenia <cgildenia@dyvenia.com>
2023-06-13 15:45:44 +0200

    🚚 renamed an import

--- tests/integration/tasks/test_vid_club.py
+++ tests/integration/tasks/test_vid_club.py
@@ -1,6 +1,10 @@
 import pytest
+from unittest import mock
 import pandas as pd
-from viadot.tasks.vid_club import VeluxClubToDF
+from viadot.tasks.vid_club import VidClubToDF
+
+class MockVidClubResponse:
+    response_data = pd.DataFrame()
 
 
 @pytest.fixture(scope="session")
@@ -8,13 +12,13 @@ def var_dictionary():
     variables = {"source": "jobs", "from_date": "2022-03-23", "to_date": "2022-03-24"}
     yield variables
 
-
-def test_vid_club_to_df(var_dictionary):
+@mock.patch.object("VidClubToDF.run", mock_run=MockVidClubResponse.response_data)
+def test_vid_club_to_df(mock_run, var_dictionary):
     source = var_dictionary["source"]
     from_date = var_dictionary["from_date"]
     to_date = var_dictionary["to_date"]
 
-    vc_to_df = VeluxClubToDF(
+    vc_to_df = VidClubToDF(
         source=source,
         to_date=to_date,
         from_date=from_date,
