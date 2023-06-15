3d8f317d234e9a7c73ac06bf15c7c0057d9cd962
Rafalz13 <rziemianek@dyvenia.com>
2023-06-15 12:53:37 +0200

    🐛 Changed the method name

--- tests/integration/tasks/test_outlook.py
+++ tests/integration/tasks/test_outlook.py
@@ -22,14 +22,13 @@ def to_df():
 
 @pytest.mark.init
 def test_outlook_to_df():
-
     outlook = Outlook(
         mailbox_name=outlook_env_vars["mail_example"],
         credentials=outlook_env_vars,
         start_date="2022-04-28",
         end_date="2022-04-29",
     )
-    df = outlook.to_df()
+    df = outlook.get_all_mails_to_df()
 
     assert isinstance(df, pd.DataFrame)
 
@@ -48,7 +47,7 @@ def test_outlook_credentials():
 @mock.patch("O365.Account", return_value=None)
 @mock.patch("O365.Account.authenticate", return_value="trial1")
 @mock.patch("O365.Account.mailbox", return_value="trial2")
-@mock.patch("viadot.sources.Outlook.to_df", return_calue=to_df)
+@mock.patch("viadot.sources.Outlook.get_all_mails_to_df", return_calue=to_df)
 @pytest.mark.task
 def test_outlook_task(mock_account, mock_auth, mock_mail, mock_to_df):
     outlook_to_df = OutlookToDF(credentials=outlook_env_vars)
