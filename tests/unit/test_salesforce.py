01b56830468ee9f69aa1905ddfcf2fa7fc5628d5
burzekj <jburzec@dyvenia.com>
2023-06-20 14:16:11 +0200

    ✅ [migration2viatot2.0]: Migrated SalesForce

---
+++ tests/unit/test_salesforce.py
@@ -0,0 +1,180 @@
+import pandas as pd
+import pytest
+from viadot.sources import SalesForce
+
+
+@pytest.fixture(scope="session")
+def salesforce():
+
+    s = SalesForce(config_key="salesforce")
+
+    yield s
+
+    sf = s.salesforce
+    result = sf.query(
+        f"SELECT Id FROM {TABLE_TO_UPSERT} WHERE SAPContactId__c in ('8812000','8812100','8811111') "
+    )
+
+    # Deletes test rows from Salesforce if any remain
+    nr_rows = len(result["records"])
+    for nr in range(nr_rows):
+        sf.Contact.delete(result["records"][nr]["Id"])
+
+
+TABLE_TO_DOWNLOAD = "Account"
+TABLE_TO_UPSERT = "Contact"
+TEST_LAST_NAME = "viadot-test"
+
+EXPECTED_VALUE = pd.DataFrame(
+    {"LastName": ["salesforce-test"], "SAPContactId__c": ["8811111"]}
+)
+
+TEST_ROW = {"LastName": "salesforce-test", "SAPContactId__c": "8811111"}
+
+TWO_TEST_ROWS_INSERT = {
+    "LastName": ["viadot-insert-1", "viadot-insert-2"],
+    "SAPContactId__c": ["8812000", "8812100"],
+}
+
+TWO_TEST_ROWS_UPDATE = {
+    "LastName": ["viadot-update-1", "viadot-update-2"],
+    "SAPContactId__c": ["8812000", "8812100"],
+}
+
+
+def get_tested_records(salesforce, multiple_rows=False):
+    # Test query to get specific records.
+    sf = salesforce.salesforce
+    if multiple_rows:
+        result = sf.query(
+            f"SELECT Id, LastName FROM {TABLE_TO_UPSERT} WHERE SAPContactId__c in ('8812000','8812100')"
+        )
+    else:
+        result = sf.query(
+            f"SELECT Id, LastName FROM {TABLE_TO_UPSERT} WHERE SAPContactId__c='8811111'"
+        )
+
+    return result["records"]
+
+
+def test_upsert_row_id(salesforce):
+    # Test query to get specific row_id.
+    assert not get_tested_records(salesforce)
+
+    sf = salesforce.salesforce
+    sf.Contact.create(TEST_ROW)
+
+    data = {
+        "Id": [get_tested_records(salesforce)[0]["Id"]],
+        "LastName": [TEST_LAST_NAME],
+    }
+    df = pd.DataFrame(data=data)
+
+    salesforce.upsert(df=df, table=TABLE_TO_UPSERT)
+
+    updated_row = get_tested_records(salesforce)
+
+    assert updated_row[0]["LastName"] == TEST_LAST_NAME
+
+    sf.Contact.delete(updated_row[0]["Id"])
+
+
+def test_upsert(salesforce):
+    # Test function which returns single record from SalesForce.
+    assert not get_tested_records(salesforce, multiple_rows=True)
+
+    df_insert = pd.DataFrame(data=TWO_TEST_ROWS_INSERT)
+
+    salesforce.upsert(
+        df=df_insert, table="Contact", external_id_column="SAPContactId__c"
+    )
+
+    inserted_rows = get_tested_records(salesforce, multiple_rows=True)
+
+    assert inserted_rows[0]["LastName"] == "viadot-insert-1"
+    assert inserted_rows[1]["LastName"] == "viadot-insert-2"
+
+    df_update = pd.DataFrame(data=TWO_TEST_ROWS_UPDATE)
+
+    salesforce.upsert(
+        df=df_update, table="Contact", external_id_column="SAPContactId__c"
+    )
+
+    updated_rows = get_tested_records(salesforce, multiple_rows=True)
+    assert updated_rows[0]["LastName"] == "viadot-update-1"
+    assert updated_rows[1]["LastName"] == "viadot-update-2"
+
+    sf = salesforce.salesforce
+    sf.Contact.delete(inserted_rows[0]["Id"])
+    sf.Contact.delete(inserted_rows[1]["Id"])
+
+
+def test_bulk_upsert(salesforce):
+    # Test function which returns multiple record from SalesForce.
+    assert not get_tested_records(salesforce, multiple_rows=True)
+
+    df_insert = pd.DataFrame(data=TWO_TEST_ROWS_INSERT)
+
+    salesforce.bulk_upsert(
+        df=df_insert, table="Contact", external_id_column="SAPContactId__c"
+    )
+
+    inserted_rows = get_tested_records(salesforce, multiple_rows=True)
+
+    assert inserted_rows[0]["LastName"] == "viadot-insert-1"
+    assert inserted_rows[1]["LastName"] == "viadot-insert-2"
+
+    df_update = pd.DataFrame(data=TWO_TEST_ROWS_UPDATE)
+
+    salesforce.bulk_upsert(
+        df=df_update, table="Contact", external_id_column="SAPContactId__c"
+    )
+
+    updated_rows = get_tested_records(salesforce, multiple_rows=True)
+    assert updated_rows[0]["LastName"] == "viadot-update-1"
+    assert updated_rows[1]["LastName"] == "viadot-update-2"
+
+    sf = salesforce.salesforce
+    sf.Contact.delete(updated_rows[0]["Id"])
+    sf.Contact.delete(updated_rows[1]["Id"])
+
+
+def test_upsert_incorrect_external_id_column(salesforce):
+    # Test function for inncorect external_id_column for upsert function.
+    data = {
+        "LastName": [TEST_LAST_NAME],
+        "SAPContactId__c": ["8811111"],
+    }
+    df = pd.DataFrame(data=data)
+    with pytest.raises(ValueError):
+        salesforce.upsert(df=df, table=TABLE_TO_UPSERT, external_id_column="SAPId")
+
+
+def test_download_no_query(salesforce):
+    # Testing donwload function with no query provided.
+    records = salesforce.download(table=TABLE_TO_DOWNLOAD)
+    assert len(records) > 0
+
+
+def test_download_with_query(salesforce):
+    # Testing donwload function with query provided.
+    query = f"SELECT Id, Name FROM {TABLE_TO_DOWNLOAD} LIMIT 10"
+    records = salesforce.download(query=query)
+    assert len(records) == 10
+
+
+def test_get_response(salesforce):
+    # Testing function which returns pd.DataFrame from SalesForce depended
+    # on provided query.
+    assert not get_tested_records(salesforce)
+
+    sf = salesforce.salesforce
+    sf.Contact.create(TEST_ROW)
+
+    df = salesforce.get_response(
+        query=f"SELECT LastName, SAPContactId__c FROM {TABLE_TO_UPSERT} WHERE SAPContactId__c='8811111'"
+    )
+
+    assert df.equals(EXPECTED_VALUE)
+
+    sf.Contact.delete(get_tested_records(salesforce)[0]["Id"])
---
+++ viadot/sources/salesforce.py
@@ -0,0 +1,255 @@
+from typing import List, Literal, OrderedDict
+
+import pandas as pd
+from simple_salesforce import Salesforce as SF
+from simple_salesforce.exceptions import SalesforceMalformedRequest
+from viadot.config import get_source_credentials
+from viadot.exceptions import CredentialError
+from viadot.sources.base import Source
+
+from pydantic import BaseModel
+
+class SalesForceCredentials(BaseModel):
+    username: str # Name of user
+    password: str # Password for the user
+    token: str    # User token
+    
+class SalesForce(Source):
+    """
+    A class for download and upsert data from Salesforce.
+
+    Args:
+        domain (str, optional): Domain of a connection. Defaults to 'test' (sandbox).
+            Can only be add if a username/password/security token is provide.
+        client_id (str, optional): Client id, keep track of API calls. 
+            Defaults to 'viadot'.
+        env (Literal["DEV", "QA", "PROD"], optional): Environment information, 
+            provides information about credential and connection configuration. 
+            Defaults to 'DEV'.
+        credentials (Dict[str, Any], optional): Credentials to connect with 
+            Salesforce. If not provided, will read from local config file. 
+            Defaults to None.
+        config_key (str, optional): The key in the viadot config holding relevant 
+            credentials. Defaults to None.
+    """
+
+    def __init__(
+        self,
+        *args,
+        domain: str = "test",
+        client_id: str = "viadot",
+        env: Literal["DEV", "QA", "PROD"] = "DEV",
+        credentials: SalesForceCredentials = None,
+        config_key: str = None,
+        **kwargs,
+    ):
+
+        credentials = credentials or get_source_credentials(config_key)
+
+        if credentials is None:
+            raise CredentialError("Please specify the credentials.")
+        
+        SalesForceCredentials(**credentials) # validate the credentials schema
+        super().__init__(*args, credentials=credentials, **kwargs)
+
+        if env.upper() == "DEV" or env.upper() == "QA":
+            self.salesforce = SF(
+                username=self.credentials["username"],
+                password=self.credentials["password"],
+                security_token=self.credentials["token"],
+                domain=domain,
+                client_id=client_id,
+            )
+
+        elif env.upper() == "PROD":
+            self.salesforce = SF(
+                username=self.credentials["username"],
+                password=self.credentials["password"],
+                security_token=self.credentials["token"],
+            )
+
+        else:
+            raise ValueError("The only available environments are DEV, QA, and PROD.")
+
+    def upsert(
+        self,
+        df: pd.DataFrame,
+        table: str,
+        external_id_column: str = None,
+        raise_on_error: bool = False,
+    ) -> None:
+        """
+        Upsert the DataFrame to Salesforce. The upsert is performed on a single record
+        at a time. Using an upsert operation gives more control over logging and error
+        handling than using bulk upsert.
+
+        Args:
+            df (pd.DataFrame): Pandas DataFrame specified the rows to upsert.
+            table (str): The table where the data should be upserte.
+            external_id_column (str, optional): The external ID to use for the upsert. 
+                Defaults to None.
+            raise_on_error (bool, optional): Whether to raise an exception if a row upsert fails.
+                If False, we only display a warning. Defaults to False.
+        """
+        if df.empty:
+            self.logger.info("No data to upsert.")
+            return
+
+        if external_id_column and external_id_column not in df.columns:
+            raise ValueError(
+                f"Passed DataFrame does not contain column '{external_id_column}'."
+            )
+
+        table_to_upsert = getattr(self.salesforce, table)
+        records = df.to_dict("records")
+        records_cp = records.copy()
+
+        for record in records_cp:
+
+            if external_id_column:
+                # If the specified external ID is on the upsert line and has a value, it will 
+                # be used as merge_key
+                if record[external_id_column] is not None:
+                    merge_key = f"{external_id_column}/{record[external_id_column]}"
+                    record.pop(external_id_column)
+            else:
+                merge_key = record.pop("Id")
+
+            try:
+                response_code = table_to_upsert.upsert(data=record, record_id=merge_key)
+            except SalesforceMalformedRequest as e:
+                msg = f"Upsert of record {merge_key} failed."
+                if raise_on_error:
+                    raise ValueError(msg) from e
+                else:
+                    self.logger.warning(msg)
+
+            valid_response_codes = {200: "updated", 201: "created", 204: "updated"}
+
+            if response_code not in valid_response_codes:
+                msg = f"Upsert failed for record: \n{record} with response code {response_code }"
+                if raise_on_error:
+                    raise ValueError(msg)
+                else:
+                    self.logger.warning(msg)
+            else:
+                self.logger.info(
+                    f"Successfully {valid_response_codes[response_code]} record {merge_key}."
+                )
+
+        self.logger.info(
+            f"Successfully upserted {len(records)} records into table '{table}'."
+        )
+
+    def bulk_upsert(
+        self,
+        df: pd.DataFrame,
+        table: str,
+        external_id_column: str = None,
+        batch_size: int = 10000,
+        raise_on_error: bool = False,
+    ) -> None:
+        """
+        Performs a bulk upsert to Salesforce of the data given in the DataFrame.
+        Bulk upsert is performed on multiple records simultaneously, it is usually 
+        used when there is a need to insert or update multiple records in a single 
+        transaction, which can be more efficient and reduce the number of API calls 
+        required.
+
+        Args:
+            df (pd.DataFrame): Pandas DataFrame specified the rows to bulk upsert.
+            table (str): The table where the data should be upserted.
+            external_id_column (str, optional): The external ID to use for the upsert. 
+            Defaults to None.
+            batch_size (int, optional): Number of records to be included in each batch
+                of records that are sent to the Salesforce API for processing. 
+                Defaults to 10000.
+            raise_on_error (bool, optional): Whether to raise an exception if a row 
+                upsert fails. If False, we only display a warning. Defaults to False.
+        """
+        if df.empty:
+            self.logger.info("No data to upsert.")
+            return
+
+        if external_id_column and external_id_column not in df.columns:
+            raise ValueError(
+                f"Passed DataFrame does not contain column '{external_id_column}'."
+            )
+        records = df.to_dict("records")
+
+        try:
+            response_code = self.salesforce.bulk.__getattr__(table).upsert(
+                data=records,
+                external_id_field=external_id_column,
+                batch_size=batch_size,
+            )
+        except SalesforceMalformedRequest as e:
+            # Bulk upsert don't work at all.
+            raise ValueError(f"Upsert of records failed: {e}") from e
+
+        self.logger.info(f"Successfully upserted bulk records.")
+
+        if any(result.get("success") is not True for result in response_code):
+            # Upsert of some individual records failed.
+            failed_records = [
+                result for result in response_code if result.get("success") is not True
+            ]
+            msg = f"Upsert failed for records {failed_records} with response code {response_code}"
+            if raise_on_error:
+                raise ValueError(msg)
+            else:
+                self.logger.warning(msg)
+
+        self.logger.info(
+            f"Successfully upserted {len(records)} records into table '{table}'."
+        )
+
+    def download(
+        self, query: str = None, table: str = None, columns: List[str] = None
+    ) -> List[OrderedDict]:
+        """
+        Download all data from the indicated table or the result of the specified query.
+
+        Args:
+            query (str, optional): The query to be used to download the data. 
+            Defaults to None.
+            table (str, optional): Table name. Defaults to None.
+            columns (List[str], optional): List of required columns. 
+                Requires `table` to be specified. Defaults to None.
+
+        Returns:
+            List[OrderedDict]: Selected rows from Salesforce.
+        """
+        if not query:
+            if columns:
+                columns_str = ", ".join(columns)
+            else:
+                columns_str = "FIELDS(STANDARD)"
+            query = f"SELECT {columns_str} FROM {table}"
+        records = self.salesforce.query(query).get("records")
+        # Remove metadata from the data
+        _ = [record.pop("attributes") for record in records]
+        return records
+    
+    def get_response(
+        self,
+        query: str = None,
+        table: str = None,
+        columns: List[str] = None,
+    ) -> pd.DataFrame:
+        """
+        Download the indicate data and return the DataFrame.
+
+        Args:
+            query (str, optional): The query to be used to download the data. 
+                Defaults to None.
+            table (str, optional): Table name. Defaults to None.
+            columns (List[str], optional): List of required columns. 
+                Requires `table` to be specified. Defaults to None.
+
+        Returns:
+            pd.DataFrame: Selected rows from Salesforce.
+        """
+        records = self.download(query=query, table=table, columns=columns)
+
+        return pd.DataFrame(records)
