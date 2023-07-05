da6262476a4529eb5b59a63a88a8d8430fcfe5f5
hha.ext <hha.ext@velux.com>
2023-07-05 13:56:28 +0200

    update tests

--- tests/integration/test_vid_club.py
+++ tests/integration/test_vid_club.py
@@ -4,8 +4,11 @@ import pandas as pd
 import pytest
 
 from viadot.exceptions import ValidationError
+from viadot.task_utils import credentials_loader
 from viadot.sources import VidClub
 
+
+CREDENTIALS = credentials_loader.run(credentials_secret="VIDCLUB")
 
 @pytest.fixture
 def var_dictionary():
@@ -21,25 +24,22 @@ class MockClass:
         df = pd.DataFrame()
         return df
 
+@pytest.mark.init
+def test_default_credential_param():
+    vc = VidClub(credentials = CREDENTIALS)
+    assert vc.credentials != None and type(vc.credentials) == dict
 
 @pytest.mark.init
 def test_create_club_class():
-    vc = VidClub()
+    vc = VidClub(credentials = CREDENTIALS)
     assert vc
-
-
-@pytest.mark.init
-def test_default_credential_param():
-    vc = VidClub()
-    assert vc.credentials != None and type(vc.credentials) == dict
-
 
 @pytest.mark.proper
 def test_build_query_wrong_source():
     with pytest.raises(
         ValidationError, match=r"Pick one these sources: jobs, product, company, survey"
     ):
-        vc = VidClub()
+        vc = VidClub(credentials = CREDENTIALS)
         query = vc.build_query(
             source="test",
             from_date="2023-03-24",
@@ -54,7 +54,7 @@ def test_get_response_wrong_source():
     with pytest.raises(
         ValidationError, match=r"The source has to be: jobs, product, company or survey"
     ):
-        vc = VidClub()
+        vc = VidClub(credentials = CREDENTIALS)
         query = vc.get_response(source="test")
 
 
@@ -64,7 +64,7 @@ def test_get_response_wrong_source():
 @pytest.mark.parametrize("source", ["jobs", "company", "product", "survey"])
 @pytest.mark.proper
 def test_get_response_sources(mock_api_response, source):
-    vc = VidClub()
+    vc = VidClub(credentials = CREDENTIALS)
     query = vc.get_response(source=source, to_date="2023-03-24", from_date="2023-03-24")
 
     assert isinstance(query, pd.DataFrame)
@@ -75,7 +75,7 @@ def test_get_response_wrong_date():
     with pytest.raises(
         ValidationError, match=r"from_date cannot be earlier than 2023-03-22!!"
     ):
-        vc = VidClub()
+        vc = VidClub(credentials = CREDENTIALS)
         query = vc.get_response(source="jobs", from_date="2021-05-09")
 
 
@@ -84,7 +84,7 @@ def test_get_response_wrong_date_range():
     with pytest.raises(
         ValidationError, match=r"to_date cannot be earlier than from_date!"
     ):
-        vc = VidClub()
+        vc = VidClub(credentials = CREDENTIALS)
         query = vc.get_response(
             source="jobs", to_date="2022-05-04", from_date="2022-05-05"
         )
