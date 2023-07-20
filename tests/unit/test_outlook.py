4d2bddf6ebdefe92a967ae68cd7fe373adbe6e6a
Diego-H-S <dhidalgosoto@dyvenia.com>
2023-07-20 09:54:38 +0200

    ✅ updated outlook test file.

--- tests/unit/test_outlook.py
+++ tests/unit/test_outlook.py
@@ -1,23 +1,11 @@
 import os
 import pytest
 from unittest import mock
-from datetime import datetime
 
 import pandas as pd
 from O365.account import Account
 
 from viadot.sources import Outlook
-from viadot.exceptions import CredentialError
-
-os.system("clear")
-
-# o = Outlook(
-#     mailbox_name="vevoszolgalat@velux.com",
-#     start_date="2023-02-12",
-#     end_date="2023-02-13",
-# )
-
-# o.get_all_mails_to_df()
 
 
 @pytest.fixture
@@ -26,61 +14,73 @@ def var_dictionary():
         "mailbox_name": "random@random.com",
         "start_date": "2023-01-12",
         "end_date": "2023-01-13",
+        "credentials": {
+            "client_id": "abcdefghijklmnopqrstuvwxyz",
+            "client_secret": "abcdefghijklmnopqrstuvwxyz",
+            "tenant_id": "abcdefghijklmnopqrstuvwxyz",
+            "mail_example": "abcdefghijklmnopqrstuvwxyz",
+        },
     }
 
     return variables
 
 
+class MockClass:
+    mailbox_name = "Trial and error"
+
+    def authenticate():
+        return True
+
+    def mailbox():
+        return None
+
+
 @pytest.mark.basics
 def test_outlook_credentials(var_dictionary):
-    o = Outlook(mailbox_name=var_dictionary["mailbox_name"])
+    o = Outlook(
+        mailbox_name=var_dictionary["mailbox_name"],
+        credentials=var_dictionary["credentials"],
+    )
 
     assert all(
         [
             isinstance(o.credentials, dict),
-            isinstance(o.date_range_end_time, datetime),
-            isinstance(o.date_range_start_time, datetime),
             isinstance(o.account, Account),
         ]
     )
 
 
 @pytest.mark.exceptions
-def test_outlook_credential_exception(var_dictionary):
-    with pytest.raises(CredentialError):
+def test_outlook_mailbox_folders_exception(var_dictionary):
+    with pytest.raises(Exception):
         o = Outlook(
             mailbox_name=var_dictionary["mailbox_name"],
             start_date=var_dictionary["start_date"],
             end_date=var_dictionary["end_date"],
+            mailbox_folders=["trial"],
             credentials=[9],
         )
 
 
-@mock.patch(
-    "viadot.sources.outlook.Outlook._get_messages_from_mailbox", return_value=("a", 1)
-)
-@pytest.mark.to_df
-def test_outlook_to_df(mock_api_response, var_dictionary):
-    o = Outlook(
-        mailbox_name=var_dictionary["mailbox_name"],
-        start_date=var_dictionary["start_date"],
-        end_date=var_dictionary["end_date"],
-    )
-
-    df = o.get_all_mails_to_df()
-    assert isinstance(df, pd.DataFrame)
-
-
+@mock.patch.object(Outlook, "get_all_mails_to_df", return_value=pd.DataFrame())
+@mock.patch("viadot.sources.outlook.Account", return_value=MockClass)
 @pytest.mark.to_csv
-def test_outlook_to_csv(var_dictionary):
+def test_outlook_to_csv(mock_method_Outlook, mock_api_Account, var_dictionary):
     o = Outlook(
         mailbox_name=var_dictionary["mailbox_name"],
         start_date=var_dictionary["start_date"],
         end_date=var_dictionary["end_date"],
+        credentials=var_dictionary["credentials"],
     )
 
     file_name = (
         var_dictionary["mailbox_name"].split("@")[0].replace(".", "_").replace("-", "_")
     )
-    o.to_csv(pd.read_csv(f"{file_name}.csv", sep="\t"))
+    df = o.get_all_mails_to_df()
+    o.to_csv(df)
+    file_name = (
+        var_dictionary["mailbox_name"].split("@")[0].replace(".", "_").replace("-", "_")
+    )
+    mock_method_Outlook.assert_called()
+    mock_api_Account.assert_called()
     assert os.path.exists(f"{file_name}.csv")
