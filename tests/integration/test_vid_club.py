12468df49f4ce0fdb750c1407975d47857ed5e19
hha.ext <hha.ext@velux.com>
2023-07-21 10:41:02 +0200

    add tests

--- tests/integration/test_vid_club.py
+++ tests/integration/test_vid_club.py
@@ -10,7 +10,8 @@ from viadot.sources import VidClub
 
 
 CREDENTIALS = credentials_loader.run(credentials_secret="VIDCLUB")
-vc = VidClub(credentials = CREDENTIALS)
+vc = VidClub(credentials=CREDENTIALS)
+
 
 @pytest.fixture
 def var_dictionary():
@@ -26,13 +27,16 @@ class MockClass:
         df = pd.DataFrame()
         return df
 
+
 @pytest.mark.init
 def test_default_credential_param():
     assert vc.credentials != None and type(vc.credentials) == dict
 
+
 @pytest.mark.init
 def test_create_club_class():
     assert vc
+
 
 @pytest.mark.proper
 def test_build_query_wrong_source():
@@ -46,6 +50,41 @@ def test_build_query_wrong_source():
             api_url="test",
             items_per_page=1,
         )
+
+
+@pytest.mark.intervals
+def test_intervals_split():
+    from_date = "2022-01-01"
+    to_date = "2022-01-19"
+    days_interval = 5
+    expected_starts = ["2022-01-01", "2022-01-06", "2022-01-11", "2022-01-16"]
+    expected_ends = ["2022-01-06", "2022-01-11", "2022-01-16", "2022-01-19"]
+    starts, ends = vc.intervals(
+        from_date=from_date, to_date=to_date, days_interval=days_interval
+    )
+
+    assert starts == expected_starts
+    assert ends == expected_ends
+
+
+@mock.patch("viadot.sources.vid_club.VidClub.check_connection")
+@pytest.mark.connection_check
+def test_check_connection(mock_function):
+    mock_first_output = "Mocked Output"
+    mock_second_output = "Mocked Output"
+    mock_function.return_value = mock_first_output, mock_function.return_value[1]
+
+    response, first_url = vc.check_connection(
+        source="jobs",
+        from_date="2023-03-24",
+        to_date="2023-03-24",
+        url="test",
+        items_per_page=1,
+    )
+    print(response)
+    print(first_url)
+    assert isinstance(first_url, str)
+    assert len(first_url) > len(mock_second_output)
 
 
 @pytest.mark.proper
@@ -65,6 +104,7 @@ def test_get_response_sources(mock_api_response, source):
     query = vc.get_response(source=source, to_date="2023-03-24", from_date="2023-03-24")
 
     assert isinstance(query, pd.DataFrame)
+    mock_api_response.assert_called_once()
 
 
 @pytest.mark.proper
@@ -84,52 +124,56 @@ def test_get_response_wrong_date_range():
             source="jobs", to_date="2022-05-04", from_date="2022-05-05"
         )
 
-@pytest.mark.test_intervals_split
-def test_intervals_split():
-    from_date = "2022-01-01"
-    to_date = "2022-01-19"
-    days_interval = 5
-    expected_starts = ["2022-01-01", "2022-01-06", "2022-01-11", "2022-01-16"]
-    expected_ends = ["2022-01-06", "2022-01-11", "2022-01-16", "2022-01-19"]
-    starts, ends = vc.intervals(from_date = from_date, to_date = to_date, days_interval = days_interval)
 
-    assert starts == expected_starts
-    assert ends == expected_ends
-
-@pytest.mark.test_total_load_for_the_same_dates
+@pytest.mark.total_load
 def test_total_load_for_the_same_dates():
     from_date = "2022-04-01"
     to_date = "2022-04-01"
     days_interval = 10
     source = "jobs"
-    df = vc.total_load(from_date=from_date, to_date=to_date, days_interval = days_interval, source=source)
+    df = vc.total_load(
+        from_date=from_date, to_date=to_date, days_interval=days_interval, source=source
+    )
 
     assert isinstance(df, pd.DataFrame)
 
-@pytest.mark.test_total_load_for_intervals
+
+@pytest.mark.total_load
 def test_total_load_for_intervals():
     from_date = "2022-04-01"
     to_date = "2022-04-12"
     days_interval = 2
     source = "jobs"
-    
-    date_object = datetime.strptime(from_date, "%Y-%m-%d") + timedelta(days=days_interval)
+
+    date_object = datetime.strptime(from_date, "%Y-%m-%d") + timedelta(
+        days=days_interval
+    )
     one_interval = date_object.strftime("%Y-%m-%d")
-    
-    df = vc.total_load(from_date=from_date, to_date=to_date, days_interval = days_interval, source=source)
-    df_one_interval = vc.total_load(from_date=from_date, to_date=one_interval, days_interval = days_interval, source=source)
+
+    df = vc.total_load(
+        from_date=from_date, to_date=to_date, days_interval=days_interval, source=source
+    )
+    df_one_interval = vc.total_load(
+        from_date=from_date,
+        to_date=one_interval,
+        days_interval=days_interval,
+        source=source,
+    )
 
     assert isinstance(df, pd.DataFrame)
     assert len(df) > len(df_one_interval)
 
-@pytest.mark.test_drop_duplicates
+
+@pytest.mark.total_load
 def test_drop_duplicates():
     from_date = "2022-04-01"
     to_date = "2022-04-12"
     days_interval = 2
     source = "jobs"
 
-    df = vc.total_load(from_date=from_date, to_date=to_date, days_interval = days_interval, source=source)
+    df = vc.total_load(
+        from_date=from_date, to_date=to_date, days_interval=days_interval, source=source
+    )
     dups_mask = df.duplicated()
     df_check = df[dups_mask]
 
