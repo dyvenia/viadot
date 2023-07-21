5641aa14acb1bbdc210020585929095add26cc00
hha.ext <hha.ext@velux.com>
2023-07-21 11:59:21 +0200

    update tests

--- tests/integration/tasks/test_vid_club.py
+++ tests/integration/tasks/test_vid_club.py
@@ -4,6 +4,9 @@ import pandas as pd
 import pytest
 
 from viadot.tasks import VidClubToDF
+from viadot.task_utils import credentials_loader
+
+CREDENTIALS = credentials_loader.run(credentials_secret="VIDCLUB")
 
 
 class MockVidClubResponse:
@@ -12,7 +15,13 @@ class MockVidClubResponse:
 
 @pytest.fixture(scope="session")
 def var_dictionary():
-    variables = {"source": "jobs", "from_date": "2022-03-23", "to_date": "2022-03-24"}
+    variables = {
+        "source": "jobs",
+        "from_date": "2022-03-23",
+        "to_date": "2022-03-24",
+        "items_per_page": 1,
+        "days_interval": 1,
+    }
     yield variables
 
 
@@ -23,12 +32,16 @@ def test_vid_club_to_df(var_dictionary):
     source = var_dictionary["source"]
     from_date = var_dictionary["from_date"]
     to_date = var_dictionary["to_date"]
+    items_per_page = var_dictionary["items_per_page"]
+    days_interval = var_dictionary["days_interval"]
 
-    vc_to_df = VidClubToDF(
+    vc_to_df = VidClubToDF(credentials=CREDENTIALS)
+    df = vc_to_df.run(
         source=source,
         to_date=to_date,
         from_date=from_date,
+        items_per_page=items_per_page,
+        days_interval=days_interval,
     )
-    df = vc_to_df.run()
 
     assert isinstance(df, pd.DataFrame)
