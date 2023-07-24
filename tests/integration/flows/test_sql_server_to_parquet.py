0e2104989726217148132adaf2aed33c8a0201d2
Angelika Tarnawa <angelikat26@gmail.com>
2023-07-24 09:40:45 +0200

    ✅ Added assert to check if file is created

--- tests/integration/flows/test_sql_server_to_parquet.py
+++ tests/integration/flows/test_sql_server_to_parquet.py
@@ -1,15 +1,31 @@
+import pytest
+import os
 from prefect import Flow
 
 from viadot.tasks import SQLServerToDF
 from viadot.flows import SQLServerToParquet
+from viadot.tasks.sql_server import SQLServerQuery
+
+SCHEMA = "sandbox"
+TABLE = "test"
+PATH = "test.parquet"
 
 
-def test_sql_server_to_parquet_flow():
+@pytest.fixture(scope="session")
+def create_table():
+    query_task = SQLServerQuery("AZURE_SQL")
+    query_task.run(f"DROP TABLE IF EXISTS {SCHEMA}.{TABLE}")
+    query_task.run(f"CREATE TABLE {SCHEMA}.{TABLE} (Id INT, Name VARCHAR (10))")
+    yield True
+
+
+def test_sql_server_to_parquet_flow(create_table):
     flow = SQLServerToParquet(
         name="test_flow",
-        sql_query="SELECT * FROM your_table",
-        local_file_path="test.parquet",
+        sql_query=f"SELECT * FROM {SCHEMA}.{TABLE}",
+        local_file_path=PATH,
         if_exists="fail",
+        sqlserver_config_key="AZURE_SQL",
         timeout=3600,
     )
     flow.gen_flow()
@@ -18,3 +34,6 @@ def test_sql_server_to_parquet_flow():
     tasks = list(flow.tasks)
 
     assert isinstance(tasks[0], SQLServerToDF)
+    flow.run()
+    assert os.path.isfile(PATH) == True
+    os.remove(PATH)
