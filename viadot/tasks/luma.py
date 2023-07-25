07e1e2e3ad91fdaee1e28390d9a28db5eaa222f0
Rafalz13 <rziemianek@dyvenia.com>
2023-07-25 14:34:25 +0200

    📝 Added docstring to LumaIngest task

--- viadot/tasks/luma.py
+++ viadot/tasks/luma.py
@@ -6,6 +6,8 @@ from .azure_key_vault import AzureKeyVaultSecret
 
 
 class LumaIngest(ShellTask):
+    """Tasks for interacting with [Luma](https://github.com/dyvenia/luma)."""
+
     def __init__(
         self,
         metadata_dir_path: str,
@@ -15,6 +17,18 @@ class LumaIngest(ShellTask):
         *args,
         **kwargs,
     ) -> None:
+        """
+        Runs Luma ingestion by sending dbt artifacts to Luma ingestion API.
+
+        Args:
+            metadata_dir_path (str): The path to the directory containing metadata files.
+                In the case of dbt, it's dbt project's `target` directory, which contains dbt artifacts
+                (`sources.json`, `catalog.json`, `manifest.json`, and `run_results.json`).
+            endpoint (str, optional): The endpoint of the Luma ingestion API. Defaults to None.
+            credentials_secret (str, optional): The name of the Azure Key Vault secret containing a Luma credentials.
+                Defaults to None.
+            vault_name (str, optional): The name of the vault from which to obtain the secrets. Defaults to None.
+        """
         super().__init__(*args, **kwargs)
 
         if not endpoint:
