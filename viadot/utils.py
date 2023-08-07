b320982ae4ad89365dbbf0ebc478d1294daf85e6
Rafalz13 <rziemianek@dyvenia.com>
2023-08-07 15:49:16 +0200

    ✨ Added `add_viadot_metadata_columns` function

--- viadot/utils.py
+++ viadot/utils.py
@@ -1,7 +1,8 @@
+import functools
 import os
 import re
 from itertools import chain
-from typing import Any, Dict, List, Literal
+from typing import Any, Callable, Dict, List, Literal
 
 import pandas as pd
 import prefect
@@ -181,7 +182,6 @@ def get_sql_server_table_dtypes(
 
 
 def _cast_df_cols(df):
-
     df = df.replace({"False": False, "True": True})
 
     datetime_cols = (col for col, dtype in df.dtypes.items() if dtype.kind == "M")
@@ -290,7 +290,6 @@ def gen_bulk_insert_query_from_df(
         )
 
     def _gen_insert_query_from_records(records: List[tuple]) -> str:
-
         tuples = map(str, tuple(records))
 
         # Change Nones to NULLs
@@ -407,3 +406,23 @@ def check_if_empty_file(
     elif os.path.splitext(path)[1] == ".parquet":
         if pyarrow.parquet.read_metadata(path).num_columns == 0:
             handle_if_empty_file(if_empty, message=f"Input file - '{path}' is empty.")
+
+
+def add_viadot_metadata_columns(func: Callable) -> Callable:
+    """
+    Decorator that adds metadata columns to df in 'to_df' method.
+    For now only _viadot_source is available because _viadot_downloaded_at_utc is added on the Flow level.
+    """
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs) -> pd.DataFrame:
+        df = func(*args, **kwargs)
+
+        # Accessing instance
+        instance = args[0]
+        _viadot_source = kwargs.get("source_name") or instance.__class__.__name__
+        df["_viadot_source"] = _viadot_source
+        # df["_viadot_downloaded_at_utc"] = datetime.now(timezone.utc).replace(microsecond=0)
+        return df
+
+    return wrapper
