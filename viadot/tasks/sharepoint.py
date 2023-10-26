d6590aed9a7fe955f9153bf7f8819f74bddbc4b6
marcinpurtak <mpurtak@gmail.com>
2023-10-26 13:08:14 +0200

    Sharepoint list connector

--- viadot/tasks/sharepoint.py
+++ viadot/tasks/sharepoint.py
@@ -1,16 +1,17 @@
+from typing import List
+import pandas as pd
 import copy
 import json
 import os
-from typing import List
+import re
 
-import pandas as pd
 from prefect import Task
 from prefect.tasks.secrets import PrefectSecret
 from prefect.utilities import logging
 from prefect.utilities.tasks import defaults_from_attrs
 
 from ..exceptions import ValidationError
-from ..sources import Sharepoint
+from ..sources import Sharepoint, SharepointList
 from .azure_key_vault import AzureKeyVaultSecret
 from ..utils import add_viadot_metadata_columns
 
@@ -229,4 +230,172 @@ class SharepointToDF(Task):
 
         df = self.df_replace_special_chars(df)
         self.logger.info(f"Successfully converted data to a DataFrame.")
+        return df
+
+
+
+class SharepointListToDF(Task):
+    """
+    Task to extract data from Sharepoint List into DataFrame.
+
+    Args:
+        list_title (str): Title of Sharepoint List. Default to None.
+        site_url (str): URL to set of Sharepoint Lists. Default to None.
+        required_fields (List[str]): Required fields(columns) need to be extracted from
+                                        Sharepoint List. Default to None.
+        field_property (List[str]): Property to expand with expand query method.
+                                    All propertys can be found under list.item.properties.
+                                    Default to ["Title"]
+        filters (dict): Dictionary with operators which filters the SharepointList output.
+                        allowed dtypes: ('datetime','date','bool','int', 'float', 'complex', 'str')
+                        allowed conjuction: ('&','|')
+                        allowed operators: ('<','>','<=','>=','==','!=')
+                        Example how to build the dict:
+                        filters = {
+                        'Column_name_1' :
+                                {
+                                'dtype': 'datetime',
+                                'value1':'YYYY-MM-DD',
+                                'value2':'YYYY-MM-DD',
+                                'operator1':'>=',
+                                'operator2':'<=',
+                                'operators_conjuction':'&',
+                                'filters_conjuction':'&',
+                                }
+                                ,
+                        'Column_name_2' : 
+                                {
+                                'dtype': 'str',
+                                'value1':'NM-PL',
+                                'operator1':'==',
+                                },
+                        }
+        row_count (int): Number of downloaded rows in single request. Default to 5000.
+
+    Returns:
+    pandas DataFrame
+    """
+
+    def __init__(
+        self,
+        path: str = None,
+        list_title: str = None, 
+        site_url: str = None,
+        required_fields: List[str] = None,
+        field_property: str = "Title",
+        filters:  dict = None,
+        row_count: int = 5000,
+        credentials_secret: str = None,
+        vault_name: str = None,
+        *args,
+        **kwargs,
+    ):
+        
+        self.path = path
+        self.list_title = list_title
+        self.site_url = site_url
+        self.required_fields = required_fields
+        self.field_property = field_property
+        self.filters = filters
+        self.row_count = row_count
+        self.vault_name = vault_name
+        self.credentials_secret = credentials_secret
+        
+        if not credentials_secret:
+            # Attempt to read a default for the service principal secret name
+            try:
+                credentials_secret = PrefectSecret("SHAREPOINT-CERT").run()
+            except ValueError:
+                pass
+
+        if credentials_secret:
+            credentials_str = AzureKeyVaultSecret(
+                secret = self.credentials_secret, vault_name=self.vault_name
+            ).run()
+            self.credentials = json.loads(credentials_str)
+
+
+        super().__init__(
+            *args,
+            **kwargs,
+        )
+
+
+    def __call__(self):
+        """Download Sharepoint_List data to a .parquet file"""
+        super().__call__(self)
+
+    
+    def _convert_camel_case_to_words(
+            self,
+            input_str: str) -> str:
+        
+        self.input_str = input_str
+
+        words = re.findall(r'[A-Z][a-z]*|[0-9]+', self.input_str)
+        converted = ' '.join(words)
+
+        return converted 
+        
+    
+    def change_column_name(
+        self,
+        df: pd.DataFrame = None,
+    ):
+        s = SharepointList()
+        list_fields = s.get_fields(
+            list_title= self.list_title,
+            site_url= self.site_url, 
+            required_fields= self.required_fields,
+            )
+        
+        self.logger.info("Changing columns names")
+        column_names_correct = [field.properties['Title'] for field in list_fields]
+        column_names_code = [field.properties['InternalName'] for field in list_fields]
+        dictionary = dict(zip(column_names_code, column_names_correct)) 
+        
+        # If duplicates in names from "Title" take "InternalName"
+        value_count = {}
+        duplicates = []
+
+        for key, value in dictionary.items():
+            if value in value_count:
+                if value_count[value] not in duplicates:
+                    duplicates.append(value_count[value])
+                duplicates.append(key)
+            else:
+                value_count[value] = key
+        
+        for key in duplicates:
+            dictionary[key] = self._convert_camel_case_to_words(key)
+            
+        # Rename columns names inside DataFrame
+        df = df.rename(columns=dictionary)
+
+        return df
+    
+
+    def run(
+        self,
+        ) -> None:
+        """
+        Run Task SharepointListToDF.
+
+        Returns:
+            pd.DataFrame
+        """
+
+        s = SharepointList(credentials=self.credentials,)
+        df_raw = s.list_item_to_df(
+            list_title = self.list_title,
+            site_url = self.site_url,
+            required_fields = self.required_fields,
+            field_property = self.field_property,
+            filters = self.filters,
+            row_count = self.row_count,
+            )
+        
+        df = self.change_column_name(df=df_raw)
+        self.logger.info("Successfully changed structure of the DataFrame")
+
         return df
