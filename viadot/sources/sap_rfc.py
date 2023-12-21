69a85beb097507484de80978f2d33c795b9b192b
Marcin Purtak <44641138+marcinpurtak@users.noreply.github.com>
2023-12-21 09:26:59 +0100

    Update sap_rfc.py
    Removal of whitespace in the unique column added.

--- viadot/sources/sap_rfc.py
+++ viadot/sources/sap_rfc.py
@@ -1133,6 +1133,10 @@ class SAPRFCV2(Source):
                 ):
                     df_tmp = pd.DataFrame(columns=fields)
                     df_tmp[fields] = records
+                    # SAP adds whitespaces to the first extracted column value and it must be removed to make a proper merge
+                    for col in self.rfc_unique_id:
+                        df_tmp[col] = df_tmp[col].str.strip()
+                        df[col] = df[col].str.strip() 
                     df = pd.merge(df, df_tmp, on=self.rfc_unique_id, how="outer")
                 else:
                     if not start:
