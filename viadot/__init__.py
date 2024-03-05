9865ad5bdabad6222e72e66addc90018f0e2be45
trymzet <michalmzawadzki@gmail.com>
2024-03-05 15:08:09 +0100

    🔖 Bump version

--- viadot/__init__.py
+++ viadot/__init__.py
@@ -1,6 +1,6 @@
 import logging
 
-__version__ = "2.0a17"
+__version__ = "2.0a18"
 
 # Remove trash Azure INFO logs which contain low-level debugging information
 # but keep WARNING and higher ones in case something actually important happens.
