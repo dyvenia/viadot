d7922f0d06f4e24f99324f4e041d61891dd3d889
Dominik Jagoda <djagoda@dyvenia.com>
2024-06-06 13:53:16 +0200

    ♻️ Synchronized `prefect-viadot` with `orchiestration/prefect`

--- src/viadot/orchestration/prefect/flows/sharepoint_to_redshift_spectrum.py
+++ src/viadot/orchestration/prefect/flows/sharepoint_to_redshift_spectrum.py
@@ -1,10 +1,97 @@
 """Flows for downloading data from Sharepoint and uploading it to AWS Redshift Spectrum."""  # noqa: W505
 
+from pathlib import Path
 from typing import Any, Literal
 
-from viadot.orchestration.prefect.tasks import df_to_redshift_spectrum, sharepoint_to_df
+import pandas as pd
+from viadot.orchestration.prefect.tasks import (
+    df_to_redshift_spectrum,
+    get_endpoint_type_from_url,
+    scan_sharepoint_folder,
+    sharepoint_to_df,
+    validate_and_reorder_dfs_columns,
+)
 
 from prefect import flow
+
+
+def load_data_from_sharepoint(
+    file_sheet_mapping: dict | None,
+    download_all_files: bool,
+    sharepoint_url: str,
+    sheet_name: str | list[str | int] | int | None = None,
+    columns: str | list[str] | list[int] | None = None,
+    credentials_config: dict[str, Any] | None = None,
+) -> dict:
+    """Loads data from SharePoint and returns it as a dictionary of DataFrames.
+
+    This function fetches data from SharePoint based on the provided file-sheet
+    mapping or by downloading all files in a specified folder. It returns the
+    data as a dictionary where keys are filenames and values are the respective
+    DataFrames.
+
+    Args:
+        file_sheet_mapping (dict): A dictionary where keys are filenames and values are
+            the sheet names to be loaded from each file. If provided, only these files
+            and sheets will be downloaded.
+        download_all_files (bool): A flag indicating whether to download all files from
+            the SharePoint folder specified by the `sharepoint_url`. This is ignored if
+            `file_sheet_mapping` is provided.
+        sharepoint_url (str): The base URL of the SharePoint site or folder.
+        sheet_name (str): The name of the sheet to load if `file_sheet_mapping` is not
+            provided. This is used when downloading all files.
+        columns (str | list[str] | list[int], optional): Which columns to ingest.
+            Defaults to None.
+        credentials_config (dict, optional): A dictionary containing credentials and
+            configuration for SharePoint. Defaults to None.
+
+    Returns:
+        dict: A dictionary where keys are filenames and values are DataFrames containing
+            the data from the corresponding sheets.
+
+    Options:
+        - If `file_sheet_mapping` is provided, only the specified files and sheets are
+        downloaded.
+        - If `download_all_files` is True, all files in the specified SharePoint folder
+        are downloaded and the data is loaded from the specified `sheet_name`.
+    """
+    dataframes_dict = {}
+    credentials_secret = (
+        credentials_config.get("secret") if credentials_config else None
+    )
+    credentials = credentials_config.get("credentials") if credentials_config else None
+    config_key = credentials_config.get("config_key") if credentials_config else None
+
+    if file_sheet_mapping:
+        for file, sheet in file_sheet_mapping.items():
+            df = sharepoint_to_df(
+                url=sharepoint_url + file,
+                sheet_name=sheet,
+                columns=columns,
+                credentials_secret=credentials_secret,
+                credentials=credentials,
+                config_key=config_key,
+            )
+            dataframes_dict[file] = df
+    elif download_all_files:
+        list_of_urls = scan_sharepoint_folder(
+            url=sharepoint_url,
+            credentials_secret=credentials_secret,
+            credentials=credentials,
+            config_key=config_key,
+        )
+        for file_url in list_of_urls:
+            df = sharepoint_to_df(
+                url=file_url,
+                sheet_name=sheet_name,
+                columns=columns,
+                credentials_secret=credentials_secret,
+                credentials=credentials,
+                config_key=config_key,
+            )
+            file_name = Path(file_url).stem + Path(file_url).suffix
+            dataframes_dict[file_name] = df
+    return dataframes_dict
 
 
 @flow(
@@ -32,8 +119,15 @@ def sharepoint_to_redshift_spectrum(  # noqa: PLR0913, PLR0917
     sharepoint_credentials_secret: str | None = None,
     sharepoint_config_key: str | None = None,
     sharepoint_credentials: dict[str, Any] | None = None,
+    file_sheet_mapping: dict | None = None,
+    download_all_files: bool = False,
+    return_as_one_table: bool = False,
 ) -> None:
-    """Download a pandas `DataFrame` from Sharepoint and upload it to Redshift Spectrum.
+    """Extract data from SharePoint and load it into AWS Redshift Spectrum.
+
+    This function downloads data from SharePoint and uploads it to AWS Redshift
+    Spectrum, either as a single table or as multiple tables depending on the provided
+    parameters.
 
     Args:
         sharepoint_url (str): The URL to the file.
@@ -71,28 +165,104 @@ def sharepoint_to_redshift_spectrum(  # noqa: PLR0913, PLR0917
             relevant credentials. Defaults to None.
         sharepoint_credentials (dict, optional): Credentials to Sharepoint. Defaults to
             None.
-    """
-    df = sharepoint_to_df(
-        url=sharepoint_url,
-        sheet_name=sheet_name,
-        columns=columns,
-        credentials_secret=sharepoint_credentials_secret,
-        config_key=sharepoint_config_key,
-        credentials=sharepoint_credentials,
-    )
+        file_sheet_mapping(dict, optional): Dictionary with mapping sheet for each file
+            that should be downloaded. If the parameter is provided only data from this
+            dictionary are downloaded. Defaults to empty dictionary.
+        download_all_files (bool, optional): Whether to download all files from
+            the folder. Defaults to False.
+        return_as_one_table (bool, optional): Whether to load data to a single table.
+            Defaults to False.
 
-    return df_to_redshift_spectrum(
-        df=df,
-        to_path=to_path,
-        schema_name=schema_name,
-        table=table,
-        extension=extension,
-        if_exists=if_exists,
-        partition_cols=partition_cols,
-        index=index,
-        compression=compression,
-        sep=sep,
-        description=description,
-        credentials=aws_credentials,
-        config_key=aws_config_key,
-    )
+    The function operates in two main modes:
+    1. If `file_sheet_mapping` is provided, it downloads and processes only
+        the specified files and sheets.
+    2. If `download_all_files` is True, it scans the SharePoint folder for all files
+        and processes them.
+
+    Additionally, depending on the value of `return_as_one_table`, the data is either
+        combined into a single table or uploaded as multiple tables.
+
+    """
+    sharepoint_credentials_config = {
+        "secret": sharepoint_credentials_secret,
+        "credentials": sharepoint_credentials,
+        "config_key": sharepoint_config_key,
+    }
+    endpoint_type = get_endpoint_type_from_url(url=sharepoint_url)
+
+    if endpoint_type == "file":
+        df = sharepoint_to_df(
+            url=sharepoint_url,
+            sheet_name=sheet_name,
+            columns=columns,
+            credentials_secret=sharepoint_credentials_secret,
+            config_key=sharepoint_config_key,
+            credentials=sharepoint_credentials,
+        )
+
+        df_to_redshift_spectrum(
+            df=df,
+            to_path=to_path,
+            schema_name=schema_name,
+            table=table,
+            extension=extension,
+            if_exists=if_exists,
+            partition_cols=partition_cols,
+            index=index,
+            compression=compression,
+            sep=sep,
+            description=description,
+            credentials=aws_credentials,
+            config_key=aws_config_key,
+        )
+    else:
+        dataframes_dict = load_data_from_sharepoint(
+            file_sheet_mapping=file_sheet_mapping,
+            download_all_files=download_all_files,
+            sharepoint_url=sharepoint_url,
+            sheet_name=sheet_name,
+            columns=columns,
+            credentials_config=sharepoint_credentials_config,
+        )
+
+        if return_as_one_table is True:
+            dataframes_list = list(dataframes_dict.values())
+            validated_and_reordered_dfs = validate_and_reorder_dfs_columns(
+                dataframes_list=dataframes_list
+            )
+            final_df = pd.concat(validated_and_reordered_dfs, ignore_index=True)
+
+            df_to_redshift_spectrum(
+                df=final_df,
+                to_path=to_path,
+                schema_name=schema_name,
+                table=table,
+                extension=extension,
+                if_exists=if_exists,
+                partition_cols=partition_cols,
+                index=index,
+                compression=compression,
+                sep=sep,
+                description=description,
+                credentials=aws_credentials,
+                config_key=aws_config_key,
+            )
+
+        elif return_as_one_table is False:
+            for file_name, df in dataframes_dict.items():
+                file_name_without_extension = Path(file_name).stem
+                df_to_redshift_spectrum(
+                    df=df,
+                    to_path=f"{to_path}_{file_name_without_extension}",  # to test
+                    schema_name=schema_name,
+                    table=f"{table}_{file_name_without_extension}",
+                    extension=extension,
+                    if_exists=if_exists,
+                    partition_cols=partition_cols,
+                    index=index,
+                    compression=compression,
+                    sep=sep,
+                    description=description,
+                    credentials=aws_credentials,
+                    config_key=aws_config_key,
+                )
--- tests/orchestration/prefect_tests/integration/test_sharepoint_tasks.py
+++ tests/orchestration/prefect_tests/integration/test_sharepoint_tasks.py
@@ -1,11 +1,18 @@
 import os
 from pathlib import Path
 
+import pandas as pd
+import pytest
 from prefect import flow
 from viadot.orchestration.prefect.tasks import (
+    get_endpoint_type_from_url,
+    scan_sharepoint_folder,
     sharepoint_download_file,
     sharepoint_to_df,
+    validate_and_reorder_dfs_columns,
 )
+
+DF1 = pd.DataFrame({"a": [1, 2], "b": [3, 4]})
 
 
 def test_to_df(sharepoint_url, sharepoint_config_key):
@@ -31,3 +38,74 @@ def test_download_file(sharepoint_url, sharepoint_config_key):
     assert file in os.listdir()
 
     Path(file).unlink()
+
+
+def test_scan_sharepoint_folder(sharepoint_folder_url, sharepoint_config_key):
+    @flow
+    def test_scan_sharepoint_folder_flow():
+        return scan_sharepoint_folder(
+            url=sharepoint_folder_url, config_key=sharepoint_config_key
+        )
+
+    received_files = test_scan_sharepoint_folder_flow()
+    assert received_files
+    assert isinstance(received_files, list)
+    assert all(isinstance(file_url, str) for file_url in received_files)
+
+
+def test_get_endpoint_type_from_url():
+    test_cases = [
+        (
+            "https://company.sharepoint.com/sites/site_name/shared_documents/folder",
+            "directory",
+        ),
+        (
+            "https://company.sharepoint.com/sites/site_name/shared_documents/",
+            "directory",
+        ),
+        (
+            "https://company.sharepoint.com/sites/site_name/shared_documents/folder/file.txt",
+            "file",
+        ),
+        (
+            "https://company.sharepoint.com/sites/site_name/shared_documents/folder/file.csv",
+            "file",
+        ),
+    ]
+
+    for url, expected_type in test_cases:
+        assert get_endpoint_type_from_url(url) == expected_type
+
+
+def test_validate_and_reorder_dfs_columns_empty_list():
+    """Test case for empty list of dataframes."""
+    with pytest.raises(IndexError):
+        validate_and_reorder_dfs_columns([])
+
+
+def test_validate_and_reorder_dfs_columns_different_structure():
+    """Test case for different column structures."""
+    df2 = pd.DataFrame({"a": [5, 6], "c": [7, 8]})
+    with pytest.raises(
+        ValueError,
+        match=r"""does not have the same structure as
+            the first DataFrame.""",
+    ):
+        validate_and_reorder_dfs_columns([DF1, df2])
+
+
+def test_validate_and_reorder_dfs_columns_reorder_columns():
+    """Test case for reordering columns."""
+    df3 = pd.DataFrame({"b": [3, 4], "a": [1, 2]})
+    df4 = pd.DataFrame({"a": [5, 6], "b": [7, 8]})
+    result = validate_and_reorder_dfs_columns([DF1, df3, df4])
+    for df in result:
+        assert df.columns.tolist() == ["a", "b"]
+
+
+def test_validate_and_reorder_dfs_columns_correct_order():
+    """Test case for columns already in order."""
+    df5 = pd.DataFrame({"a": [1, 2], "b": [3, 4]})
+    result = validate_and_reorder_dfs_columns([DF1, df5])
+    for df in result:
+        assert df.columns.tolist() == ["a", "b"]
