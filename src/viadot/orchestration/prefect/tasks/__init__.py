b58885488acca95dba253c8c3f3a240fd7d47ac6
angelika233 <atarnawa@dyvenia.com>
2024-07-03 11:59:37 +0200

    ✨ Added `SQLServer` source and tasks for it

--- src/viadot/orchestration/prefect/tasks/__init__.py
+++ src/viadot/orchestration/prefect/tasks/__init__.py
@@ -7,6 +7,7 @@ from .dbt import dbt_task  # noqa: F401
 from .exchange_rates import exchange_rates_to_df  # noqa: F401
 from .git import clone_repo  # noqa: F401
 from .luma import luma_ingest_task  # noqa: F401
+from .minio import df_to_minio  # noqa: F401
 from .redshift_spectrum import df_to_redshift_spectrum  # noqa: F401
 from .s3 import s3_upload_file  # noqa: F401
 from .sap_rfc import sap_rfc_to_df  # noqa: F401
@@ -17,3 +18,4 @@ from .sharepoint import (
     sharepoint_to_df,  # noqa: F401
     validate_and_reorder_dfs_columns,  # noqa: F401
 )
+from .sql_server import sql_server_query, sql_server_to_df, create_sql_server_table
