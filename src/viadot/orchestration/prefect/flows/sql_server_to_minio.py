6bc35128aa34e6ea49a55dec748f0e019051b364
angelika233 <atarnawa@dyvenia.com>
2024-07-08 11:51:39 +0200

    🎨 Updated credentials options

--- src/viadot/orchestration/prefect/flows/sql_server_to_minio.py
+++ src/viadot/orchestration/prefect/flows/sql_server_to_minio.py
@@ -1,6 +1,6 @@
 """Flows for downloading data from SQLServer and uploading it to MinIO."""
 
-from typing import Any, Literal
+from typing import Literal
 
 from viadot.orchestration.prefect.tasks import sql_server_to_df, df_to_minio
 
@@ -19,9 +19,7 @@ def sql_server_to_minio(
     if_exists: Literal["error", "delete_matching", "overwrite_or_ignore"] = "error",
     basename_template: str | None = None,
     sql_server_credentials_secret: str | None = None,
-    sql_server_credentials: dict[str, Any] | None = None,
     sql_server_config_key: str | None = None,
-    minio_credentials: dict[str, Any] | None = None,
     minio_credentials_secret: str | None = None,
     minio_config_key: str | None = None,
 ):
@@ -36,15 +34,11 @@ def sql_server_to_minio(
                 an automatically incremented integer. Defaults to None.
         if_exists (Literal["error", "delete_matching", "overwrite_or_ignore"],
             optional). What to do if the dataset already exists. Defaults to "error".
-        sql_server_credentials (dict[str, Any], optional): Credentials to the SQLServer.
-            Defaults to None.
         sql_server_credentials_secret (str, optional): The name of the secret storing
             the credentialsto the SQLServer. Defaults to None.
             More info on: https://docs.prefect.io/concepts/blocks/
         sql_server_config_key (str, optional): The key in the viadot config holding relevant
             credentials to the SQLServer. Defaults to None.
-            Defaults to None.
-        minio_credentials (dict[str, Any], optional): Credentials to the MinIO.
             Defaults to None.
         minio_credentials_secret (str, optional): The name of the secret storing
             the credentials to the MinIO. Defaults to None.
@@ -56,7 +50,6 @@ def sql_server_to_minio(
     """
     df = sql_server_to_df(
         query=query,
-        credentials=sql_server_credentials,
         config_key=sql_server_config_key,
         credentials_secret=sql_server_credentials_secret,
     )
@@ -67,6 +60,5 @@ def sql_server_to_minio(
         if_exists=if_exists,
         basename_template=basename_template,
         config_key=minio_config_key,
-        credentials=minio_credentials,
         credentials_secret=minio_credentials_secret,
     )
