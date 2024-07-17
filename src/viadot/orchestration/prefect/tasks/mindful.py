fdc83ff4b133e0b62b121284cfb8c2364536b488
Diego-H-S <dhidalgosoto@dyvenia.com>
2024-07-17 08:50:59 +0200

    🎨 updated code performance.

--- src/viadot/orchestration/prefect/tasks/mindful.py
+++ src/viadot/orchestration/prefect/tasks/mindful.py
@@ -1,4 +1,30 @@
-import logging
+"""
+'mindful.py'.
+
+Prefect task wrapper for the Mindful API connector.
+
+This module provides an intermediate wrapper between the prefect flow and the connector:
+- Generate the Mindful API connector.
+- Create and return a pandas Data Frame with the response of the API.
+
+Typical usage example:
+
+    data_frame = mindful_to_df(
+        credentials=credentials,
+        config_key=config_key,
+        azure_key_vault_secret=azure_key_vault_secret,
+        region=region,
+        endpoint=end,
+        date_interval=date_interval,
+        limit=limit,
+    )
+
+Functions:
+
+    mindful_to_df(credentials, config_key, azure_key_vault_secret, region,
+        endpoint, date_interval, limit): Task to download data from Mindful API.
+"""  # noqa: D412
+
 from datetime import date
 from typing import Any, Dict, List, Literal, Optional
 
@@ -9,11 +35,8 @@ from viadot.orchestration.prefect.exceptions import MissingSourceCredentialsErro
 from viadot.orchestration.prefect.utils import get_credentials
 from viadot.sources import Mindful
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
 
-
-@task(retries=3, retry_delay_seconds=10, timeout_seconds=60 * 60)
+@task(retries=3, log_prints=True, retry_delay_seconds=10, timeout_seconds=60 * 60)
 def mindful_to_df(
     credentials: Optional[Dict[str, Any]] = None,
     config_key: str = None,
@@ -24,27 +47,29 @@ def mindful_to_df(
     limit: int = 1000,
 ) -> pd.DataFrame:
     """
-    Description:
-        Task for downloading data from Mindful API to Data Frame.
+    Task to download data from Mindful API.
 
     Args:
-        credentials (Optional[Dict[str, Any]], optional): Mindful credentials as a dictionary.
+        credentials (Optional[Dict[str, Any]], optional): Mindful credentials as a
+            dictionary. Defaults to None.
+        config_key (str, optional): The key in the viadot config holding relevant
+            credentials. Defaults to None.
+        azure_key_vault_secret (Optional[str], optional): The name of the Azure Key
+            Vault secret where credentials are stored. Defaults to None.
+        region (Literal[us1, us2, us3, ca1, eu1, au1], optional): Survey Dynamix region
+            from where to interact with the mindful API. Defaults to "eu1" English
+            (United Kingdom).
+        endpoint (Optional[str], optional): API endpoint for an individual request.
             Defaults to None.
-        config_key (str, optional): The key in the viadot config holding relevant credentials.
-            Defaults to None.
-        azure_key_vault_secret (Optional[str], optional): The name of the Azure Key Vault secret
-            where credentials are stored. Defaults to None.
-        region (Literal[us1, us2, us3, ca1, eu1, au1], optional): Survey Dynamix region from
-            where to interact with the mindful API. Defaults to "eu1" English (United Kingdom).
-        endpoint (Optional[str], optional): API endpoint for an individual request. Defaults to None.
-        date_interval (Optional[List[date]], optional): Date time range detailing the starting date and the ending date.
-            If no range is passed, one day of data since this moment will be retrieved. Defaults to None.
-        limit (int, optional): The number of matching interactions to return. Defaults to 1000.
+        date_interval (Optional[List[date]], optional): Date time range detailing the
+            starting date and the ending date. If no range is passed, one day of data
+            since this moment will be retrieved. Defaults to None.
+        limit (int, optional): The number of matching interactions to return.
+            Defaults to 1000.
 
     Returns:
         pd.DataFrame: The response data as a Pandas Data Frame.
     """
-
     logger = get_run_logger()
 
     if not (azure_key_vault_secret or config_key or credentials):
@@ -55,7 +80,8 @@ def mindful_to_df(
 
     if endpoint is None:
         logger.warning(
-            "The API endpoint parameter was not defined. The default value is 'surveys'."
+            "The API endpoint parameter was not defined. "
+            + "The default value is 'surveys'."
         )
         endpoint = "surveys"
 
--- src/viadot/sources/mindful.py
+++ src/viadot/sources/mindful.py
@@ -1,3 +1,50 @@
+"""
+'mindful.py'.
+
+Structure for the Mindful API connector.
+
+This module provides functionalities for connecting to Mindful API and download
+the response. It includes the following features:
+- Direct connection to Mindful API.
+- Introduce any downloaded data into a Pandas Data Frame.
+
+Typical usage example:
+
+    mindful = Mindful(
+        credentials=credentials,
+        config_key=config_key,
+        region=region,
+    )
+    mindful.api_connection(
+        endpoint=endpoint,
+        date_interval=date_interval,
+        limit=limit,
+    )
+    data_frame = mindful.to_df()
+
+Mindful Class Attributes:
+
+    credentials (Optional[MindfulCredentials], optional): Mindful credentials.
+        Defaults to None.
+    config_key (str, optional): The key in the viadot config holding relevant
+        credentials. Defaults to "mindful".
+    region (Literal[us1, us2, us3, ca1, eu1, au1], optional): Survey Dynamix region
+        from where to interact with the mindful API. Defaults to "eu1" English
+        (United Kingdom).
+
+Functions:
+
+    api_connection(endpoint, date_interval, limit): General method to connect to Survey
+        Dynamix API and generate the response.
+    to_df(if_empty, drop_duplicates, validate_df_dict): Generate a Pandas Data Frame
+        with the data in the Response object, and metadata.
+
+Classes:
+
+    MindfulCredentials: Checking for values in Mindful credentials dictionary.
+    Mindful: Class implementing the Mindful API.
+"""  # noqa: D412
+
 import json
 import logging
 from datetime import date, timedelta
@@ -15,7 +62,7 @@ from viadot.sources.base import Source
 from viadot.utils import add_viadot_metadata_columns, handle_api_response
 
 
-class MINDFUL_CREDENTIALS(BaseModel):
+class MindfulCredentials(BaseModel):
     """Checking for values in Mindful credentials dictionary.
 
     Two key values are held in the Mindful connector:
@@ -23,7 +70,8 @@ class MINDFUL_CREDENTIALS(BaseModel):
         - auth_token: A unique token to be used as the password for API requests.
 
     Args:
-        BaseModel (pydantic.main.ModelMetaclass): A base class for creating Pydantic models.
+        BaseModel (pydantic.main.ModelMetaclass): A base class for creating
+            Pydantic models.
     """
 
     customer_uuid: str
@@ -42,31 +90,30 @@ class Mindful(Source):
 
     def __init__(
         self,
-        credentials: Optional[MINDFUL_CREDENTIALS] = None,
+        *args,
+        credentials: Optional[MindfulCredentials] = None,
         config_key: str = "mindful",
         region: Literal["us1", "us2", "us3", "ca1", "eu1", "au1"] = "eu1",
-        *args,
         **kwargs,
     ):
         """
-        Description:
-            Creation of a Mindful instance.
+        Create a Mindful instance.
 
         Args:
-            credentials (Optional[MINDFUL_CREDENTIALS], optional): Mindful credentials.
+            credentials (Optional[MindfulCredentials], optional): Mindful credentials.
                 Defaults to None.
-            config_key (str, optional): The key in the viadot config holding relevant credentials.
-                Defaults to "mindful".
-            region (Literal[us1, us2, us3, ca1, eu1, au1], optional): Survey Dynamix region from
-                where to interact with the mindful API. Defaults to "eu1" English (United Kingdom).
+            config_key (str, optional): The key in the viadot config holding relevant
+                credentials. Defaults to "mindful".
+            region (Literal[us1, us2, us3, ca1, eu1, au1], optional): Survey Dynamix
+                region from where to interact with the mindful API. Defaults to "eu1"
+                English (United Kingdom).
         """
-
         credentials = credentials or get_source_credentials(config_key) or None
         if credentials is None:
             raise CredentialError("Missing credentials.")
 
         logging.basicConfig()
-        validated_creds = dict(MINDFUL_CREDENTIALS(**credentials))
+        validated_creds = dict(MindfulCredentials(**credentials))
         super().__init__(*args, credentials=validated_creds, **kwargs)
         self.logger.setLevel(logging.INFO)
 
@@ -81,16 +128,18 @@ class Mindful(Source):
         params: Optional[Dict[str, Any]] = None,
         endpoint: str = "",
     ) -> Response:
-        """Basic call to Mindful API given an endpoint.
+        """
+        Call to Mindful API given an endpoint.
 
         Args:
-            params (Optional[Dict[str, Any]], optional): Parameters to be passed into the request. Defaults to None.
-            endpoint (str, optional): API endpoint for an individual request. Defaults to "".
+            params (Optional[Dict[str, Any]], optional): Parameters to be passed into
+                the request. Defaults to None.
+            endpoint (str, optional): API endpoint for an individual request.
+                Defaults to "".
 
         Returns:
             Response: request object with the response from the Mindful API.
         """
-
         response = handle_api_response(
             url=f"https://{self.region}surveydynamix.com/api/{endpoint}",
             params=params,
@@ -109,20 +158,22 @@ class Mindful(Source):
         """General method to connect to Survey Dynamix API and generate the response.
 
         Args:
-            endpoint (Literal["interactions", "responses", "surveys"], optional): API endpoint for an individual request.
-                Defaults to "surveys".
-            date_interval (Optional[List[date]], optional): Date time range detailing the starting date and the ending date.
-                If no range is passed, one day of data since this moment will be retrieved. Defaults to None.
-            limit (int, optional): The number of matching interactions to return. Defaults to 1000.
+            endpoint (Literal["interactions", "responses", "surveys"], optional): API
+                endpoint for an individual request. Defaults to "surveys".
+            date_interval (Optional[List[date]], optional): Date time range detailing
+                the starting date and the ending date. If no range is passed, one day of
+                data since this moment will be retrieved. Defaults to None.
+            limit (int, optional): The number of matching interactions to return.
+                Defaults to 1000.
 
         Raises:
             ValueError: Not available endpoint.
             APIError: Failed to download data from the endpoint.
         """
-
         if endpoint not in self.ENDPOINTS:
             raise ValueError(
-                f"Survey Dynamix endpoint: '{endpoint}', is not available through Mindful viadot connector."
+                f"Survey Dynamix endpoint: '{endpoint}',"
+                + " is not available through Mindful viadot connector."
             )
 
         if (
@@ -135,10 +186,11 @@ class Mindful(Source):
 
             self.logger.warning(
                 (
-                    "No `date_interval` parameter was defined, or was erroneously defined."
-                    "`date_interval` parameter must have the folloing structure:\n"
-                    "\t[`date_0`, `date_1`], having that `date_1` > `date_0`.\n"
-                    f"By default, one day of data, from {date_interval[0].strftime('%Y-%m-%d')} to "
+                    "No `date_interval` parameter was defined, or was erroneously "
+                    "defined. `date_interval` parameter must have the folloing "
+                    "structure:\n\t[`date_0`, `date_1`], having that `date_1` > "
+                    "`date_0`.\nBy default, one day of data, from "
+                    f"{date_interval[0].strftime('%Y-%m-%d')} to "
                     f"{date_interval[1].strftime('%Y-%m-%d')}, will be obtained."
                 )
             )
@@ -165,7 +217,8 @@ class Mindful(Source):
             self.data = StringIO(response.content.decode("utf-8"))
         elif response.status_code == 204 and not response.content.decode():
             self.logger.warning(
-                f"Thera are not '{endpoint}' data to download from {date_interval[0]} to {date_interval[1]}."
+                f"Thera are not '{endpoint}' data to download from"
+                + f" {date_interval[0]} to {date_interval[1]}."
             )
             self.data = json.dumps({})
         else:
@@ -175,17 +228,27 @@ class Mindful(Source):
             raise APIError(f"Failed to downloaded '{endpoint}' data.")
 
     @add_viadot_metadata_columns
-    def to_df(self) -> pd.DataFrame:
-        """Generate a Pandas Data Frame with the data in the Response object and metadata.
+    def to_df(
+        self,
+        if_empty: str = "warn",
+    ) -> pd.DataFrame:
+        """
+        Generate a Pandas Data Frame with the data in the Response object and metadata.
+
+        Args:
+            if_empty (str, optional): What to do if a fetch produce no data.
+                Defaults to "warn
 
         Returns:
             pd.Dataframe: The response data as a Pandas Data Frame plus viadot metadata.
         """
+        super().to_df(if_empty=if_empty)
+
         data_frame = pd.read_json(self.data)
 
         if data_frame.empty:
             self._handle_if_empty(
-                if_empty="warn",
+                if_empty=if_empty,
                 message="The response does not contain any data.",
             )
         else:
