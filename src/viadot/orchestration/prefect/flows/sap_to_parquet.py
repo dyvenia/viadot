56b53ec5f6912eb0dc0170a0c0d3ddc524f502b7
judynah <mrs.nataliawalczak@gmail.com>
2024-07-18 11:15:30 +0200

    📝 Improved docstring

--- src/viadot/orchestration/prefect/flows/sap_to_parquet.py
+++ src/viadot/orchestration/prefect/flows/sap_to_parquet.py
@@ -23,35 +23,32 @@ def sap_to_parquet(
     rfc_total_col_width_character_limit: int = 400,
     rfc_unique_id: list[str] | None = None,
     sap_credentials_secret: str | None = None,
-    sap_credentials: dict[str, Any] | None = None,
     sap_config_key: str = "SAP",
     alternative_version: bool = False,
     replacement: str = "-",
 ) -> None:
-    """Download a pandas `DataFrame` from SAP and upload it to AWS Redshift Spectrum.
+    """Download a pandas `DataFrame` from SAP load it into Parquet file.
 
     Args:
         path (str): Path to Parquet file, where the data will be located.
             Defaults to None.
-        if_exists (Literal["append", "replace", "skip"], optional): What to do if the table exists.
+        if_exists (Literal["append", "replace", "skip"], optional): Information what has to be done, if the file exists.
             Defaults to "replace"
         query (str): The query to be executed with pyRFC.
         sap_sep (str, optional): The separator to use when reading query results.
-            If not provided, multiple options are automatically tried.
+            If not provided, multiple options are automatically tested.
             Defaults to None.
         func (str, optional): SAP RFC function to use. Defaults to None.
         rfc_total_col_width_character_limit (int, optional): Number of characters by
             which query will be split in chunks in case of too many columns for RFC
             function. According to SAP documentation, the limit is 512 characters.
-            However, we observed SAP raising an exception even on a slightly lower
-            number of characters, so we add a safety margin. Defaults to 400.
+            However, it was observed that SAP raising an exception even on a slightly lower
+            number of characters, so safety margin was added. Defaults to 400.
         rfc_unique_id  (list[str], optional): Reference columns to merge chunks Data
             Frames. These columns must to be unique. If no columns are provided, all
                 data frame columns will by concatenated. Defaults to None.
         sap_credentials_secret (str, optional): The name of the Prefect Secret that stores
             SAP credentials. Defaults to None.
-        sap_credentials (dict[str, Any], optional): Credentials to SAP.
-            Defaults to None.
         sap_config_key (str, optional): The key in the viadot config holding relevant
             credentials. Defaults to "SAP".
         alternative_version (bool, optional): Enable the use version 2 in source.
@@ -69,7 +66,6 @@ def sap_to_parquet(
         rfc_unique_id=rfc_unique_id,
         config_key=sap_config_key,
         credentials_secret=sap_credentials_secret,
-        credentials=sap_credentials,
         alternative_version=alternative_version,
     )
 
