38c73662167fc8a65f2d61f870f8ad648d39fcd3
judynah <mrs.nataliawalczak@gmail.com>
2024-07-18 14:07:00 +0200

    ✅ Updated test

--- tests/orchestration/prefect_tests/flows/test_sap_to_parquet.py
+++ tests/orchestration/prefect_tests/flows/test_sap_to_parquet.py
@@ -11,7 +11,7 @@ def test_sap_to_parquet():
 
     flow = sap_to_parquet(
         path=PATH,
-        query="""SELECT MATKL, MTART, ERSDA FROM MARA WHERE ERSDA = '20221230'""",
+        query="""SELECT MATKL, MTART, ERSDA FROM MARA LIMIT 100""",
         func="RFC_READ_TABLE",
         rfc_total_col_width_character_limit=400,
         sap_credentials_secret=SAP_CREDS,
@@ -20,4 +20,9 @@ def test_sap_to_parquet():
     parquet_file = os.path.isfile(PATH)
     assert parquet_file is True
 
+    df = pd.read_parquet(PATH)
+    n_row = 100
+    n_col = 5
+
+    assert (n_row, n_col) == df.shape
     os.remove(PATH)
