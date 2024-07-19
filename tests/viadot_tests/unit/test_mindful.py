2837f178333e1c12170504a18e4e86f9c0305b50
Diego-H-S <dhidalgosoto@dyvenia.com>
2024-07-19 13:58:53 +0200

    ✅ updated test file to reach 80% coverage.

--- tests/viadot_tests/unit/test_mindful.py
+++ tests/viadot_tests/unit/test_mindful.py
@@ -1,136 +1,153 @@
+import unittest
 from datetime import date, timedelta
-from unittest import mock
+from io import StringIO
+from unittest.mock import MagicMock, patch
 
 import pandas as pd
 import pytest
+from requests.auth import HTTPBasicAuth
+from requests.models import Response
 
-from viadot.exceptions import APIError
+from viadot.exceptions import APIError, CredentialError
 from viadot.sources import Mindful
 from viadot.sources.mindful import MindfulCredentials
 
-
-@pytest.fixture(scope="function")
-def var_dictionary():
-    variables = {
-        "credentials": {
-            "customer_uuid": "xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx",
-            "auth_token": "xxxxxxxxxxxxxxxxxxxxxxx",
-        },
-        "date_interval": [
-            date.today() - timedelta(days=2),
-            date.today() - timedelta(days=1),
-        ],
-    }
-    return variables
-
-
-class MockClass:
-    status_code = 200
-    content = b'[{"id":7277599,"survey_id":505,"phone_number":"","survey_type":"inbound"},{"id":7277294,"survey_id":504,"phone_number":"","survey_type":"web"}]'
-
-    def json():
-        test = [
-            {
-                "id": 7277599,
-                "survey_id": 505,
-                "phone_number": "",
-                "survey_type": "inbound",
-            },
-            {
-                "id": 7277294,
-                "survey_id": 504,
-                "phone_number": "",
-                "survey_type": "web",
-            },
-        ]
-        return test
-
-
-class MockClassException:
-    status_code = 404
-    content = b""
-
-    def json():
-        return None
-
-
-@mock.patch("viadot.sources.mindful.handle_api_response", return_value=MockClass)
-@pytest.mark.connect
-def test_mindful_api_response(mock_connection, var_dictionary):
-    mf = Mindful(credentials=var_dictionary["credentials"])
-    mf.api_connection(
-        endpoint="interactions",
-        date_interval=var_dictionary["date_interval"],
-    )
-
-    mock_connection.assert_called_once()
-    mock_connection.reset_mock()
-
-    mf.api_connection(
-        endpoint="responses",
-        date_interval=var_dictionary["date_interval"],
-    )
-
-    mock_connection.assert_called_once()
-    mock_connection.reset_mock()
-
-    mf.api_connection(
-        endpoint="surveys",
-        date_interval=var_dictionary["date_interval"],
-    )
-    mock_connection.assert_called_once()
-
-
-@mock.patch(
-    "viadot.sources.mindful.handle_api_response", return_value=MockClassException
-)
-@pytest.mark.connect
-def test_mindful_api_error(mock_connection, var_dictionary):
-    mf = Mindful(credentials=var_dictionary["credentials"])
-    with pytest.raises(APIError):
-        mf.api_connection(
-            endpoint="interactions",
-            date_interval=var_dictionary["date_interval"],
-        )
-
-    mock_connection.assert_called_once()
-
-
-@mock.patch("viadot.sources.mindful.handle_api_response", return_value=MockClass)
-@pytest.mark.response
-def test_mindful_api_df_response(mock_connection, var_dictionary):
-    mf = Mindful(credentials=var_dictionary["credentials"])
-    mf.api_connection(
-        endpoint="interactions",
-        date_interval=var_dictionary["date_interval"],
-    )
-    df = mf.to_df()
-
-    viadot_set = {"_viadot_source", "_viadot_downloaded_at_utc"}
-
-    assert set(df.columns).issuperset(viadot_set)
-    assert isinstance(df, pd.DataFrame)
+variables = {"credentials": {"customer_uuid": "fake_uuid", "auth_token": "fake_token"}}
 
 
 class TestMindfulCredentials:
-    @pytest.fixture(scope="function")
-    def mindfulcredentials(self):
-        return MindfulCredentials()
+    """Test Mindful Credentials Class."""
+
+    @pytest.mark.basic
+    def test_mindful_credentials(self):
+        """Test Genesys credentials."""
+        MindfulCredentials(
+            customer_uuid="test_customer_uuid", auth_token="test_auth_token"
+        )
 
 
-class TestMindful:
-    @pytest.fixture(scope="function")
-    def mindful(self):
-        return Mindful()
+class TestMindful(unittest.TestCase):
+    """Test Mindful Class."""
 
-    def test__mindful_api_response(self, mindful):
-        # TODO [use mock.patch, assert]: Implement test for Mindful._mindful_api_response (line 126)
-        pass
+    @classmethod
+    def setUpClass(cls):
+        """Defined based Mindful Class for the rest of test."""
+        cls.mindful_instance = Mindful(credentials=variables["credentials"])
 
-    def test_api_connection(self, mindful):
-        # TODO [use mock.patch, assert]: Implement test for Mindful.api_connection (line 152)
-        pass
+    @patch("viadot.sources.mindful.get_source_credentials", return_value=None)
+    @pytest.mark.basic
+    def test_init_no_credentials(self, mock_get_source_credentials):
+        """Test raise error without credentials."""
+        with pytest.raises(CredentialError):
+            Mindful()
 
-    def test_to_df(self, mindful):
-        # TODO [use mock.patch, assert]: Implement test for Mindful.to_df (line 231)
-        pass
+        mock_get_source_credentials.assert_called_once()
+
+    @pytest.mark.connect
+    @patch("viadot.sources.mindful.handle_api_response")
+    def test_mindful_api_response(self, mock_handle_api_response):
+        """Test Genesys `_mindful_api_response` method."""
+        mock_response = MagicMock(spec=Response)
+        mock_handle_api_response.return_value = mock_response
+
+        self.mindful_instance._mindful_api_response(endpoint="interactions")
+        mock_handle_api_response.assert_called_once_with(
+            url="https://eu1.surveydynamix.com/api/interactions",
+            params=None,
+            method="GET",
+            auth=unittest.mock.ANY,
+        )
+
+        auth_arg = mock_handle_api_response.call_args[1]["auth"]
+        self.assertIsInstance(auth_arg, HTTPBasicAuth)
+        self.assertEqual(auth_arg.username, variables["credentials"]["customer_uuid"])
+        self.assertEqual(auth_arg.password, variables["credentials"]["auth_token"])
+
+    @pytest.mark.connect
+    @patch("viadot.sources.mindful.handle_api_response")
+    def test_api_connection(self, mock_handle_api_response):
+        """Test Genesys `api_connection` method."""
+        mock_response = MagicMock(spec=Response)
+        mock_response.status_code = 200
+        mock_response.content = b'{"data": "some_data"}'
+        mock_handle_api_response.return_value = mock_response
+
+        date_interval = [date.today() - timedelta(days=1), date.today()]
+        self.mindful_instance.api_connection(
+            endpoint="responses", date_interval=date_interval
+        )
+
+        mock_handle_api_response.assert_called_once()
+        self.assertIsInstance(self.mindful_instance.data, StringIO)
+
+    @pytest.mark.connect
+    @patch("viadot.sources.mindful.handle_api_response")
+    def test_api_connection_no_data(self, mock_handle_api_response):
+        """Test Genesys `api_connection` method without data."""
+        mock_response = MagicMock(spec=Response)
+        mock_response.status_code = 204
+        mock_response.content = b""
+        mock_handle_api_response.return_value = mock_response
+
+        date_interval = [date.today() - timedelta(days=1), date.today()]
+        self.mindful_instance.api_connection(
+            endpoint="responses", date_interval=date_interval
+        )
+
+        mock_handle_api_response.assert_called_once()
+        self.assertEqual(self.mindful_instance.data, "{}")
+
+    @pytest.mark.connect
+    @patch("viadot.sources.mindful.handle_api_response")
+    def test_api_connection_error(self, mock_handle_api_response):
+        """Test Genesys `api_connection` method, APIError."""
+        mock_response = MagicMock(spec=Response)
+        mock_response.status_code = 500
+        mock_response.content = b"Internal Server Error"
+        mock_handle_api_response.return_value = mock_response
+
+        with self.assertRaises(APIError):
+            self.mindful_instance.api_connection(endpoint="responses")
+
+    @pytest.mark.functions
+    @patch("viadot.sources.mindful.pd.read_json")
+    @patch("viadot.sources.mindful.super")
+    def test_to_df(self, mock_super, mock_read_json):
+        """Test Genesys `to_df` method."""
+        mock_super().to_df = MagicMock()
+        mock_read_json.return_value = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
+        self.mindful_instance.data = StringIO('{"A": [1, 2], "B": [3, 4]}')
+
+        result_df = self.mindful_instance.to_df()
+        result_df.drop(
+            columns=["_viadot_source", "_viadot_downloaded_at_utc"],
+            inplace=True,
+            axis=1,
+        )
+        expected_df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
+
+        pd.testing.assert_frame_equal(result_df, expected_df)
+        mock_super().to_df.assert_called_once()
+
+    @pytest.mark.functions
+    @patch("viadot.sources.mindful.pd.read_json")
+    @patch("viadot.sources.mindful.super")
+    def test_to_df_empty(self, mock_super, mock_read_json):
+        """Test Genesys `to_df` method, checking emptiness."""
+        mock_super().to_df = MagicMock()
+        mock_read_json.return_value = pd.DataFrame()
+        self.mindful_instance.data = StringIO("{}")
+
+        with patch.object(
+            self.mindful_instance, "_handle_if_empty"
+        ) as mock_handle_if_empty:
+            result_df = self.mindful_instance.to_df()
+            mock_handle_if_empty.assert_called_once_with(
+                if_empty="warn", message="The response does not contain any data."
+            )
+            self.assertTrue(result_df.empty)
+            mock_super().to_df.assert_called_once()
+
+
+if __name__ == "__main__":
+    unittest.main()
