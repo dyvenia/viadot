a1a8f1b492cfde9e50cea3b5fe981326f3f3549f
trymzet <michalmzawadzki@gmail.com>
2024-08-01 17:03:21 +0200

    ♻️ Refactor some weird stuff

--- tests/integration/test_genesys.py
+++ tests/integration/test_genesys.py
@@ -1,13 +1,9 @@
-import os
-
 from viadot.orchestration.prefect.flows import genesys_to_adls
 
-os.system("clear")
 
-
-if "__main__" == __name__:
-    genesys_to_adls(
-        azure_key_vault_secret="genesys-access-1",
+def test_genesys_to_adls():
+    state = genesys_to_adls(
+        azure_key_vault_secret="genesys-access-1",  # noqa: S106
         verbose=True,
         endpoint="routing_queues_members",
         post_data_list=[""],
@@ -16,7 +12,8 @@ if "__main__" == __name__:
             "f4ef329a-d903-41f4-ad4d-876a753adf3c",
         ],
         drop_duplicates=True,
-        adls_azure_key_vault_secret="app-azure-cr-datalakegen2",
+        adls_azure_key_vault_secret="app-azure-cr-datalakegen2",  # noqa: S106
         adls_path="raw/dyvenia_sandbox/genesys/genesys_agents.csv",
         adls_path_overwrite=True,
     )
+    assert state.is_successful()
