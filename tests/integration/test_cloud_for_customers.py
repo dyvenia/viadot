01ed782599795948f9a3e36543deae186c1a740a
trymzet <michalmzawadzki@gmail.com>
2024-08-07 09:34:46 +0200

    🔥 Remove duplicate tests

--- tests/integration/test_cloud_for_customers.py
+++ tests/integration/test_cloud_for_customers.py
@@ -3,8 +3,6 @@
 from datetime import datetime, timedelta
 
 import pandas as pd
-import pytest
-from viadot.exceptions import CredentialError
 from viadot.sources.cloud_for_customers import CloudForCustomers
 
 
@@ -14,28 +12,6 @@ yesterday = datetime.utcnow() - timedelta(days=10)
 cursor_value = yesterday.isoformat(timespec="seconds") + "Z"
 cursor_filter = f"{cursor_field} ge datetimeoffset'{cursor_value}'"
 filter_params = {"$filter": cursor_filter}
-
-
-def test_is_configured():
-    c4c = CloudForCustomers(
-        credentials={"username": "test_user", "password": "test_password"},
-    )
-    assert c4c
-
-
-def test_is_configured_throws_credential_error():
-    with pytest.raises(CredentialError):
-        CloudForCustomers(
-            credentials={"username": "test_user", "password": None},
-        )
-    with pytest.raises(CredentialError):
-        CloudForCustomers(
-            credentials={"username": None, "password": "test_password"},
-        )
-    with pytest.raises(CredentialError):
-        CloudForCustomers(
-            credentials={"username": None, "password": None},
-        )
 
 
 def test_to_df(c4c_config_key):
