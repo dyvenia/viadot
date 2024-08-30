be79fd0784e836e5fc41800debd7887a7a919a2e
Diego-H-S <dhidalgosoto@dyvenia.com>
2024-08-30 12:07:54 +0200

    🔥 removed credentials in df_to_adls.

--- src/viadot/sources/bigquery.py
+++ src/viadot/sources/bigquery.py
@@ -1,48 +1,8 @@
-"""
-'bigquery.py'.
-
-Structure for the BigQuery API connector.
-This module provides functionalities for connecting to BigQuery API and download the
-response. It includes the following features:
-- Direct connection to BigQuery API.
-- Introduce any downloaded data into a Pandas Data Frame.
-
-Typical usage example:
-    bigquery = BigQuery(credentials=credentials, config_key=config_key)
-    bigquery.api_connection(
-        query=query,
-        dataset_name=dataset_name,
-        table_name=table_name,
-        date_column_name=date_column_name,
-        start_date=start_date,
-        end_date=end_date,
-        columns=columns,
-    )
-    data_frame = bigquery.to_df()
-
-BigQuery Class Attributes:
-    credentials (Optional[BigQueryCredentials], optional): BigQuery credentials.
-        Defaults to None.
-    config_key (Optional[str], optional): The key in the viadot config holding
-        relevant credentials. Defaults to None.
-
-Functions:
-    api_connection(query, dataset_name, table_name, date_column_name, start_date,
-        end_date, columns): Connect to BigQuery API and generate the response.
-    to_df(if_empty): Generate a Pandas Data Frame with the data in the Response object
-        and metadata
-
-Classes:
-    BigQueryCredentials: Checking for values in BigQuery credentials dictionary.
-    BigQuery: Class implementing the BigQuery API.
-"""  # noqa: D412
-
-from typing import List, Optional
+"""'bigquery.py'."""
 
 import numpy as np
 import pandas as pd
 import pandas_gbq
-from colorama import Fore, Style
 from google.oauth2 import service_account
 from pandas_gbq.gbq import GenericGBQException
 from pydantic import BaseModel
@@ -74,8 +34,7 @@ class BigQueryCredentials(BaseModel):
 
 
 class BigQuery(Source):
-    """
-    Class to connect with Bigquery project and SQL tables.
+    """Class to connect with Bigquery project and SQL tables.
 
     Documentation for this API is located at: https://cloud.google.com/bigquery/docs.
     """
@@ -83,22 +42,22 @@ class BigQuery(Source):
     def __init__(
         self,
         *args,
-        credentials: Optional[BigQueryCredentials] = None,
-        config_key: Optional[str] = None,
+        credentials: BigQueryCredentials | None = None,
+        config_key: str | None = None,
         **kwargs,
     ):
-        """
-        Create an instance of the Mediatool class.
+        """Create an instance of the Mediatool class.
 
         Args:
-            credentials (Optional[BigQueryCredentials], optional): BigQuery credentials.
+            credentials (BigQueryCredentials, optional): BigQuery credentials.
                 Defaults to None.
-            config_key (Optional[str], optional): The key in the viadot config holding
+            config_key (str, optional): The key in the viadot config holding
                 relevant credentials. Defaults to None.
         """
         credentials = credentials or get_source_credentials(config_key) or None
         if credentials is None:
-            raise CredentialError("Missing credentials.")
+            message = "Missing credentials."
+            raise CredentialError(message)
 
         validated_creds = dict(BigQueryCredentials(**credentials))
 
@@ -116,21 +75,17 @@ class BigQuery(Source):
         self.df_data = None
 
     def _list_datasets(self) -> str:
-        """
-        Get datasets from BigQuery project.
+        """Get datasets from BigQuery project.
 
         Returns:
             str: Custom query.
         """
-        query = f"""SELECT schema_name
+        return f"""SELECT schema_name
                 FROM {self.project_id}.INFORMATION_SCHEMA.SCHEMATA
                 """
 
-        return query
-
     def _list_tables(self, dataset_name: str) -> str:
-        """
-        Get tables from BigQuery dataset. Dataset is required.
+        """Get tables from BigQuery dataset. Dataset is required.
 
         Args:
             dataset_name (str): Dataset from Bigquery project.
@@ -138,15 +93,12 @@ class BigQuery(Source):
         Returns:
             str: Custom query.
         """
-        query = f"""SELECT table_name
+        return f"""SELECT table_name
                 FROM {self.project_id}.{dataset_name}.INFORMATION_SCHEMA.TABLES
                 """
 
-        return query
-
     def _list_columns(self, dataset_name: str, table_name: str) -> np.ndarray:
-        """
-        Get columns from BigQuery table. Dataset name and Table name are required.
+        """Get columns from BigQuery table. Dataset name and Table name are required.
 
         Args:
             dataset_name (str): Dataset from Bigquery project.
@@ -184,34 +136,34 @@ class BigQuery(Source):
 
     def api_connection(
         self,
-        query: Optional[str] = None,
-        dataset_name: Optional[str] = None,
-        table_name: Optional[str] = None,
-        date_column_name: Optional[str] = None,
-        start_date: Optional[str] = None,
-        end_date: Optional[str] = None,
-        columns: List[str] = [""],
+        query: str | None = None,
+        dataset_name: str | None = None,
+        table_name: str | None = None,
+        date_column_name: str | None = None,
+        start_date: str | None = None,
+        end_date: str | None = None,
+        columns: list[str] | None = None,
     ) -> None:
         """Connect to BigQuery API and generate the response.
 
         Args:
-            query (Optional[str]): SQL query to querying data in BigQuery.
+            query (str): SQL query to querying data in BigQuery.
                 Defaults to None.
                 Espetial queries:
                 -----------------
                 If the words "tables" or "datasets" are passed in this parameter all
                 tables and data sets will be returned as special internal queries.
-            dataset_name (Optional[str], optional): Dataset name. Defaults to None.
-            table_name (Optional[str], optional): Table name. Defaults to None.
-            date_column_name (Optional[str], optional): The user can provide the name of
-                the date. If the user-specified column does not exist, all data will be
-                retrieved from the table. Defaults to None.
-            start_date (Optional[str], optional): Parameter to pass start date e.g.
+            dataset_name (str, optional): Dataset name. Defaults to None.
+            table_name (str, optional): Table name. Defaults to None.
+            date_column_name (str, optional): The user can provide the name of the date.
+                If the user-specified column does not exist, all data will be retrieved
+                from the table. Defaults to None.
+            start_date (str, optional): Parameter to pass start date e.g.
                 "2022-01-01". Defaults to None.
-            end_date (Optional[str], optional): Parameter to pass end date e.g.
+            end_date (str, optional): Parameter to pass end date e.g.
                 "2022-01-01". Defaults to None.
-            columns (List[str], optional): List of columns from given table name.
-                Defaults to [""].
+            columns (list[str], optional): List of columns from given table name.
+                Defaults to None.
         """
         if query == "tables":
             query = self._list_tables(dataset_name=dataset_name)
@@ -225,10 +177,9 @@ class BigQuery(Source):
                     dataset_name=dataset_name, table_name=table_name
                 )
 
-            if not set(columns).issubset(set(table_columns)):
-                print(
-                    f"{Fore.YELLOW}WARNING{Style.RESET_ALL}: "
-                    + "Some of the columns provided are either, not in the table or "
+            if columns is None or not set(columns).issubset(set(table_columns)):
+                self.logger.warning(
+                    "Some of the columns provided are either, not in the table or "
                     + "the list is empty. Downloading all the data instead."
                 )
                 columns = "*"
@@ -241,16 +192,17 @@ class BigQuery(Source):
                     or start_date is None
                     or end_date is None
                 ):
-                    print(
-                        f"{Fore.YELLOW}WARNING{Style.RESET_ALL}: "
-                        + f"'{date_column_name}' column is not recognized, "
+                    self.logger.warning(
+                        f"'{date_column_name}' column is not recognized, "
                         + f"or either `start_date`: {start_date} or `end_date`: "
                         + f"{end_date} is None.\n"
                         + "Downloading all the data instead."
                     )
                     query = None
                 else:
-                    print(f"Filtering data from date {start_date} to {end_date}")
+                    self.logger.info(
+                        f"Filtering data from date {start_date} to {end_date}"
+                    )
                     query = f"""
                         SELECT {columns}
                             FROM `{self.project_id}.{dataset_name}.{table_name}`
@@ -268,15 +220,14 @@ class BigQuery(Source):
 
         self.df_data = self._gbd(query)
 
-        print(f"Downloaded the data from the table name: '{table_name}'.")
+        self.logger.info(f"Downloaded the data from the table name: '{table_name}'.")
 
     @add_viadot_metadata_columns
     def to_df(
         self,
         if_empty: str = "warn",
     ) -> pd.DataFrame:
-        """
-        Response from the API queried BigQuery table and transforms it into DataFrame.
+        """Response from the API queried BigQuery table into DataFrame.
 
         Args:
             if_empty (str, optional): if_empty param is checking params passed to
