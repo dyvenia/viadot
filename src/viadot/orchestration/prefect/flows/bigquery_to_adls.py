fec8e797b7bf98f1c83b4f46a55a2b09aeccf07c
Diego-H-S <dhidalgosoto@dyvenia.com>
2024-08-30 12:07:54 +0200

    🔥 removed credentials in df_to_adls.

--- src/viadot/orchestration/prefect/flows/bigquery_to_adls.py
+++ src/viadot/orchestration/prefect/flows/bigquery_to_adls.py
@@ -1,30 +1,6 @@
-"""
-'bigquery_to_adls.py'.
+"""'bigquery_to_adls.py'."""
 
-Prefect flow for the BigQuery API connector.
-This module provides a prefect flow function to use the BigQuery connector:
-- Call to the prefect task wrapper to get a final Data Frame from the connector.
-- Upload that data to Azure Data Lake Storage.
-
-Typical usage example:
-    flow = bigquery_to_adls(
-        credentials=credentials,
-        dataset_name=dataset_name,
-        table_name=table_name,
-        query=query,
-        adls_path=adls_path,
-        adls_azure_key_vault_secret=adls_azure_key_vault_secret,
-        adls_path_overwrite=True,
-    )
-
-Functions:
-    bigquery_to_adls(credentials, config_key, azure_key_vault_secret, query,
-        dataset_name, table_name, date_column_name, start_date, end_date, columns,
-        adls_credentials, adls_config_key, adls_azure_key_vault_secret, adls_path,
-        adls_path_overwrite): Flow to download data from Outlook API to Azure Data Lake.
-"""  # noqa: D412
-
-from typing import Any, Dict, List, Optional
+from typing import Any
 
 from prefect import flow
 from prefect.task_runners import ConcurrentTaskRunner
@@ -40,57 +16,53 @@ from viadot.orchestration.prefect.tasks import bigquery_to_df, df_to_adls
     task_runner=ConcurrentTaskRunner,
 )
 def bigquery_to_adls(
-    credentials: Optional[Dict[str, Any]] = None,
-    config_key: str = None,
-    azure_key_vault_secret: Optional[str] = None,
-    query: Optional[str] = None,
-    dataset_name: Optional[str] = None,
-    table_name: Optional[str] = None,
-    date_column_name: Optional[str] = None,
-    start_date: Optional[str] = None,
-    end_date: Optional[str] = None,
-    columns: List[str] = [""],
-    adls_credentials: Optional[Dict[str, Any]] = None,
-    adls_config_key: Optional[str] = None,
-    adls_azure_key_vault_secret: Optional[str] = None,
-    adls_path: Optional[str] = None,
+    credentials: dict[str, Any] | None = None,
+    config_key: str | None = None,
+    azure_key_vault_secret: str | None = None,
+    query: str | None = None,
+    dataset_name: str | None = None,
+    table_name: str | None = None,
+    date_column_name: str | None = None,
+    start_date: str | None = None,
+    end_date: str | None = None,
+    columns: list[str] | None = None,
+    adls_config_key: str | None = None,
+    adls_azure_key_vault_secret: str | None = None,
+    adls_path: str | None = None,
     adls_path_overwrite: bool = False,
 ) -> None:
-    """
-    Download data from BigQuery to Azure Data Lake.
+    """Download data from BigQuery to Azure Data Lake.
 
     Args:
-        credentials (Optional[Dict[str, Any]], optional): Mediatool credentials as a
-            dictionary. Defaults to None.
+        credentials (dict[str, Any], optional): Mediatool credentials as a dictionary.
+            Defaults to None.
         config_key (str, optional): The key in the viadot config holding relevant
             credentials. Defaults to None.
-        azure_key_vault_secret (Optional[str], optional): The name of the Azure Key
-            Vault secret where credentials are stored. Defaults to None.
-        query (Optional[str]): SQL query to querying data in BigQuery. Format of
-            basic query: (SELECT * FROM `{project}.{dataset_name}.{table_name}`).
+        azure_key_vault_secret (str, optional): The name of the Azure Key Vault secret
+            where credentials are stored. Defaults to None.
+        query (str, optional): SQL query to querying data in BigQuery. Format of basic
+            query: (SELECT * FROM `{project}.{dataset_name}.{table_name}`).
             Defaults to None.
-        dataset_name (Optional[str], optional): Dataset name. Defaults to None.
-        table_name (Optional[str], optional): Table name. Defaults to None.
-        dataset_name (Optional[str], optional): Dataset name. Defaults to None.
-        table_name (Optional[str], optional): Table name. Defaults to None.
-        date_column_name (Optional[str], optional): The user can provide the name of
-            the date. If the user-specified column does not exist, all data will be
-            retrieved from the table. Defaults to None.
-        start_date (Optional[str], optional): Parameter to pass start date e.g.
+        dataset_name (str, optional): Dataset name. Defaults to None.
+        table_name (str, optional): Table name. Defaults to None.
+        dataset_name (str, optional): Dataset name. Defaults to None.
+        table_name (str, optional): Table name. Defaults to None.
+        date_column_name (str, optional): The user can provide the name of the date. If
+            the user-specified column does not exist, all data will be retrieved from
+            the table. Defaults to None.
+        start_date (str, optional): Parameter to pass start date e.g.
             "2022-01-01". Defaults to None.
-        end_date (Optional[str], optional): Parameter to pass end date e.g.
+        end_date (str, optional): Parameter to pass end date e.g.
             "2022-01-01". Defaults to None.
-        columns (List[str], optional): List of columns from given table name.
-            Defaults to [""].
-        adls_credentials (Optional[Dict[str, Any]], optional): The credentials as a
-            dictionary. Defaults to None.
-        adls_config_key (Optional[str], optional): The key in the viadot config holding
-            relevant credentials. Defaults to None.
-        adls_azure_key_vault_secret (Optional[str], optional): The name of the Azure Key
-            Vault secret containing a dictionary with ACCOUNT_NAME and Service Principal
+        columns (list[str], optional): List of columns from given table name.
+            Defaults to None.
+        adls_config_key (str, optional): The key in the viadot config holding relevant
+            credentials. Defaults to None.
+        adls_azure_key_vault_secret (str, optional): The name of the Azure Key Vault
+            secret containing a dictionary with ACCOUNT_NAME and Service Principal
             credentials (TENANT_ID, CLIENT_ID, CLIENT_SECRET) for the Azure Data Lake.
             Defaults to None.
-        adls_path (Optional[str], optional): Azure Data Lake destination file path.
+        adls_path (str, optional): Azure Data Lake destination file path.
             Defaults to None.
         adls_path_overwrite (bool, optional): Whether to overwrite the file in ADLS.
             Defaults to True.
@@ -111,7 +83,6 @@ def bigquery_to_adls(
     return df_to_adls(
         df=data_frame,
         path=adls_path,
-        credentials=adls_credentials,
         credentials_secret=adls_azure_key_vault_secret,
         config_key=adls_config_key,
         overwrite=adls_path_overwrite,
--- src/viadot/orchestration/prefect/tasks/bigquery.py
+++ src/viadot/orchestration/prefect/tasks/bigquery.py
@@ -1,32 +1,6 @@
-"""
-'bigquery.py'.
+"""'bigquery.py'."""
 
-Prefect task wrapper for the BigQuery API connector.
-This module provides an intermediate wrapper between the prefect flow and the connector:
-- Generate the BigQuery API connector.
-- Create and return a pandas Data Frame with the response of the API.
-
-Typical usage example:
-    data_frame = bigquery_to_df(
-        credentials=credentials,
-        config_key=config_key,
-        azure_key_vault_secret=azure_key_vault_secret,
-        query=query,
-        dataset_name=dataset_name,
-        table_name=table_name,
-        date_column_name=date_column_name,
-        start_date=start_date,
-        end_date=end_date,
-        columns=columns,
-    )
-
-Functions:
-    bigquery_to_df(credentials, config_key, azure_key_vault_secret, query, dataset_name,
-    table_name, date_column_name, start_date, end_date, columns): Task to download data
-    from BigQuery API.
-"""  # noqa: D412
-
-from typing import Any, Dict, List, Optional
+from typing import Any
 
 import pandas as pd
 from prefect import task
@@ -38,41 +12,39 @@ from viadot.sources import BigQuery
 
 @task(retries=3, log_prints=True, retry_delay_seconds=10, timeout_seconds=60 * 60)
 def bigquery_to_df(
-    credentials: Optional[Dict[str, Any]] = None,
-    config_key: str = None,
-    azure_key_vault_secret: Optional[str] = None,
-    query: Optional[str] = None,
-    dataset_name: Optional[str] = None,
-    table_name: Optional[str] = None,
-    date_column_name: Optional[str] = None,
-    start_date: Optional[str] = None,
-    end_date: Optional[str] = None,
-    columns: List[str] = [""],
+    credentials: dict[str, Any] | None = None,
+    config_key: str | None = None,
+    azure_key_vault_secret: str | None = None,
+    query: str | None = None,
+    dataset_name: str | None = None,
+    table_name: str | None = None,
+    date_column_name: str | None = None,
+    start_date: str | None = None,
+    end_date: str | None = None,
+    columns: list[str] | None = None,
 ) -> pd.DataFrame:
-    """
-    Task to download data from BigQuery API.
+    """Task to download data from BigQuery API.
 
     Args:
-        credentials (Optional[Dict[str, Any]], optional): Mediatool credentials as a
-            dictionary. Defaults to None.
+        credentials (dict[str, Any], optional): Mediatool credentials as a dictionary.
+            Defaults to None.
         config_key (str, optional): The key in the viadot config holding relevant
             credentials. Defaults to None.
-        azure_key_vault_secret (Optional[str], optional): The name of the Azure Key
-            Vault secret where credentials are stored. Defaults to None.
-        query (Optional[str]): SQL query to querying data in BigQuery. Format of
-                basic query: (SELECT * FROM `{project}.{dataset_name}.{table_name}`).
+        azure_key_vault_secret (str, optional): The name of the Azure Key Vault secret
+            where credentials are stored. Defaults to None.
+        query (str): SQL query to querying data in BigQuery. Format of basic query:
+            (SELECT * FROM `{project}.{dataset_name}.{table_name}`). Defaults to None.
+        dataset_name (str, optional): Dataset name. Defaults to None.
+        table_name (str, optional): Table name. Defaults to None.
+        date_column_name (str, optional): The user can provide the name of the date. If
+            the user-specified column does not exist, all data will be retrieved from
+            the table. Defaults to None.
+        start_date (str, optional): Parameter to pass start date e.g.
+            "2022-01-01". Defaults to None.
+        end_date (str, optional): Parameter to pass end date e.g.
+            "2022-01-01". Defaults to None.
+        columns (list[str], optional): List of columns from given table name.
                 Defaults to None.
-        dataset_name (Optional[str], optional): Dataset name. Defaults to None.
-        table_name (Optional[str], optional): Table name. Defaults to None.
-        date_column_name (Optional[str], optional): The user can provide the name of
-            the date. If the user-specified column does not exist, all data will be
-            retrieved from the table. Defaults to None.
-        start_date (Optional[str], optional): Parameter to pass start date e.g.
-            "2022-01-01". Defaults to None.
-        end_date (Optional[str], optional): Parameter to pass end date e.g.
-            "2022-01-01". Defaults to None.
-        columns (List[str], optional): List of columns from given table name.
-                Defaults to [""].
 
     Raises:
         MissingSourceCredentialsError: Credentials were not loaded.
@@ -96,6 +68,5 @@ def bigquery_to_df(
         end_date=end_date,
         columns=columns,
     )
-    data_frame = bigquery.to_df()
 
-    return data_frame
+    return bigquery.to_df()
