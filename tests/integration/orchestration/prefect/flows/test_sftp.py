d03a2bc1c0b5f489c160e8a1b86be8a88446f2f7
Diego-H-S <dhidalgosoto@dyvenia.com>
2024-09-18 09:25:19 +0200

    ✅ updated integration test folder.

--- tests/integration/orchestration/prefect/flows/test_sftp.py
+++ tests/integration/orchestration/prefect/flows/test_sftp.py
@@ -3,13 +3,13 @@
 from viadot.orchestration.prefect.flows import sftp_to_adls
 
 
-def test_sftp_to_adls():
+def test_sftp_to_adls(azure_key_vault_secret, adls_azure_key_vault_secret):
     """Test SFTP server prefect flow."""
     state = sftp_to_adls(
-        azure_key_vault_secret="pim-sftp",
+        azure_key_vault_secret=azure_key_vault_secret,
         file_name="Data Lake- V-F Products with Prices.tsv",
         adls_path="raw/dyvenia_sandbox/sftp/sftp.csv",
-        adls_azure_key_vault_secret="app-azure-cr-datalakegen2-dev",
+        adls_azure_key_vault_secret=adls_azure_key_vault_secret,
         adls_path_overwrite=True,
     )
     all_successful = all(s.type == "COMPLETED" for s in state)
