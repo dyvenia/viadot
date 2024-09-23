03dfceea9eb4ceca6e096cbc79f3537219bb6d09
trymzet <michalmzawadzki@gmail.com>
2024-09-23 14:22:30 +0200

    🐛 Fix modules shadowing 3rd party lib names

--- src/viadot/sources/__init__.py
+++ src/viadot/sources/__init__.py
@@ -2,8 +2,9 @@
 
 from importlib.util import find_spec
 
+from ._duckdb import DuckDB
+from ._trino import Trino
 from .cloud_for_customers import CloudForCustomers
-from .duckdb import DuckDB
 from .epicor import Epicor
 from .exchange_rates import ExchangeRates
 from .genesys import Genesys
@@ -12,7 +13,6 @@ from .mindful import Mindful
 from .outlook import Outlook
 from .sharepoint import Sharepoint
 from .sql_server import SQLServer
-from .trino import Trino
 from .uk_carbon_intensity import UKCarbonIntensity
 
 
@@ -36,7 +36,7 @@ if find_spec("adlfs"):
     __all__.extend(["AzureDataLake"])
 
 if find_spec("duckdb"):
-    from viadot.sources.duckdb import DuckDB  # noqa: F401
+    from viadot.sources._duckdb import DuckDB  # noqa: F401
 
     __all__.extend(["DuckDB"])
 
