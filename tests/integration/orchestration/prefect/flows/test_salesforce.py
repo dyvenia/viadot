da1a2226554d5b53147a48ed22e3184409e1bf84
Maciej Gardzinski <aic.ext@velux.com>
2024-09-25 06:34:47 +0000

    🔐 adls_path hidden

--- tests/integration/orchestration/prefect/flows/test_salesforce.py
+++ tests/integration/orchestration/prefect/flows/test_salesforce.py
@@ -3,13 +3,13 @@
 from viadot.orchestration.prefect.flows import salesforce_to_adls
 
 
-def test_salesforce_to_adls(azure_key_vault_secret, adls_azure_key_vault_secret):
+def test_salesforce_to_adls(azure_key_vault_secret, adls_path, adls_azure_key_vault_secret):
     """Test Salesforce prefect flow."""
     state = salesforce_to_adls(
         azure_key_vault_secret=azure_key_vault_secret,
         env="dev",
         table="Contact",
-        adls_path="raw/dyvenia_sandbox/salesforce/salesforce.csv",
+        adls_path=adls_path,
         adls_azure_key_vault_secret=adls_azure_key_vault_secret,
         adls_path_overwrite=True,
     )
