dc612080cfc63ef5896560c33fa98c6b762c8d4a
Maciej Gardzinski <aic.ext@velux.com>
2024-09-26 07:49:16 +0000

    🎨 Remove ids from test

--- tests/integration/orchestration/prefect/flows/test_mediatool.py
+++ tests/integration/orchestration/prefect/flows/test_mediatool.py
@@ -2,34 +2,12 @@
 
 from viadot.orchestration.prefect.flows import mediatool_to_adls
 
-organization_ids = [
-    "03073189740550375",
-    "075472954388631",
-]
-
-media_entries_columns = [
-    "_id",
-    "organizationId",
-    "mediaTypeId",
-    "vehicleId",
-    "startDate",
-    "endDate",
-    "businessAreaId",
-    "campaignObjectiveId",
-    "campaignId",
-    "netMediaCostChosenCurrency",
-    "currencyId",
-    "eurExchangeRate",
-    "netMediaCostEur",
-    "fee",
-    "totalFeeOfNetMediaCostEur",
-    "totalCostToClientChosenCurrency",
-    "totalCostToClientEur",
-    "nonBiddableMediaCostEur",
-]
 
 
-def test_genesys_to_adls(azure_key_vault_secret, adls_azure_key_vault_secret):
+def test_genesys_to_adls(azure_key_vault_secret,
+                            organization_ids,
+                            media_entries_columns,
+                            adls_azure_key_vault_secret):
     state = mediatool_to_adls(
         azure_key_vault_secret=azure_key_vault_secret,
         organization_ids=organization_ids,
