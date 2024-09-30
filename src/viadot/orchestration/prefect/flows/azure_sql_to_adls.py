6b2dd2b02ccd8864f4bd2432582df7edcec1b79e
rziemianek <rziemianek@dyvenia.com>
2024-09-30 12:28:19 +0200

    🎨 Removed unused parameters and improved structure of the flow code

--- src/viadot/orchestration/prefect/flows/azure_sql_to_adls.py
+++ src/viadot/orchestration/prefect/flows/azure_sql_to_adls.py
@@ -1,10 +1,11 @@
 """Flows for downloading data from Azure SQL and uploading it to Azure ADLS."""
 
-from typing import Any, Dict, List, Literal
+from typing import Any
 
 from prefect import flow
 from prefect.task_runners import ConcurrentTaskRunner
-from viadot.orchestration.prefect.tasks import df_to_adls, azure_sql_to_df
+
+from viadot.orchestration.prefect.tasks import azure_sql_to_df, df_to_adls
 
 
 @flow(
@@ -17,37 +18,29 @@ from viadot.orchestration.prefect.tasks import df_to_adls, azure_sql_to_df
     log_prints=True,
 )
 def azure_sql_to_adls(
-    query: str = None,
-    credentials_secret: str = None,
-    sep: str = "\t",
-    file_path: str = None,
-    if_exists: Literal["replace", "append", "delete"] = "replace",
-    validate_df_dict: Dict[str, Any] = None,
+    query: str | None = None,
+    credentials_secret: str | None = None,
+    validate_df_dict: dict[str, Any] | None = None,
     convert_bytes: bool = False,
-    remove_special_characters: bool = None,
-    columns_to_clean: List[str] = None,
+    remove_special_characters: bool | None = None,
+    columns_to_clean: list[str] | None = None,
     adls_config_key: str | None = None,
     adls_azure_key_vault_secret: str | None = None,
     adls_path: str | None = None,
     adls_path_overwrite: bool = False,
 ) -> None:
-    r"""
-    Download data from Azure SQL to a CSV file and uploading it to ADLS.
+    r"""Download data from Azure SQL to a CSV file and uploading it to ADLS.
 
     Args:
         query (str): Query to perform on a database. Defaults to None.
         credentials_secret (str, optional): The name of the Azure Key Vault
             secret containing a dictionary with database credentials.
             Defaults to None.
-        sep (str, optional): The delimiter for the output CSV file. Defaults to "\t".
-        file_path (str, optional): Local destination path. Defaults to None.
-        if_exists (Literal, optional): What to do if the table exists.
-            Defaults to "replace".
         validate_df_dict (Dict[str], optional): A dictionary with optional list of
             tests to verify the output dataframe. If defined, triggers the `validate_df`
             task from task_utils. Defaults to None.
         convert_bytes (bool). A boolean value to trigger method df_converts_bytes_to_int
-            It is used to convert bytes data type into int, as pulling data with bytes 
+            It is used to convert bytes data type into int, as pulling data with bytes
             can lead to malformed data in data frame.
             Defaults to False.
         remove_special_characters (str, optional): Call a function that remove
@@ -69,9 +62,6 @@ def azure_sql_to_adls(
     data_frame = azure_sql_to_df(
         query=query,
         credentials_secret=credentials_secret,
-        sep=sep,
-        file_path=file_path,
-        if_exists=if_exists,
         validate_df_dict=validate_df_dict,
         convert_bytes=convert_bytes,
         remove_special_characters=remove_special_characters,
