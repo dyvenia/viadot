6a957ec59e435eaccf51b30ac3c32c135580866e
Angelika Tarnawa <59344718+angelika233@users.noreply.github.com>
2024-10-02 13:41:02 +0200

    🎨 Change append to concat in Epicor source (#1075)
    * 🎨 Change append to concat in Epicor source
    
    * ⬆️ Bump viadot
    
    * ⚡️ Optimize concat in the loop
    
    ---------
    
    Co-authored-by: angelika233 <atarnawa@dyvenia.com>

--- src/viadot/sources/epicor.py
+++ src/viadot/sources/epicor.py
@@ -134,7 +134,7 @@ def parse_orders_xml(xml_data: str) -> pd.DataFrame:  # noqa: C901, PLR0912
     Returns:
         pd.DataFrame: DataFrame containing parsed orders data.
     """
-    final_df = pd.DataFrame()
+    dfs = []
     ship_dict = {}
     invoice_dict = {}
     header_params_dict = {}
@@ -197,10 +197,8 @@ def parse_orders_xml(xml_data: str) -> pd.DataFrame:  # noqa: C901, PLR0912
                 line_item = LineItemDetail(**item_params_dict)
                 row = Order(HeaderInformation=header_info, LineItemDetail=line_item)
                 my_dict = row.dict()
-                final_df = final_df.append(
-                    pd.json_normalize(my_dict, max_level=2), ignore_index=True
-                )
-    return final_df
+                dfs.append(pd.json_normalize(my_dict, max_level=2))
+    return pd.concat(dfs, ignore_index=True)
 
 
 def parse_bookings_xml(xml_data: str) -> pd.DataFrame:  # noqa: C901, PLR0912
@@ -211,7 +209,7 @@ def parse_bookings_xml(xml_data: str) -> pd.DataFrame:  # noqa: C901, PLR0912
     Returns:
         pd.DataFrame: DataFrame containing parsed  data.
     """
-    final_df = pd.DataFrame()
+    dfs = []
     ship_dict = {}
     header_params_dict = {}
     item_params_dict = {}
@@ -254,10 +252,8 @@ def parse_bookings_xml(xml_data: str) -> pd.DataFrame:  # noqa: C901, PLR0912
                     HeaderInformation=header_info, LineItemDetail=line_item
                 )
                 my_dict = row.dict()
-                final_df = final_df.append(
-                    pd.json_normalize(my_dict, max_level=2), ignore_index=True
-                )
-    return final_df
+                dfs.append(pd.json_normalize(my_dict, max_level=2))
+    return pd.concat(dfs, ignore_index=True)
 
 
 def parse_open_orders_xml(xml_data: str) -> pd.DataFrame:  # noqa: C901, PLR0912
@@ -268,7 +264,7 @@ def parse_open_orders_xml(xml_data: str) -> pd.DataFrame:  # noqa: C901, PLR0912
     Returns:
         pd.DataFrame: DataFrame containing parsed  data.
     """
-    final_df = pd.DataFrame()
+    dfs = []
     ship_dict = {}
     header_params_dict = {}
     item_params_dict = {}
@@ -310,10 +306,8 @@ def parse_open_orders_xml(xml_data: str) -> pd.DataFrame:  # noqa: C901, PLR0912
                 line_item = LineItemDetail(**item_params_dict)
                 row = Order(HeaderInformation=header_info, LineItemDetail=line_item)
                 my_dict = row.dict()
-                final_df = final_df.append(
-                    pd.json_normalize(my_dict, max_level=2), ignore_index=True
-                )
-    return final_df
+                dfs.append(pd.json_normalize(my_dict, max_level=2))
+    return pd.concat(dfs, ignore_index=True)
 
 
 def parse_customer_xml(xml_data: str) -> pd.DataFrame:
@@ -324,7 +318,7 @@ def parse_customer_xml(xml_data: str) -> pd.DataFrame:
     Returns:
         pd.DataFrame: DataFrame containing parsed  data.
     """
-    final_df = pd.DataFrame()
+    dfs = []
     ship_dict = {}
     customer_params_dict = {}
 
@@ -354,10 +348,8 @@ def parse_customer_xml(xml_data: str) -> pd.DataFrame:
                     customer_params_dict.update(cust_parameter)
                 cust_info = Customer(**customer_params_dict)
                 my_dict = cust_info.dict()
-                final_df = final_df.append(
-                    pd.json_normalize(my_dict, max_level=2), ignore_index=True
-                )
-    return final_df
+                dfs.append(pd.json_normalize(my_dict, max_level=2))
+    return pd.concat(dfs, ignore_index=True)
 
 
 class EpicorCredentials(BaseModel):
