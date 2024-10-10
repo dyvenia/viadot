1d5268af03521a3139804ea188b7ad7da853ab95
trymzet <michalmzawadzki@gmail.com>
2024-10-10 18:04:24 +0200

    🐛 Fix `boto3.Session` not having a `endpoint_url` param

--- src/viadot/sources/redshift_spectrum.py
+++ src/viadot/sources/redshift_spectrum.py
@@ -98,6 +98,9 @@ class RedshiftSpectrum(Source):
                 "Credentials not specified. Falling back to `boto3` default credentials."
             )
 
+        if self.credentials:
+            wr.config.s3_endpoint_url = self.credentials.get("endpoint_url")
+
         self._session = None
         self._con = None
 
@@ -121,7 +124,6 @@ class RedshiftSpectrum(Source):
                 profile_name=self.credentials.get("profile_name"),
                 aws_access_key_id=self.credentials.get("aws_access_key_id"),
                 aws_secret_access_key=self.credentials.get("aws_secret_access_key"),
-                endpoint_url=self.credentials.get("endpoint_url"),
             )
         return self._session
 
