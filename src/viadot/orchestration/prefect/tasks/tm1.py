684e8f62516ee0eb52c9fa10488fdc91e573d25b
judynah <mrs.nataliawalczak@gmail.com>
2024-11-18 10:55:01 +0100

    ✨ Added tm1 task

---
+++ src/viadot/orchestration/prefect/tasks/tm1.py
@@ -0,0 +1,80 @@
+"""Task for downloading data from TM1 to a pandas DataFrame."""
+
+from typing import Any
+
+from pandas import DataFrame
+from prefect.logging import get_run_logger
+
+from src.viadot.config import get_source_credentials
+from src.viadot.orchestration.prefect import tasks
+from src.viadot.orchestration.prefect.exceptions import MissingSourceCredentialsError
+from src.viadot.orchestration.prefect.utils import get_credentials
+from src.viadot.sources.tm1 import TM1
+
+
+@tasks(retries=3, retry_delay_seconds=10, timeout_seconds=60 * 60 * 3)
+def tm1_to_df(
+    mdx_query: str | None = None,
+    cube: str | None = None,
+    view: str | None = None,
+    limit: int | None = None,
+    private: bool = False,
+    credentials_secret: dict[str, Any] | None = None,
+    config_key: str = "TM1",
+    verify: bool = False,
+    if_empty: str = "skip",
+) -> DataFrame:
+    """Download data from TM1 to pandas DataFrame.
+
+    Args:
+        credentials_secret (dict[str, Any], optional): The name of the secret that
+        stores TM1 credentials.
+        More info on: https://docs.prefect.io/concepts/blocks/. Defaults to None.
+        config_key (str, optional): The key in the viadot config holding relevant
+        credentials. Defaults to "TM1".
+        mdx_query (str, optional): MDX select query needed to download the data.
+        Defaults to None.
+        cube (str, optional): Cube name from which data will be downloaded.
+        Defaults to None.
+        view (str, optional): View name from which data will be downloaded.
+        Defaults to None.
+        limit (str, optional): How many rows should be extracted.
+        If None all the available rows will be downloaded. Defaults to None.
+        private (bool, optional): Whether or not data download should be private.
+        Defaults to False.
+        verify (bool, optional): Whether or not verify SSL certificates.
+        Defaults to False.
+        if_empty (str, optional): What to do if output DataFrame is empty.
+        Defaults to "skip".
+
+    """
+    if not (credentials_secret or config_key):
+        raise MissingSourceCredentialsError
+
+    logger = get_run_logger()
+
+    credentials = get_source_credentials(config_key) or get_credentials(
+        credentials_secret
+    )
+
+    bc = TM1(
+        credentials=credentials,
+        config_key=config_key,
+        mdx_query=mdx_query,
+        cube=cube,
+        view=view,
+        limit=limit,
+        private=private,
+        verify=verify,
+    )
+
+    df = bc.to_df(if_empty=if_empty)
+
+    nrows = df.shape[0]
+    ncols = df.shape[1]
+
+    logger.info(
+        f"Successfully downloaded {nrows} rows and {ncols} columns of data to a DataFrame."
+    )
+
+    return df
