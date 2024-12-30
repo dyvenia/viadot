7ba8698f5a4403f47600cd645c0e157178b59d76
Maciej Gardzinski <mgardzinski@dyvenia.com>
2024-12-30 14:15:40 +0100

    🔥 Remove default values from `test_bigquery` variables

--- tests/unit/test_bigquery.py
+++ tests/unit/test_bigquery.py
@@ -18,9 +18,6 @@ variables = {
         "private_key": "----------",
         "client_email": "email@test.com",
         "client_id": "client_id",
-        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        "token_uri": "https://oauth2.googleapis.com/token",
-        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
         "client_x509_cert_url": "https://www.googleapis.com/robot/v1/"
         + "metadata/x509/email@test.com",
     }
