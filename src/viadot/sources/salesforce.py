dea48fc3c921f4178dbeb667e970ec0471bc426b
winiar <winiar93@gmail.com>
2025-04-07 13:37:38 +0200

    adjusted method docstring

--- src/viadot/sources/salesforce.py
+++ src/viadot/sources/salesforce.py
@@ -154,7 +154,8 @@ class Salesforce(Source):
             df (pd.DataFrame): Selected rows from Salesforce.
             table (str): Table name.
             external_id (str, optional): External ID. Defaults to None.
-            raise_on_error (bool, optional): Whether to raise an exception when
+            raise_on_error (bool, optional): If True, raises an exception on error. Defaults to False.
+
         Returns:
             None
         """
