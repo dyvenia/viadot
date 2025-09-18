f050763f0e8bf5187e871221017c0d88fd52d9dc
Dominik Tyrala <dtyrala@dyvenia.com>
2025-09-18 17:35:59 +0200

    changes to match sap connector

--- src/viadot/orchestration/prefect/flows/sap_to_redshift_spectrum.py
+++ src/viadot/orchestration/prefect/flows/sap_to_redshift_spectrum.py
@@ -100,7 +100,7 @@ def sap_to_redshift_spectrum(  # noqa: PLR0913
             ...
         )
     """
-    csv_path = sap_rfc_to_df(
+    df = sap_rfc_to_df(
         query=query,
         tests=tests,
         func=func,
@@ -116,7 +116,7 @@ def sap_to_redshift_spectrum(  # noqa: PLR0913
     )
 
     return df_to_redshift_spectrum(
-        csv_path=csv_path,
+        df=df,
         to_path=to_path,
         schema_name=schema_name,
         table=table,
