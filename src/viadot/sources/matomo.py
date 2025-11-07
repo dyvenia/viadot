a3563223f02b3da255789e9e1c4052773e1e1cde
Dominik Tyrala <dtyrala@dyvenia.com>
2025-11-07 15:41:49 +0100

    Fix tests and minors

--- src/viadot/sources/matomo.py
+++ src/viadot/sources/matomo.py
@@ -237,7 +237,7 @@ class Matomo(Source):
             record_prefix=record_prefix,
         )
 
-        df = df.reindex(columns=top_level_fields)  
+        df = df.reindex(columns=top_level_fields)
 
         if df.empty:
             self.logger.warning("No records found in the specified record_path.")
