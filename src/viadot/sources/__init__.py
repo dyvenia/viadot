a614e662973c0d8958b16444b8583e7c9add239b
fstajniak <fstajniak@dyvenia.com>
2025-12-23 14:49:56 +0100

    Add smbclient wrapper

--- src/viadot/sources/__init__.py
+++ src/viadot/sources/__init__.py
@@ -25,6 +25,7 @@ from .salesforce import Salesforce
 from .sftp import Sftp
 from .sharepoint import Sharepoint, SharepointList
 from .smb import SMB
+from .smbclient_wrapper import SMBClientWrapper
 from .sql_server import SQLServer
 from .sqlite import SQLite
 from .supermetrics import Supermetrics
@@ -35,6 +36,7 @@ from .vid_club import VidClub
 
 __all__ = [
     "SMB",
+    "SMBClientWrapper",
     "TM1",
     "AzureSQL",
     "BigQuery",
