2f5113ae68766b8148979464007962117466da8d
Damian Bugaj <damianbugajit@gmail.com>
2026-03-27 10:22:40 +0100

    Update src/viadot/sources/matomo.py
    Co-authored-by: Marcin Purtak <44641138+marcinpurtak@users.noreply.github.com>

--- src/viadot/sources/matomo.py
+++ src/viadot/sources/matomo.py
@@ -138,7 +138,7 @@ class Matomo(Source):
         """
       dates=(date_range[0], date_range[1])
       date_range_parsed = parse_dates(dates)
-        return f"{start:%Y-%m-%d},{end:%Y-%m-%d}"
+        return f"{date_range_parsed[0]:%Y-%m-%d},{date_range_parsed[1]:%Y-%m-%d}
 
     def fetch_data(
         self,
