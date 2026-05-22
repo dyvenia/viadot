4182f027eb2b228a9ed08f53f72e45690d2423db
Michał Zawadzki <michalmzawadzki@gmail.com>
2026-05-22 12:44:39 +0200

    🔥 Remove old Rye files (#1314)

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
 # Check whether dev requirements are already initialized.
-if [ ! -f "./requirements-dev.txt" ]; then
-    echo "Installing dev dependencies..."
-    sed '/-e/d' requirements-dev.lock > requirements-dev.txt
-    pip install -r requirements-dev.txt --user
-    echo "Dev dependencies have been installed successfully!"
-fi
+echo "Installing dev dependencies..."
+uv sync --dev
+echo "Dev dependencies have been installed successfully!"
 
 sleep infinity
